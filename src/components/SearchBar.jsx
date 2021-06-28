f3e332a0e3ae2f89296c7bf0207443859497a48a
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-06-28 14:33:16 -0300

    iniciando o projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          Inclui o texto
+          <input
+            type="text"
+            data-testid="text-input"
+            value="searchText"
+            onChange="onSearchTextChange"
+          />
+        </label>
+      </form>
+    );
+  }
+}
