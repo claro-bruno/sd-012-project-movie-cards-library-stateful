61887aefdd943156b5a684a7af7e90972187651f
Bruno Augusto <brunaugusto@gmail.com>
2021-06-28 18:03:01 -0300

    Requisito 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -15,15 +15,39 @@ class MovieLibrary extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.moviesFilter = this.moviesFilter.bind(this);
   }
 
   handleChange(e) {
     const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
     this.setState({ [e.target.name]: value });
+    this.moviesFilter();
+  }
+
+  moviesFilter() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    let filteredMovies = movies;
+
+    if (searchText !== '') {
+      filteredMovies = filteredMovies.filter((movie) => (
+        movie.title.includes(searchText)
+        || movie.subtitle.includes(searchText)
+        || movie.storyline.includes(searchText)
+      ));
+    }
+    if (bookmarkedOnly) {
+      filteredMovies = filteredMovies.filter((movie) => movie.bookmarked);
+    }
+    if (selectedGenre !== '') {
+      filteredMovies = filteredMovies.filter((movie) => movie.genre === selectedGenre);
+    }
+
+    return filteredMovies;
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const movies = this.moviesFilter();
     return (
       <section>
         <h2> My awesome movie library </h2>
