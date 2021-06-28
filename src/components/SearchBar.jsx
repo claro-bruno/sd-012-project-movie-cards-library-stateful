887b4d61b3e0bf02c893092579bc8b2d13335d69
Milton Castro <castro.milton07@gmail.com>
2021-06-28 16:42:51 -0300

    Requisito 3 - Renderize um input do tipo texto dentro do form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <form data-testid="search-bar-form"> </form>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            id="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
     );
   }
 }
