58d86c0c2f09919f21f41e32577c867ffb7937dc
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-28 14:58:03 -0300

    Requisito 1 - Cria Componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,18 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+   const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props
+    return (
+      <div>
+        <h2> My awesome SearchBar </h2>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
