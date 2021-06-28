f310204d7551be1d4bc27b8c7dd455197d533593
Teika <shimworks.tk@gmail.com>
2021-06-28 15:31:55 -0300

    resolve requisito 06

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,26 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+constructor(){
+  super()
+  this.state = {
+    subtitle: "",
+    title: "",
+    imagePath: "",
+    storyline: "",
+    rating: 0,
+    genre: "action",
+  }
+}
+
+  render(){
+    const { onClick } = this.props
+    return(
+      <div>
+        {this.state.genre}
+      </div>
+    )
+  }
+}
+
+export default AddMovie
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -15,7 +15,7 @@ class MovieLibrary extends Component {
         <h2> My awesome movie library </h2>
         <SearchBar />
         <MovieList movies={this.props.movies} />
-        {/* <AddMovie /> */}
+        <AddMovie />
       </div>
     );
   }
