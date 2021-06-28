8845b8df8831025486fdc86dd98f903da366967f
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-06-28 16:16:10 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,25 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      movies,
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes
+    .arrayOf(PropTypes.oneOfType([PropTypes.string])).isRequired,
+};
