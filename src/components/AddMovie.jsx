ed18c8e2b7957c9618db361faaf23eb3cdb6ce3f
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-28 16:38:30 -0300

    Requisito 13 - Renderiza select de genero

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -80,6 +80,19 @@ class AddMovie extends Component {
             onChange={ this.handleChange }
           />
         </label>
+        <label htmlFor="genre-input" data-testid="genre-input-label">
+          Gênero
+          <select
+            data-testid="genre-input"
+            name="genre"
+            value={ genre }
+            onChange={ this.handleChange }
+          >
+            <option data-testid="genre-option" value="action" selected>Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
