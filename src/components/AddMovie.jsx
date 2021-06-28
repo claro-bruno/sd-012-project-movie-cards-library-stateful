5433de0dcb100bcbbf166b151a21f1486358be0d
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-28 15:33:48 -0300

    criando estado inicial do componente

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-type';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 5,
+      genre: 'action',
+    }
+  }
   render() {
+    const { props } = this;
+    const { onClick } = props;
 
   }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
 }
 
 export default AddMovie;
