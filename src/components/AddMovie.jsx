03e48740142d2b14ae942eb6b885682276828dbf
Merlone <alestori7@gmail.com>
2021-06-28 15:02:30 -0300

    requisito 10 - parcial + mudança na criação do AddMovieInputs

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -19,20 +19,29 @@ export default class AddMovie extends Component {
 
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
-
+    const image = imagePath;
     return (
       <form data-testid="add-movie-form">
         <AddMovieInputs
+          id="title-input"
           value={ title }
           name="title"
           onChange={ this.handleChange }
           inner="Título"
         />
         <AddMovieInputs
+          id="subtitle-input"
           value={ subtitle }
           name="subtitle"
           onChange={ this.handleChange }
           inner="Subtítulo"
+        />
+        <AddMovieInputs
+          id="image-input"
+          value={ imagePath }
+          name="imagePath"
+          onChange={ this.handleChange }
+          inner="Imagem"
         />
       </form>
     );
--- src/components/AddMovieInputs.jsx
+++ src/components/AddMovieInputs.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 export default class AddMovieInputs extends Component {
   render() {
-    const { value, onChange, name, inner } = this.props;
+    const { value, onChange, name, inner, id } = this.props;
     return (
-      <label data-testid={ `${name}-input-label` } htmlFor={ `${name}-input` }>
+      <label data-testid={ `${id}-label` } htmlFor={ `${id}` }>
         {inner}
         <input
           value={ value }
-          data-testid={ `${name}-input` }
+          data-testid={ `${id}` }
           onChange={ onChange }
           name={ name }
-          id={ `${name}-input` }
+          id={ `${id}` }
         />
       </label>
     );
