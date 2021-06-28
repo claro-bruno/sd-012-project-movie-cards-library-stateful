b72c37e749418eaf813f2583d1cbde2341789c18
Daniel <danielbped@gmail.com>
2021-06-28 18:06:23 -0300

    Resolve requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-    };
-  }
-
   render() {
     return (
       <div>
@@ -22,17 +12,5 @@ class MovieLibrary extends Component {
     );
   }
 }
-
-MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    rating: PropTypes.number,
-    imagePath: PropTypes.string,
-    bookmarked: PropTypes.bool,
-    genre: PropTypes.string,
-  })).isRequired,
-};
 
 export default MovieLibrary;
