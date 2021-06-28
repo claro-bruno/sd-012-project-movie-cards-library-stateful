d6ff9d7af98087f9d557e5949d170bb2383234b5
ladcs <hpladcs@gmail.com>
2021-06-28 17:23:01 -0300

    R5, searchBar test check

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,7 +18,36 @@ class SearchBar extends React.Component {
         <form data-testid="search-bar-form">
           <label htmlFor="searchText" data-testid="text-input-label">
             Inclui o texto:
-            <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input" />
+            <input
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+              id="searchText"
+            />
+          </label>
+          <label data-testid="checkbox-input-label" htmlFor="favorite">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+              id="favorite"
+            />
+          </label>
+          <label htmlFor="gender" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
           </label>
         </form>
       </div>
