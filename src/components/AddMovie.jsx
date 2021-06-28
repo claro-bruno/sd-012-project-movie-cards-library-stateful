0c8e3b92f21ccb9ca80dc28c12e945954898afeb
Daniel <danielbped@gmail.com>
2021-06-28 15:08:03 -0300

    Resolve requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
+    const { onClick } = this.props;
     return (
-      <div />
+      <form data-testid="add-movie-form" />
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
