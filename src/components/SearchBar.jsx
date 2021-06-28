46abf28ec74b0d79b8caee550e0f44145ad2ae3a
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-28 14:46:29 -0300

    adicionando componente SearchBar e as props dele

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,18 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { props } = this;
+    const { 
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnlye,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenre
+      } = props;
+    return;
+  }
+}
+
+export default SearchBar;
