15e49742057624f20d100ad08bfc3feea3b2a9bc
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-28 15:47:14 -0300

    Requisito 6 - Cria Componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,18 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  //   constructor(props) {
+  //     super(props);
+  //   }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome AddMovie</h2>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
