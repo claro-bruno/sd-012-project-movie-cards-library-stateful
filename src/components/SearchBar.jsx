ca957dfc6392b846bd35c1b55602ce90b40ef55e
Guilherme Dias <gmd.delta@gmail.com>
2021-06-28 17:07:23 -0300

    [PT-BR] Resolvido requisito 5.[EN] Solved 5th requirement.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,6 +23,7 @@ class SearchBar extends Component {
         </label>
         <br />
         <label htmlFor="booksmark-checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
           <input
             type="checkbox"
             id="booksmark-checkbox"
@@ -30,7 +31,21 @@ class SearchBar extends Component {
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
-          Mostrar somente favoritos
+        </label>
+        <br />
+        <label htmlFor="genreOption" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="genreOption"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
