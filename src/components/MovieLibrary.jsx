cb0d672607a6d9b4ba1e3d6b621b3c1e6cc8c74a
pedro21medeiros <pedro21medeiros@gmail.com>
2021-06-28 18:29:15 -0300

    implementa funçoes responsaveis pela interaçao do usuario

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+    this.handleAddMovie = this.handleAddMovie.bind(this);
+    this.searchTextChange = this.searchTextChange.bind(this);
+    this.bookmarkedChange = this.bookmarkedChange.bind(this);
+  }
+
+  handleAddMovie(movieData) {
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, movieData],
+    });
+  }
+
+  searchTextChange(evt) {
+    this.setState({
+      searchText: evt.target.value,
+    });
+  }
+
+  bookmarkedChange(evt) {
+    this.setState({
+      bookmarkedOnly: evt.target.checked,
+    });
+  }
+}
