f6ae137898bee1936aac70a3425beec04b6c935c
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 15:27:57 -0300

    Requirement 03-> Add props to input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="text-input">
+        <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
-          <input type="text" id="text-input" data-testid="text-input" />
+          <input
+            type="text"
+            id="text-input"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
         </label>
       </form>
     );
