23a67e7101828648f3e79d4ac304c0af00835d60
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-06-28 16:18:10 -0300

    Resolvendo requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+
+    this.handle
+  }
+    
+  render() {
+    const { onClick } = this.props;
+    return (
+[]
+    );
+  }
+}
+
+export default AddMovie;
