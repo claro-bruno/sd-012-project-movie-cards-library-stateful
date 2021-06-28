d976a24cd0d2b5bf99ee6e802f03de2983b1d965
Merlone <alestori7@gmail.com>
2021-06-28 16:54:46 -0300

    corrige erros de proptypes em Rating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
-import movies from './data';
+import moviesData from './data';
 import MovieLibrary from './components/MovieLibrary';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: moviesData,
+    };
+  }
+
   render() {
+    const { movies } = this.state;
     return (
       <div className="App">
         <Header />
