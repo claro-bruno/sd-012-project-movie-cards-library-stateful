0ed09d00dcb6ea925ad5f6b9999a68f23574bcbc
David Azevedo <zevid.dev@outlook.com>
2021-06-28 15:28:00 -0300

    Create/Render label, input in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,6 +7,13 @@ class SearchBar extends React.Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto</label>
+        <input
+          data-testid="text-input"
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
       </form>
     );
   }
