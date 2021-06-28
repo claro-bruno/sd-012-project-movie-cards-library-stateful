5148c0e2a7a8d3ce7a5b222936ef3b94ebbdc383
Daniel <danielbped@gmail.com>
2021-06-28 18:37:38 -0300

    Resolve requisitos 15, 16, 17

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
+import data from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={ data } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   render() {
+    const { movies } = this.props;
+    const filteredMovies = movies.map((movie) => movie.genre === 'fantasy');
     return (
       <div>
         <SearchBar />
-        <MovieList />
+        <AddMovie />
+        <MovieList movies={ filteredMovies } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+      bookmarked: PropTypes.bool,
+      genre: PropTypes.string,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default MovieLibrary;
