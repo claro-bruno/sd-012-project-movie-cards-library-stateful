2fab7326b1b349172e90e51b87dafcdfc4627d5d
Rodrigo <rodrigo.facury14@gmail.com>
2021-06-28 19:36:35 -0300

    Requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange() {
+    console.log(this.state);
+  }
+
   render() {
     const { onClick } = this.props;
     return (
-      <form>
-        { onClick }
+      <form data-testid="add-movie-form">
+        { onClick() }
         <input type="text" id="find-subtitle" />
         <input type="text" id="find-title" />
         <input type="text" id="find-image-path" />
