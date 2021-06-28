870f549eb20e93454ca3a9a8ea98ad60fd06e986
Nuwanda <morato_dev@protonmail.com>
2021-06-28 17:05:32 -0300

    começando o form de criar novos movies

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="importMovieTitle" data-testid="title-input-label">
+          Título
+          <input type="text" name="" id="importMovieTitle" data-testid="title-input" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
