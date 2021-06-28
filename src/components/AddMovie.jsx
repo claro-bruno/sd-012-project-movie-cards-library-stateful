2b59f1fd2feebe04a079d5b734b4ef5df06a207a
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 17:02:49 -0300

    Requirement 08-> Render input to get movie `title`

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,11 +12,40 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
   }
 
   render() {
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
     return (
       <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            id="title-input"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
