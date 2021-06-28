d61c5d49a5f0bb091a7bb75a45271bde61f0aa99
Teika <shimworks.tk@gmail.com>
2021-06-28 15:23:42 -0300

    resolve requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,6 +14,15 @@ class SearchBar extends React.Component {
           Mostrar somente favoritos
           <input id="checkbox-input" data-testid="checkbox-input" type="checkbox" checked ={bookmarkedOnly} onChange={onBookmarkedChange} />
           </label>
+          <label htmlFor="selectedGenre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select name="Genre" id="selectedGenre" data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange} >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+          </label>
         </form>
       </div>
     )
