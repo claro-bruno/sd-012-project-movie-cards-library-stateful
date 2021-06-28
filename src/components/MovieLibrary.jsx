aada3939ce1199f77fd1070bd2ad903b38341a51
pedro21medeiros <pedro21medeiros@gmail.com>
2021-06-28 18:55:17 -0300

    implementa parte da MovieLibrary e PropTypes

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
-    const { movies } = this.props;
+
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
@@ -14,6 +16,8 @@ class MovieLibrary extends React.Component {
     this.handleAddMovie = this.handleAddMovie.bind(this);
     this.searchTextChange = this.searchTextChange.bind(this);
     this.bookmarkedChange = this.bookmarkedChange.bind(this);
+    this.selectedGenreChange = this.selectedGenreChange.bind(this);
+    this.movieFilter = this.movieFilter.bind(this);
   }
 
   handleAddMovie(movieData) {
@@ -34,4 +38,47 @@ class MovieLibrary extends React.Component {
       bookmarkedOnly: evt.target.checked,
     });
   }
+
+  selectedGenreChange(evt) {
+    this.setState({
+      selectedGenre: evt.target.value,
+    });
+  }
+
+  movieFilter() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return movies
+      .filter(({ genre }) => genre.includes(selectedGenre))
+      .filter(({ bookmarked }) => !bookmarkedOnly || bookmarked)
+      .filter(({ title, subtitle, storyline }) => (
+        title.toLowerCase().includes(searchText.toLowerCase())
+      || subtitle.toLowerCase().includes(searchText.toLowerCase())
+      || storyline.toLowerCase().includes(searchText.toLowerCase())
+      ));
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+
+    return (
+      <>
+        <SearchBar
+          searchText={ searchText }
+          searchTextChange={ this.searchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          bookmarkedChange={ this.bookmarkedChange }
+          selectedGenre={ selectedGenre }
+          selectedGenreChange={ this.selectedGenreChange }
+        />
+
+        <MovieList
+          movies={ this.movieFilter() }
+        />
+
+        <AddMovie onClick={ this.handleAddMovie } />
+      </>
+    );
+  }
 }
+
+export default MovieLibrary;
