7655d390058c1c9edfc137615dbdb23c9904736e
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-28 14:56:01 -0300

    Requisito 8 - Criado o estado inicial dentro de um constructor, criado a funcao handleChange e adicionado o input referente ao title

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const initialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = initialState;
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-        <h1>AddMovie</h1>
+        <label htmlFor="title-input-label" data-testid="title-input-label">
+          Título
+          <input
+            name="title"
+            data-testid="title-input"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
