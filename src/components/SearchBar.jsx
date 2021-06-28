ff8f6ddb9ce6ef4b1ecccbce7ff7dd66744dee33
Patrick Dack <patrickdack89@gmail.com>
2021-06-28 15:19:58 -0300

    Implementa propTypes para o compomente SeachBar e inicia a implementaçao do input do requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <p>Hello World</p>
+        <label data-testid="text-input-label" htmlFor>
+          Inclui o texto:
+          <input
+            value={ searchText }
+          />
+        </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
