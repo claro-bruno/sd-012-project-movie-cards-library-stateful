9a356f952fb2a8ffbb44dd1c2f8503dfebdc4fe7
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-28 20:41:09 -0300

    cria MovieLibrary

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <AddMovie />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,28 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        Bora passar no lint com:
+        { movies }
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    bookmarked: PropTypes.bool.isRequired,
+    genre: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default MovieLibrary;
