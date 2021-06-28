61ee3d0cb87d0209746294c976f9abdc83637932
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-28 17:12:54 -0300

    Requisito 18 - Chamar o MovieList passando movies como props, adicionado as props de SearchBar do requisito anterior e concluido o ultimo que faltou do 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -16,11 +17,63 @@ class MovieLibrary extends React.Component {
     };
   }
 
+  onSearchTextChange = (e) => {
+    this.setState({
+      searchText: e.target.value,
+    }, () => {
+      this.setState({
+        movies: this.filterMoviesBy(),
+      });
+    });
+  }
+
+  onBookmarkedChange = (e) => {
+    this.setState({ bookmarkedOnly: e.target.checked }, () => {
+      this.setState({
+        movies: this.filterMoviesBy('bookmarked'),
+      });
+    });
+  }
+
+  onSelectedGenreChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      selectedGenre: value,
+    }, () => {
+      this.setState({
+        movies: this.filterMoviesBy('genre'),
+      });
+    });
+  }
+
+  filterMoviesBy = (typeFilter) => {
+    const { searchText, selectedGenre, bookmarkedOnly, movies } = this.state;
+    if (typeFilter === 'genre') {
+      return movies.filter((item) => item.genre === selectedGenre);
+    }
+    if (typeFilter === 'bookmarked') {
+      return movies.filter((movie) => movie.bookmarked === bookmarkedOnly);
+    }
+    return movies.filter(({ title, subtitle, storyline }) => (
+      (title.includes(searchText))
+      || (subtitle.includes(searchText))
+      || (storyline.includes(searchText))
+    ));
+  }
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
       </div>
     );
   }
