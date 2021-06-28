4be20d81b59873ad22d175c638b5104fb093c644
Sass <dudu_sass12@hotmail.com>
2021-06-28 14:38:44 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <span>oi</span>
+    );
+  }
+}
+
+export default SearchBar;
 // implement SearchBar component here
