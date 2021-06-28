0d3dd53acdf867e41de056636980c1e59c2b229a
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-06-28 16:03:19 -0300

    Adiciona estados ao componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   render() {
     const { onClick } = this.props;
     return (
