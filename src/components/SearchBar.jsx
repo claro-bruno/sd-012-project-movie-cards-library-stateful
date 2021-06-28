531b55974a4b8f10a7f6b7aac67c74f5f9952cd2
Adriana Biberg <adribiberg@hotmail.com>
2021-06-28 17:45:27 -0300

    Cumpre requisito 5 - Adiciona select com 4 options ao form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -30,8 +30,21 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
-        { selectedGenre }
-        { onSelectedGenreChange }
+        <label htmlFor="input-select" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name="input-select"
+            id="input-select"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>);
   }
 }
