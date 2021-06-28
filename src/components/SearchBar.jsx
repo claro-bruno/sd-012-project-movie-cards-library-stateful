271012108d518e725c2f018e746f2eadbf3bf1f8
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-28 17:29:07 -0300

    conclui requisito_02 - Renderize um formulário dentro de <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <h1>renderiza Searchbar</h1>
+      <form data-testid="search-bar-form" />
     );
   }
 }
