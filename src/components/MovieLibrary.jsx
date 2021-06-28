8cef2a4783a6c10e9440a5a5ea7ff455146dfb6f
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-28 16:26:02 -0300

    Requisito 15 - Criado o componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,12 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    // const { movies } = this.state;
+    return (
+      <h1>Requisito 15</h1>
+    );
+  }
+}
+
+export default MovieLibrary;
