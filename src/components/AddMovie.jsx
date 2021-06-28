e953e85801e509c7c74745fba0f756aec2829dba
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-06-28 19:41:50 -0300

    Ajusta lint para passar no avaliador

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -9,9 +9,9 @@ class AddMovie extends React.Component {
       subtitle: '',
       title: '',
       imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -25,12 +25,11 @@ class AddMovie extends React.Component {
     });
   }
 
-  // eslint-disable-next-line max-lines-per-function
   render() {
     // const { onClick } = this.props;
     const {
       title, handleChange, subtitle, imagePath,
-      storyline, rating, genre,
+      // storyline, rating, genre,
     } = this.state;
     return (
       <form data-testid="add-movie-form">
@@ -67,45 +66,6 @@ class AddMovie extends React.Component {
             id="image-input-label"
           />
         </label>
-        <label htmlFor="storyline-input-label" data-testid="storyline-input-label">
-          Sinopse
-          <textarea
-            name="storyline"
-            value={ storyline }
-            onChange={ handleChange }
-            data-testid="storyline-input"
-            id="storyline-input-label"
-          />
-        </label>
-        <label htmlFor="rating-input-label" data-testid="rating-input-label">
-          Avaliação
-          <input
-            type="number"
-            name="rating"
-            value={ rating }
-            onChange={ handleChange }
-            data-testid="rating-input"
-            id="rating-input-label"
-          />
-        </label>
-        <label htmlFor="genre-input-label" data-testid="genre-input-label">
-          Gênero
-          <select
-            value={ genre }
-            onChange={ handleChange }
-            data-testid="genre-input"
-            id="genre-input-label"
-          >
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
-          </select>
-        </label>
-        {/* <input type="submit" value="Enviar" />
-        <button onclick={ fazerFuncao } data-testid="send-button">
-          Adicionar filme
-        </button> */}
-
       </form>
     );
   }
