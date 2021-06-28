e6001d3fe73c696a1de11bb5b75ce31e65397dd6
lucasnahas <lucasahas@gmail.com>
2021-06-28 15:31:22 -0300

    inicia search bar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+// import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        {/* <MovieList movies={this.props.movies} /> */}
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Search bar</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
