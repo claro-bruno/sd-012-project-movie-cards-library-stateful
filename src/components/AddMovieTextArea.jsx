bd3580350f1af18cdbfac1c97688287738ef7139
Merlone <alestori7@gmail.com>
2021-06-28 18:59:01 -0300

    adiciona id ao textarea de sinopse

--- src/components/AddMovieTextArea.jsx
+++ src/components/AddMovieTextArea.jsx
@@ -5,13 +5,14 @@ export default class AddStory extends Component {
   render() {
     const { value, onChange, name } = this.props;
     return (
-      <label data-testid="storyline-input-label" htmlFor={ name }>
+      <label data-testid="storyline-input-label" htmlFor="storyline-input">
         Sinopse
         <textarea
           value={ value }
           data-testid="storyline-input"
           onChange={ onChange }
           name={ name }
+          id="storyline-input"
         />
       </label>
     );
