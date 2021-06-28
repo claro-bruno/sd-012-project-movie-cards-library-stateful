cb91a7883357f9353d329af511d4b4ab46ff57d6
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-28 14:34:08 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,22 @@
 // implement MovieLibrary component here
+
+import React from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
