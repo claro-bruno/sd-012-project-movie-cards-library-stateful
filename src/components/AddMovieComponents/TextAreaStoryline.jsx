7effe56739fc9f07fe83a1973a79ef41602655c5
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-28 20:17:20 -0300

    espaça elementos do componente TextAreaStoryline para melhorar identaçao - Requisito 11

--- src/components/AddMovieComponents/TextAreaStoryline.jsx
+++ src/components/AddMovieComponents/TextAreaStoryline.jsx
@@ -5,17 +5,21 @@ class TextAreaStoryline extends Component {
   render() {
     const { onChange, value } = this.props;
     return (
+
       <label
         htmlFor="add-storyline"
         data-testid="storyline-input-label"
       >
+
         Sinopse
+
         <textarea
           name="storyline"
           value={ value }
           data-testid="storyline-input"
           onChange={ onChange }
         />
+
       </label>
     );
   }
