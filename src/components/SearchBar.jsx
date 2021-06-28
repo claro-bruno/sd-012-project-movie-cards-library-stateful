a9439f232978361756396087578c92f25209c1cb
Renata Nunes <renatanunes231@yahoo.com.br>
2021-06-28 15:58:09 -0300

    Renderizando Form dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    return ();
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly, 
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form"></form>
+    );
   }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
 }
 
 export default SearchBar;
