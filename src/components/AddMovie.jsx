624933f7e1bfa51985caf1221c28f1cf9166ae90
David Azevedo <zevid.dev@outlook.com>
2021-06-28 17:00:08 -0300

    Create input type text and label subtitulo in AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,12 +14,16 @@ class AddMovie extends React.Component {
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="" data-testid="title-input-label">
           Título
           <input data-testid="title-input" type="text" value={title}/>
+        </label>
+        <label htmlFor="" data-testid="subtitle-input-label">
+          Subtítulo
+          <input data-testid="subtitle-input" type="text" value={subtitle}/>
         </label>
       </form>
     )
