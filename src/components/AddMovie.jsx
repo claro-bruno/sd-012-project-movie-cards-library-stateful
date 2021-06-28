660681de60fc4d44711ddd70e7b0e215920ea43f
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-06-28 18:47:34 -0300

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,33 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const initialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = { ...initialState };
+  }
+
+  render() {
+    return (
+      <div>
+        oi
+      </div>
+    );
+  }
+}
+
+AddMovie.porpTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
