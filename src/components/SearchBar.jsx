b08b68fad017f6bac27bd57baa0097b59a0a220f
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-06-28 15:13:50 -0300

    Adiciona requisito 1 - Criar component SearchBar que recebe props especificas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
 // implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+
+};
+
+export default SearchBar;
