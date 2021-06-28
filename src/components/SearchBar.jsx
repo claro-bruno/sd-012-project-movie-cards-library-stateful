b3d49180b8f284c702139abea98f080b67c50c86
RyanLaiber <ryanlaiber@gmail.com>
2021-06-28 15:35:13 -0300

    adiciona requisito 3 - renderiza input dentro do froms de SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,10 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-
+        <label data-testid="text-input-label">
+          Inclui o texto:
+          <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input"/>
+        </label>
       </form>
     )
   }
