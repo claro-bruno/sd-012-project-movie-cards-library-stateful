26af1678a497d0e84826ec3c4580b9341def6c8d
David Azevedo <zevid.dev@outlook.com>
2021-06-28 17:30:29 -0300

    Correction eslint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -10,35 +10,41 @@ class AddMovie extends React.Component {
       storyline: '',
       rating: 0,
       genre: 'action',
-    }
+    };
   }
 
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre, onClick } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="" data-testid="title-input-label">
+        <label htmlFor="title-input" data-testid="title-input-label">
           Título
-          <input data-testid="title-input" type="text" name="title" onChange={title} value={title}/>
+          <input
+            data-testid="title-input"
+            type="text"
+            name="title"
+            onChange={ title }
+            value={ title }
+          />
         </label>
-        <label htmlFor="" data-testid="subtitle-input-label">
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
           Subtítulo
-          <input data-testid="subtitle-input" type="text" value={subtitle}/>
+          <input data-testid="subtitle-input" type="text" value={ subtitle } />
         </label>
-        <label htmlFor="" data-testid="image-input-label">
+        <label htmlFor="image-input" data-testid="image-input-label">
           Imagem
-          <input data-testid="image-input" type="text" value={imagePath}/>
+          <input data-testid="image-input" type="text" value={ imagePath } />
         </label>
-        <label htmlFor="" data-testid="storyline-input-label">
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
           Sinopse
-          <textarea data-testid="storyline-input" type="text" value={storyline}/>
+          <textarea data-testid="storyline-input" type="text" value={ storyline } />
         </label>
-        <label htmlFor="" data-testid="rating-input-label">
+        <label htmlFor="rating-input" data-testid="rating-input-label">
           Avaliação
-          <input data-testid="rating-input" type="number" value={rating}/>
+          <input data-testid="rating-input" type="number" value={ rating } />
         </label>
-        <label data-testid="genre-input-label">
-        Gênero
+        <label htmlFor="genre-input" data-testid="genre-input-label">
+          Gênero
           <select
             data-testid="genre-input"
             value={ genre }
@@ -48,13 +54,13 @@ class AddMovie extends React.Component {
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
           </select>
-          </label>
-          <button data-testid="send-button" onClick={onClick}>Adicionar Filme</button>
+        </label>
+        <button data-testid="send-button" type="submit" onClick={ onClick }>
+          Adicionar filme
+        </button>
       </form>
-    )
+    );
   }
 }
-
-
 
 export default AddMovie;
