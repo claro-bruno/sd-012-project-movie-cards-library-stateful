15d113065216307d5a0f1513da6f5685166ff225
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 15:05:55 -0300

    Refactoring `App` using class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
