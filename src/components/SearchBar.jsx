e64f109b87a37e1f6e86fa7c98193676f8a52d93
gscuri <gscuri6@hotmail.com>
2021-06-28 18:01:42 -0300

    fazendo exercicio 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div> </div>
+    );
+  }
+}
+
+export default SearchBar;
