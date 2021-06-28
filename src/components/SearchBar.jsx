b4ad080e3636ad582c4af898f52772b8e4e02610
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-06-28 15:06:24 -0300

    Requisito 1 e 2 sem lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,18 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SeachBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+
+    return (
+      <div>
+        <form data-testid="search-bar-form" />
+        <input type="text" data-testid="text-input-label" value={ searchText } onChange={ onSearchTextChange } />
+        <form />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
