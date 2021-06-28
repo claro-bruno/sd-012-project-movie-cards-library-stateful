ea434330d29d1e8a1fcf75042a371ff31773ebf0
Guilherme Dias <gmd.delta@gmail.com>
2021-06-28 19:14:17 -0300

    [PT-BR] Resolvido requisito 7.[EN] Solved 7th requirement.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,25 +2,25 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     subtitle: '',
-  //     title: '',
-  //     imagePath: '',
-  //     storyline: '',
-  //     rating: 0,
-  //     genre: 'action',
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
 
   render() {
-    // const { title, subtitle, storyline, imagePath, rating } = this.state;
-    // const { onClick } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = this.state;
+    const { onClick } = this.props;
     return (
-      <section>
-        Aqui teste do addmovie
-      </section>
+      <form data-testid="add-movie-form" action="">
+        7 Requisito
+      </form>
     );
   }
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,18 +3,24 @@ import React, { Component } from 'react';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor() {
+    super();
+  }
+
+  onClick = () => console.log('aqui');
+
   render() {
-    const { movies } = this.props;
+    const { movies, onClick } = this.props;
     return (
       <div>
         <br />
         {/* <h2> My awesome movie library </h2> */}
         <SearchBar />
         <MovieList movies={ movies } />
-        {/* <AddMovie /> */}
+        <AddMovie onClick={ onClick } />
       </div>
     );
   }
