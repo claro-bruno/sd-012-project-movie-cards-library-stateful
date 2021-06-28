0ab06a4964de7d8c1d1ae1bafcdffc20c46922a4
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-06-28 16:49:29 -0300

    Requisito 3 e refatoraçao dos requisitos 1 e 2: Renderiza um input do tipo texto dentro do formulario e conserta as props usando destructuring

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,39 @@
 // implement SearchBar component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
+
     return (
       <section>
-        {this.searchText}
-        {this.onSearchTextChange}
-        {this.bookmarkedOnly}
-        {this.onBookmarkedChange}
-        {this.selectedGenre}
-        {this.onSelectedGenreChange}
-        <form data-testid="search-bar-form" />
+        <form data-testid="search-bar-form">
+          <label
+            data-testid="text-input-label"
+            htmlFor="input-text"
+          >
+            Inclui o texto
+            <input
+              id="input-text"
+              type="text"
+              data-testid="text-input"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+        </form>
       </section>
 
     );
   }
 }
+
+SearchBar.propTypes = {
+
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+
+};
 
 export default SearchBar;
