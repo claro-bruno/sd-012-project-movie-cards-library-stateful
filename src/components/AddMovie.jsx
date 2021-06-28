0728c7512a814cb4ab322d03d2c6f777fd1b67ff
Adryan Rosa <adryanrosa374@gmail.com>
2021-06-28 16:01:34 -0300

    Cria componente AddMovie e adiciona form com input title nele

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
