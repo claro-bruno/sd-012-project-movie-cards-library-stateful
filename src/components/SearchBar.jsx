fba999676339fcddfa3f08571ef219e20d6e05ba
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-06-28 18:11:47 -0300

    adiciona ate o req 3 para teste

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    // const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,  selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          <input id="text-input" type="text" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
