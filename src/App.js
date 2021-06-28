98e51253b22e68ca7623f2d47b17a5f10f0e6f5e
Marcela Silva <marcela.ops@hotmail.br>
2021-06-28 20:11:32 -0300

    requisitos 15 - criando componente MovieLibrary

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 // import AddMovie from './components/AddMovie';
+// import MovieLibrary from './components/MovieLibrary';
+// import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <AddMovie onClick={ (parametro) => console.log('oi', parametro) } /> */}
+      {/* <MovieLibrary movies={ movies } /> */}
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,30 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+// const INITIAL_STATE = {
+//   searchText: '',
+//   bookmarkedOnly: false,
+//   selectedGenre: '',
+//   movies: '',
+// };
+
+export default class MovieLibrary extends Component {
+  render() {
+    // const { movies } = this.props;
+    return (
+      <div>
+        {/* { movies } */}
+        <SearchBar />
+        {/* <MovieList /> */}
+        <AddMovie /* onClick={ } */ />
+      </div>
+    );
+  }
+}
+
+// MovieLibrary.propTypes = {
+//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+// };
