7bb568ae8c7863a171b783b458cca74c17198837
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-28 16:59:38 -0300

    Resolve os requisitos 3 a 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 // implement SearchBar component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -13,13 +14,56 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form action="" data-testid="search-bar-form">
-        <label>
-          Search:
-          <input type="text" name="" />
+        <label data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            data-testid="text-input"
+            name=""
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
+        </label>
+        <label htmlFor="" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name=""
+            data-testid="checkbox-input"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          />
+        </label>
+        <label htmlFor="" data-testid="select-input-label">
+          Filtrar por gênero:
+          <select
+            name=""
+            data-testid="select-input"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+          >
+            <option data-testid="select-option" value="">
+              Todos
+            </option>
+            <option data-testid="select-option" value="action">
+              Ação
+            </option>
+            <option data-testid="select-option" value="comedy">
+              Comédia
+            </option>
+            <option data-testid="select-option" value="thriller">
+              Suspense
+            </option>
+          </select>
         </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  selectedGenre: PropTypes.string,
+};
 
 export default SearchBar;
