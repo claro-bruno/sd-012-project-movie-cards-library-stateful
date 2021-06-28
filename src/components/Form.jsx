d751ee2a0137181558c3819b1be62463e9d8e230
Álvaro Palma <palma.alvaro@outlook.com>
2021-06-28 19:25:47 -0300

    Cria componentes SearchBar e Form

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    <div>
+      <label data-testid="text-input-label">Inclui o texto:</label>
+      <input type="text"></input>
+    </div>
+  }
+}
+
+export default Form;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
-// implement SearchBar component here
+import React from 'react';
+import Form from './Form';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div
+      searchText=""
+      onSearchTextChange={}
+      bookmarkedOnly={}
+      onBookmarkedChange={}
+      selectedGenre=""
+      onSelectedGenreChange={}
+      >
+        <Form data-testid="search-bar-form" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
