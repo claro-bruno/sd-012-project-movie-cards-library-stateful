8288f55d08d316954a8d0452b829f09786c62c32
julio11lp <julio11lp@hotmail.com>
2021-06-28 18:57:41 -0300

    Criteri 3 aprovado no lint e test

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,32 @@
 // implement SearchBar component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    // const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
+    const { searchText, onSearchTextChange /* bookmarkedOnly */ } = this.props;
     // const { selectedGenre, onSelectedGenreChange, onBookmarkedChange } = this.props;
 
     return (
       <form data-testid="search-bar-form">
-        <p>To aqui</p>
+        <label data-testid="text-input-label" htmlFor="inputTxt">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            id="inputTxt"
+          />
+        </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
