20ccd9735867a30de1534567ae33df3ab52af116
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-06-28 19:51:56 -0300

    Requisito 12 - Renderiza input do tipo number dentro do formulario AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,25 +1,105 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       subtitle: '',
+//       title: '',
+//       imagePath: '',
+//       storyline: '',
+//       rating: 0,
+//       genre: 'action',
+//     };
+
+  //     this.handleChange = this.handleChange.bind(this);
+  //     // this.handleSubmit = this.handleSubmit.bind(this);
+  //   }
+
+  //   handleChange({ target }) {
+  //     const { name, value } = target;
+  //     this.setState({
+  //       [name]: value,
+  //     });
+  //   }
+
+  renderTitle() {
+    return (
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          data-testid="title-input"
+          id="title-input"
+        //   value={ title }
+        //   onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderSubtitle() {
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+        Subtítulo
+        <input
+          data-testid="subtitle-input"
+          id="subtitle-input"
+          // value={ subtitle }
+          // onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderImage() {
+    return (
+      <label data-testid="image-input-label" htmlFor="image-input">
+        Imagem
+        <input
+          data-testid="image-input"
+          id="image-input"
+        />
+      </label>
+    );
+  }
+
+  renderStoryline() {
+    return (
+      <label data-testid="storyline-input-label" htmlFor="textarea-input">
+        Sinopse
+        <textarea
+          data-testid="storyline-input"
+          id="textarea-input"
+        />
+      </label>
+    );
+  }
+
+  renderRating() {
+    return (
+      <label data-testid="rating-input-label" htmlFor="number-input">
+        Avaliação
+        <input
+          type="number"
+          data-testid="rating-input"
+          id="number-input"
+          defaultValue="0"
+          // value={ rating }
+          // onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
   render() {
+    // const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title-input">
-          Título
-          <input data-testid="title-input" id="title-input" />
-        </label>
-        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
-          Subtítulo
-          <input data-testid="subtitle-input" id="subtitle-input" />
-        </label>
-        <label data-testid="image-input-label" htmlFor="image-input">
-          Imagem
-          <input data-testid="image-input" id="image-input" />
-        </label>
-        <label data-testid="storyline-input-label" htmlFor="textarea-input">
-          Sinopse
-          <textarea data-testid="storyline-input" id="textarea-input" />
-        </label>
+        { this.renderTitle() }
+        { this.renderSubtitle() }
+        { this.renderImage() }
+        { this.renderStoryline() }
+        { this.renderRating() }
       </form>
     );
   }
