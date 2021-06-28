8c5a0446ff919faa44cd3de5fcb1de1e86ab402e
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-06-28 15:53:44 -0300

    Requisito 1 ao 5 corrigidos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,11 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+    render(){
+        const {onClick, subtitle, title, imagePath, storyline, rating, genre}
+        return(
+
+        );
+    }
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,38 @@
 // implement SearchBar component here
 import React from 'react';
 
-class SeachBar extends React.Component {
+class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
       <div>
         <form data-testid="search-bar-form" />
-        <input type="text" data-testid="text-input-label" value={ searchText } onChange={ onSearchTextChange } />
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto
+          <input
+            type="text"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
         <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
           Mostrar somente favoritos
-          <input type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } data-testid="checkbox-input" />
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
         </label>
         <label data-testid="select-input-label" htmlFor="select-input">
           Filtrar por gênero
-          <select data-testid="select-input-label" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
             <option value="" data-testid="select-option">Todos</option>
             <option value="action" data-testid="select-option">Ação</option>
             <option value="comedy" data-testid="select-option">Comédia</option>
