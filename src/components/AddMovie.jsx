3a1954ee0ec395ee467b2f33d7437955facec68b
Gabriel Viana <g.viana87@hotmail.com>
2021-06-28 17:01:38 -0300

    Req 6 - criado componente AddMovie renderizando e estruturado com titulo, subtitulo, imagem, sinopse avaliaçao e genero

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,14 +12,56 @@ class AddMovie extends Component {
       storyline: '',
       rating: 0,
       genre: 'action',
-    }
+    };
   }
 
 
   render() {
     return(
-      
-    )
+      <form data-testid="add-movie-form">
+        <label htmlFor="">
+          Titulo
+          <input
+            type="text"
+            name="title"
+          />
+        </label>
+        <label htmlFor="">
+          Subtitulo
+          <input
+            type="text"
+            name="subtitle"
+          />
+        </label>
+        <label htmlFor="">
+          Imagem
+          <input
+            type="text"
+            name="imagePath"
+          />
+        </label>
+        <label htmlFor="">
+          Sinopse
+          <textarea
+            name="storyline"
+          />
+        </label>
+        <label htmlFor="">
+          Avaliação
+          <input
+            type="number"
+            name="rating"
+          />
+        </label>
+        <label htmlFor="">
+          Gênero
+          <input
+            type="text"
+            name="genre"
+          />
+        </label>
+      </form>
+    );
   }
 }
 
