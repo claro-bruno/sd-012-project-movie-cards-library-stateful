31a25f32c7211df32331c11761172e0acdffcfa3
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-06-28 14:16:08 -0300

    Requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,22 +3,29 @@ import PropTypes from 'prop-types';
 
 export default class SearchBar extends Component {
   render() {
-    return (
-      <div>
-        <form data-testid="search-bar-form" />
-      </div>
+    const { searchText, onSearchTextChange } = this.props;
 
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="text">
+          Inclui o texto
+          <input
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            type="text"
+          />
+        </label>
+      </form>
     );
   }
 }
 
 SearchBar.propTypes = {
-  searchBarProps: PropTypes.shape({
-    searchText: PropTypes.string,
-    selectedGenre: PropTypes.string,
-    bookmarkedOnly: PropTypes.bool,
-    onSearchTextChange: PropTypes.func,
-    onBookmarkedChange: PropTypes.func,
-    onSelectedGenreChange: PropTypes.func,
-  }).isRequired,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
