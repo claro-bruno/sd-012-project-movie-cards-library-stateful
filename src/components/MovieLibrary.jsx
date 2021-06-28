cdadeea18f30d8b67f6e67797c9409eccdabb574
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-28 19:05:34 -0300

    Requisitos 15 e 16 - Cria Componente MovieLibrary e inicializa seus estados

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,25 +1,42 @@
 // implement MovieLibrary component here
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+    const { movies } = this.props;
 
-// export default MovieLibrary;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+
+export default MovieLibrary;
