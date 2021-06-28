a0b6b3ef0a5277942d70d5c83cc7fd87453a4efc
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-06-28 16:09:57 -0300

    Adiciona requisito 6 e 7 - Cria componente AddMovie e renderiza o um forms no mesmo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form action="" data-testid="add-movie-form">
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
