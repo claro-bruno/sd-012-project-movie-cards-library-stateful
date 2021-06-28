79af1d9179f394a1f7377fe20c30645d67792756
Guilherme Dias <gmd.delta@gmail.com>
2021-06-28 14:52:39 -0300

    [PT-BR]Resolvido o primeiro requisito.[EN] Solved 1st requirement.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,33 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+// import MovieList from './MovieList';
+// import AddMovie from './AddMovie';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange = () => {}
+
+  onBookmarkedChange = () => {}
+
+  onSelectedGenreChange = () => {}
+
+  render() {
+    return (
+      <div>
+        <h2></h2>
+
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
