915f3e8f757b3b856b3b66fe73a21ab8b6f07ea5
matheus oliveira de proenca <mproenca.marketing@gmail.com>
2021-06-28 16:47:54 -0300

    adiciona propTypes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form data-testid="search-bar-form">          
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto:
+          <input
+            id="text-input"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
