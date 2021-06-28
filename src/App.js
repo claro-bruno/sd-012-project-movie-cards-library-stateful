4e0ace1cda5a4593a57d97a845f6177fd5c14549
Erick Silva <erick.silva6653@gmail.com>
2021-06-28 12:31:37 -0300

    renderizando MovieList no App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import data from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ data } />
     </div>
   );
 }
