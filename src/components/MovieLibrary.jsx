b3c3be33677403b4cfd1e54a050e4822815fbac0
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-28 14:36:46 -0300

    iniciando projeto, criando movieLibrary component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,9 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends  React.Component {
+  render() {
+    return;
+  }
+}
+
+export default MovieLibrary;
