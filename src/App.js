21603b1cf1fa6cf378da55888d3ddd81ad41e54c
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-28 16:14:12 -0300

    Importa movies e passa como props para <MovieList />

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import MovieList from './components/MovieList';
 import AddMovie from './components/AddMovie';
+import movies from './data';
 import './App.css';
 
 class App extends Component {
@@ -21,7 +22,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <SearchBar />
-        <MovieList />
+        <MovieList movies={ movies } />
         <AddMovie onClick={ this.handleChange } />
       </div>
     );
