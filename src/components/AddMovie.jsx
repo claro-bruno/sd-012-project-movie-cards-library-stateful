228e94673b08c61599d9689a341137f1086e0d75
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-28 16:16:07 -0300

    cria AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,39 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyLine, rating, genre } = this.state;
+    return (
+      <form>
+        Bora passar no lint com:
+        { onClick }
+        { subtitle }
+        { title }
+        { imagePath }
+        { storyLine }
+        { rating }
+        { genre }
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
