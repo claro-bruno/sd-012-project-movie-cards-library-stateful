774e4c44fa33495c3fd7b77096a990f0b59502e4
Sarah <sarahgsb@hotmail.com>
2021-06-28 20:07:13 -0300

    App de função para classe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
