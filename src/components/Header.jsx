684db7e75cf229633c12f5a547f627b5a5b28005
rachidb5 <jordan.rachid@gmail.com>
2021-06-28 16:27:28 -0300

    Insere Barra de pesquisa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class Header extends React.Component {
   render() {
     return (
       <header className="movie-card-header">
-        <h1 className="page-title">Movie Cards Libraria</h1>
+        <h1 className="page-title">Movie Cards Library</h1>
+        <SearchBar />
       </header>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <input
+        className="form-control col-3 ml-5"
+        type="text"
+        placeholder="O Que Você Procura?"
+      />
+    );
+  }
+}
+
+export default SearchBar;
