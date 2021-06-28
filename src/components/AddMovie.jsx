7b5aedca35b224ca658f2e9ad26592b4b5981495
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-28 17:59:03 -0300

    Requisitos 6 a 13 - Cria Componente TextInput e componetiza AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import TextInput from './TextInput';
 
 class AddMovie extends Component {
   constructor(props) {
@@ -30,44 +31,35 @@ class AddMovie extends Component {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
-          Título
-          <input
-            type="text"
-            data-testid="title-input"
-            name="title"
-            value={ title }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            type="text"
-            data-testid="subtitle-input"
-            name="subtitle"
-            value={ subtitle }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="image-input" data-testid="image-input-label">
-          Imagem
-          <input
-            type="text"
-            data-testid="image-input"
-            name="imagePath"
-            value={ imagePath }
-            onChange={ this.handleChange }
-          />
-        </label>
+        <TextInput
+          id="title"
+          label="Título"
+          name="title"
+          value={title}
+          handleChange={this.handleChange}
+        />
+        <TextInput
+          id="subtitle"
+          label="Subtítulo"
+          name="subtitle"
+          value={subtitle}
+          handleChange={this.handleChange}
+        />
+        <TextInput
+          id="image"
+          label="Imagem"
+          name="imagePath"
+          value={imagePath}
+          handleChange={this.handleChange}
+        />
         <label htmlFor="storyline-input" data-testid="storyline-input-label">
           Sinopse
           <textarea
             type="text"
             data-testid="storyline-input"
             name="storyline"
-            value={ storyline }
-            onChange={ this.handleChange }
+            value={storyline}
+            onChange={this.handleChange}
           />
         </label>
         <label htmlFor="rating-input" data-testid="rating-input-label">
@@ -76,8 +68,8 @@ class AddMovie extends Component {
             type="number"
             data-testid="rating-input"
             name="rating"
-            value={ rating }
-            onChange={ this.handleChange }
+            value={rating}
+            onChange={this.handleChange}
           />
         </label>
         <label htmlFor="genre-input" data-testid="genre-input-label">
@@ -85,12 +77,18 @@ class AddMovie extends Component {
           <select
             data-testid="genre-input"
             name="genre"
-            value={ genre }
-            onChange={ this.handleChange }
+            value={genre}
+            onChange={this.handleChange}
           >
-            <option data-testid="genre-option" value="action" selected>Ação</option>
-            <option data-testid="genre-option" value="comedy">Comédia</option>
-            <option data-testid="genre-option" value="thriller">Suspense</option>
+            <option data-testid="genre-option" value="action" selected>
+              Ação
+            </option>
+            <option data-testid="genre-option" value="comedy">
+              Comédia
+            </option>
+            <option data-testid="genre-option" value="thriller">
+              Suspense
+            </option>
           </select>
         </label>
       </form>
