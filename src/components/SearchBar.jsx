0c9b49b917241ae38f13960e8b377a4c6acbca3a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-28 14:58:15 -0300

    requisito 1 - Crie um componente chamado SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form itemID="search-bar-form">
+        <input />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
