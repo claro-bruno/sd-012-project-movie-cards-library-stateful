e148761cac04030d860d3daf4fc94bbdcec0913a
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-28 19:06:00 -0300

    edita Componente AddMovie de forma que passe no requisito 07

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,71 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Input from './generics/Input';
-import Select from './generics/Select';
+// import Select from './generics/Select';
 
 class AddMovie extends Component {
   constructor() {
     super();
     this.state = {
-      subtitle: '',
       title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+    //   subtitle: '',
+    //   imagePath: '',
+    //   storyline: '',
+    //   rating: 0,
+    //   genre: 'action',
     };
   }
 
   render() {
     const { onClick } = this.props;
+
+    const {
+      title,
+    //   subtitle,
+    //   imagePath,
+    //   storyline,
+    //   rating,
+    //   genre,
+    } = this.state;
     return (
-      <form
-        className="form-addMovie"
-        data-testid="add-movie-form"
-      >
-        <Input
-          id="add-subtitle"
-          textLabel="Subtítulo : "
-          name="subtitle"
-        />
-
-        <Input
-          id="add-title"
-          textLabel="Título : "
-          name="title"
-        />
-
-        <Input
-          id="add-imagePath"
-          textLabel="Caminho da Imagem : "
-          name="imagePath"
-        />
-
-        <Input
-          id="add-storyline"
-          textLabel="Sinopse : "
-          name="storyline"
-        />
-
-        <Input
-          id="add-rating"
-          textLabel="Avaliação : "
-          name="rating"
-        />
-
-        <Select
-          id="add-genre"
-          textLabel="Gênero : "
-          name="genre"
-        />
-
+      <form className="form-addMovie" data-testid="add-movie-form">
+        <Input id="add-title" textLabel="Título : " name="title" value={ title } />
+        <button type="submit" onClick={ onClick }>Adicionar</button>
       </form>
     );
   }
 }
 
 AddMovie.propTypes = {
-  onclick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
