df0ed60658d5d8b6db573b8789efe84b99c19901
cristianocsl <cslcristiano@gmail.com>
2021-06-28 18:40:37 -0300

    Requisitos 6 e 7:  Crie um componente chamado <AddMovie /> e Renderize um formulário dentro de <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,18 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        test
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func,
+}.isRequired;
+
+export default AddMovie;
