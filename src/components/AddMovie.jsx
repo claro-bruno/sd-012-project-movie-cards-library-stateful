584ef1f91355bffb3d5482e598e4419a1ca84f8f
Pedro Gonçalves <peg.gep27@gmail.com>
2021-06-28 17:34:26 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Input from './Input'
 
 class AddMovie extends React.Component {
   render() {
     const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-        
+        <Input
+        dataTestidLabel="title-input-label"
+        htmlFor="text-addmovie"
+        labelText="Título"
+        id="text-addmovie"
+        type="text"
+        name="title"
+        dataTestidInput="title-input"
+      />
       </form>
     );
   }
