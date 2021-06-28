97fde5b51d57876f40e37b1febdffa86e6fa4ade
Renata Nunes <renatanunes231@yahoo.com.br>
2021-06-28 17:20:38 -0300

    Renderizando Label e Input para Title no AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,12 +11,31 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.onChangeInputTitle = this.onChangeTitle.bind(this);
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
     return (
-      <form data-testid="add-movie-form"></form>
+      <form data-testid="add-movie-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            id="text-input"
+            type="text"
+            value={ this.state.title }
+            onChange={ this.onChangeTitle }
+          />
+        </label>
+      </form>
     );
   }
 }
