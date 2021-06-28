cddab6d0b993b1d4738fca2df41fb100696e4f22
David Azevedo <zevid.dev@outlook.com>
2021-06-28 16:47:10 -0300

    Creat form tag, add states AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
   render() {
-    return ()
+    return (
+      <form data-testeid="add-movie-form">
+        <input type="text" />
+      </form>
+    )
   }
 }
+
+
 
 export default AddMovie;
