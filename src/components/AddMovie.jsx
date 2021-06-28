c10c5e2147f7d4f63690fb8ca33eb1554938e980
AlyssonSene <alysson_silva@outlook.com>
2021-06-28 19:02:04 -0300

    requisitos 6,7 e 8 implementados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,18 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+export default class AddMovie extends Component {
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+          />
+        </label>
+      </form>
+    );
+  }
+}
