9a75c1615f28a0512b7643aaca1ca3b02f110767
David Azevedo <zevid.dev@outlook.com>
2021-06-28 17:16:02 -0300

    Create select and three options in AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,12 +14,12 @@ class AddMovie extends React.Component {
   }
 
   render() {
-    const { title, subtitle, imagePath, storyline } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="" data-testid="title-input-label">
           Título
-          <input data-testid="title-input" type="text" value={title}/>
+          <input data-testid="title-input" type="text" name="title" onChange={title} value={title}/>
         </label>
         <label htmlFor="" data-testid="subtitle-input-label">
           Subtítulo
@@ -33,6 +33,22 @@ class AddMovie extends React.Component {
           Sinopse
           <textarea data-testid="storyline-input" type="text" value={storyline}/>
         </label>
+        <label htmlFor="" data-testid="rating-input-label">
+          Avaliação
+          <input data-testid="rating-input" type="number" value={rating}/>
+        </label>
+        <label data-testid="genre-input-label">
+        Gênero
+          <select
+            data-testid="genre-input"
+            value={ genre }
+            onChange={ genre }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+          </label>
       </form>
     )
   }
