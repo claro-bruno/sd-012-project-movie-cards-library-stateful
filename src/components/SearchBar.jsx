240e081cb7cdac7d9f4074e74ecd21fc48b29513
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-06-28 16:26:09 -0300

    searchbar form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
 // implement SearchBar component here
+
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super()
+
+
+  }
+  render() {
+    return <>
+    <form data-testid="search-bar-form">
+
+    </form>
+    </>
+  }
+}
+
+export default SearchBar;
