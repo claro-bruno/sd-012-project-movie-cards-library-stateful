2f5c3fcd76dcb6ca323d858d1d7d73fa0f049d0b
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-06-28 15:33:46 -0300

    Renderiza um input do tipo texto dentro de SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -13,7 +13,7 @@ class MovieLibrary extends Component {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar searchText="xablau" onSearchTextChange="xablau" />
         <MovieList movies={this.props.movies} />
         <AddMovie />
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    // const { filter } = this.props;
-    // const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = filter;
+    const { filter } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      // bookmarkedOnly,
+      // onBookmarkedChange,
+      // selectedGenre,
+      // onSelectedGenreChange
+    } = filter;
     return (
       <form data-testid="search-bar-form">
-        <input type="text" />
+        [/** Resolvi um problema do link com o htmlFor consultado no link:
+        https://stackoverflow.com/questions/54446655/eslint-rule-for-label */]
+        <label htmlFor="temp-id" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            id="temp-id"
+          />
+        </label>
       </form>
     );
   }
 }
 
-// SearchBar.propTypes = {
-//   filter: PropTypes.shape({
-//     searchText: PropTypes.string.isRequired,
-//     onSearchTextChange: PropTypes.callback.isRequired,
-//     bookmarkedOnly: PropTypes.boolean.isRequired,
-//     onBookmarkedChange: PropTypes.callback.isRequired,
-//     selectedGenre: PropTypes.string.isRequired,
-//     onSelectedGenreChange: PropTypes.callback.isRequired,
-//   }),
-// };
+SearchBar.propTypes = {
+  filter: PropTypes.shape({
+    searchText: PropTypes.string.isRequired,
+    onSearchTextChange: PropTypes.func.isRequired,
+    // bookmarkedOnly: PropTypes.boolean.isRequired,
+    // onBookmarkedChange: PropTypes.callback.isRequired,
+    // selectedGenre: PropTypes.string.isRequired,
+    // onSelectedGenreChange: PropTypes.callback.isRequired,
+  }),
+};
 
-// SearchBar.defaultProps = {
-//   movie: { },
-// };
+SearchBar.defaultProps = {
+  filter: { },
+};
 
 export default SearchBar;
