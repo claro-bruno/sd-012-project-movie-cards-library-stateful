24138d674e0465c8bc349e808a0e04a114f8db0b
RyanLaiber <ryanlaiber@gmail.com>
2021-06-28 16:20:22 -0300

    adiciona requisitos 6 e 7, cria componente AddMovie onde sera contido um formulario utilizado para adicao de filmes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle:'',
+      title:'',
+      imagePath:'',
+      storyline:'',
+      rating:0,
+      genre:'action',
+    };
+  }
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
