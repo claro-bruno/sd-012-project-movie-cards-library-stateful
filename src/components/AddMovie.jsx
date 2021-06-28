17bc27e11baf09b93b56714a7aa2c37bdb2598a9
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-28 18:02:42 -0300

    Requisito 10 resolvido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,7 +25,7 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick } = this.props;
-    const { title, subtitle } = this.state;
+    const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title-input">
@@ -43,6 +43,15 @@ class AddMovie extends React.Component {
             name="subtitle"
             value={ subtitle }
             data-testid="subtitle-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="image-input-label" htmlFor="subtitle-input">
+          Imagem
+          <input
+            name="imagePath"
+            value={ imagePath }
+            data-testid="image-input"
             onChange={ this.handleChange }
           />
         </label>
