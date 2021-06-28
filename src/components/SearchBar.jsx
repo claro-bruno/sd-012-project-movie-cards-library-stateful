2267cdec09bca732419e922765e34b1d7f370870
raphael <raphael.ph2ll@email.com>
2021-06-28 15:54:54 -0300

    Resolve ex01/02 - SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,26 @@
-// implement SearchBar component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    // const { item } = this.props;
+    return (
+      <forms data-testid="search-bar-form">
+        alo
+      </forms>
+    );
+  }
+}
+// SearchBar.PropTypes = {
+//  item: PropTypes.exact({
+//    searchText: PropTypes.string,
+//  onSearchTextChange: PropTypes.func,
+//    bookmarkedOnly: PropTypes.bol,
+//    onBookmarkedChange: PropTypes.func,
+//    selectedGenre: PropTypes.string,
+//    onSelectedGenre: PropTypes.func,
+//  }),
+// };
+export default SearchBar;
+// Links
+// PropTypes https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
