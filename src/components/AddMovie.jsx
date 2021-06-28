0026d21ad8b96d3633f93bc707d8ea6692ef7e6e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-28 15:43:18 -0300

    requisito 7 - Renderize um formulário dentro de `<AddMovie />`

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,7 +17,7 @@ class AddMovie extends React.Component {
   render() {
     // const { onClick } = this.props;
     return (
-      <form>
+      <form data-testid="add-movie-form">
         <input />
       </form>
     );
