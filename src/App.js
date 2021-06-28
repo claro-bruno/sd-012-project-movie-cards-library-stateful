c7bb97057f07dc732628d5ab00954d8e1e95a5cd
matheustkaczyk <matheus111904@hotmail.com>
2021-06-28 16:43:32 -0300

    requesitos, 1, 2 e 3 parciais

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <div className="App">
-        <Header />
-      </div>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    const { searchText } = this.state;
+    return (
       <div>
-        <SearchBar />
+        <div className="App">
+          <Header />
+        </div>
+        <div>
+          <SearchBar
+            searchText={ searchText }
+            onSearchTextChange={this.handleChange}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-      return(
-          <label>Inclui o texto:
-              <input type='text' data-testid="search-bar-form" />
-          </label>
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="search-bar">
+          Inclui o texto:
+          <input
+            type="text"
+            name="searchText"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type='checkbox'
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkChange }
+          />
+        </label>
+      </form>
     );
   }
 }
