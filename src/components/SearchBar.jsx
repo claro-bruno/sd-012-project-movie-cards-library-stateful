800b2950c3aea3675911db76614f1f9c7469531d
matheustkaczyk <matheus111904@hotmail.com>
2021-06-28 14:51:07 -0300

    correcoes identacao

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-class SearchBar extends React.Component{
-    render() {
-        return(
-            <label>Inclui o texto:
-                <input type='text' data-testid="search-bar-form" />
-            </label>
-        )
-    }
+class SearchBar extends React.Component {
+  render() {
+      return(
+          <label>Inclui o texto:
+              <input type='text' data-testid="search-bar-form" />
+          </label>
+    );
+  }
 }
 
 export default SearchBar;
