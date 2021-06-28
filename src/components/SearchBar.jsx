90846612e27f10b19184c064dd725b52bacb79c7
Nuwanda <morato_dev@protonmail.com>
2021-06-28 14:52:15 -0300

    Implementaçao inicial SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,9 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return <input type="text" />;
+  }
+}
+
+export default SearchBar;
