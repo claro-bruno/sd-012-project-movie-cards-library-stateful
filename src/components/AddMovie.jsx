d6c8c53401e97c5bff197ebd16aae7cdffd1dcfa
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-28 17:03:22 -0300

    cria componente AddMovie adiciona states a ele e inicia sua elementaçao - Requisito 06

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (<p>olá</p>);
+  }
+}
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
