c59bac3bcb5c05d92ae431fa2d6013e0fb07d7ed
Pedro Gonçalves <peg.gep27@gmail.com>
2021-06-28 17:52:09 -0300

    requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import Input from './Input';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.setState = {
+      title: '',
+      subtitle: '',
+    };
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     // const { onClick } = this.props;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <Input
@@ -14,6 +30,15 @@ class AddMovie extends React.Component {
           type="text"
           name="title"
           dataTestidInput="title-input"
+          value={ title }
+        />
+        <Input
+          type="text"
+          labelText="Subtítulo"
+          dataTestidLabel="subtitle-input-label"
+          value={ subtitle }
+          dataTestidInput="subtitle-input"
+          change={ this.handleChange }
         />
       </form>
     );
