ee34bb53d4d623485994d94810bf7eacc9a488c4
Rodrigo <rodrigo.facury14@gmail.com>
2021-06-28 19:06:51 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form>
+        { onClick }
+        <input type="text" id="find-subtitle" />
+        <input type="text" id="find-title" />
+        <input type="text" id="find-image-path" />
+        <input type="text" id="find-story-line" />
+        <input type="text" id="find-rating" />
+        <input type="text" id="find-genre" />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
