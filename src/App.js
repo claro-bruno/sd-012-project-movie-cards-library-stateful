6efb4be7def5dde1f8cba2c52785c3476b694bcb
matheustkaczyk <matheus111904@hotmail.com>
2021-06-28 14:49:21 -0300

    trabalhando requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div>
+      <div className="App">
+        <Header />
+      </div>
+      <div>
+        <SearchBar />
+      </div>
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
 
+class SearchBar extends React.Component{
+    render() {
+        return(
+            <label>Inclui o texto:
+                <input type='text' data-testid="search-bar-form" />
+            </label>
+        )
+    }
+}
+
+export default SearchBar;
