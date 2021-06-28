e3e80ced84f2908b6679fae54d1e3758d33fe1c2
Nuwanda <morato_dev@protonmail.com>
2021-06-28 16:51:05 -0300

    eslint limpo ate o exercicio 5 tudo correto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
   render() {
     const { onSearchTextChange, searchText } = this.props;
     const { onBookmarkedChange, bookmarkedOnly } = this.props;
-    const { onSelectedGenreChange } = this.props;
+    const { onSelectedGenreChange, selectedGenre } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="textInput" data-testid="text-input-label">
@@ -30,13 +30,19 @@ class SearchBar extends React.Component {
             onChange={ onBookmarkedChange }
           />
         </label>
-        <label htmlFor="genreOption">
+        <label htmlFor="genreOption" data-testid="select-input-label">
           Filtrar por gênero
-          <select name="" id="genreOption" data-testid="select-input-label" onChange={}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
+          <select
+            data-testid="select-input"
+            name="genreOption"
+            value={ selectedGenre }
+            id="genreOption"
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
           </select>
         </label>
       </form>
@@ -49,10 +55,13 @@ SearchBar.propTypes = {
   searchText: PropTypes.string,
   onBookmarkedChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string,
 };
 
 SearchBar.defaultProps = {
   searchText: '',
+  selectedGenre: '',
 };
 
 export default SearchBar;
