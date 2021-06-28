2ef282beae147cb6e4e25eaafb745fa11f1734d6
AlyssonSene <alysson_silva@outlook.com>
2021-06-28 19:22:58 -0300

    requisito 10 e 11 implementados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,11 +12,24 @@ export default class AddMovie extends Component {
             type="text"
           />
         </label>
-        <label htmlFor="subtitle-input-label" data-testid="subtitle-input-label">
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input-label">
           Subtítulo
           <input
             data-testid="subtitle-input"
             type="text"
+          />
+        </label>
+        <label data-testid="image-input-label" htmlFor="image-input-label">
+          Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+          />
+        </label>
+        <label data-testid="storyline-input-label" htmlFor="storyline-input-label">
+          Sinopse
+          <textarea
+            data-testid="storyline-input"
           />
         </label>
       </form>
