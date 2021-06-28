f079465eaad31a2f79e921221b9d8726da87a9a5
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-06-28 14:31:11 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        props={ {
+          searchText: 'My Text',
+          onSearchTextChange: () => '',
+          bookmarkedOnly: true,
+          onBookmarkedChange: () => '',
+          selectedGenre: 'action',
+          onSelectedGenreChange: () => '',
+        } }
+      />
     </div>
   );
 }
