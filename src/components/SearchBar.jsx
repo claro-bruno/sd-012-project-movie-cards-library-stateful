10697585d5e74a875857b46edb08e766d3bb9f10
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 15:07:32 -0300

    Requirement 03-> Add label and input to form in `SearchBar`

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,10 @@ class SearchBar extends Component {
   render() {
     return (
       <form data-testid="search-bar-form">
+        <label htmlFor="text-input">
+          Inclui o texto:
+          <input type="text" id="text-input" data-testid="text-input" />
+        </label>
       </form>
     );
   }
