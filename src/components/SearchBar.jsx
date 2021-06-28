1305064fe6e68615902c07fd4e195bb8bb82270f
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 15:06:32 -0300

    Requirement 01 e 02-> Create `SearchBar` and render form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
