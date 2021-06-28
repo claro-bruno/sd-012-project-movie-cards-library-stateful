e14110758a662946797c65fc957bf57078fe6568
Eder Santos <3derms@gmail.com>
2021-06-28 17:16:56 -0300

    Resolve o requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,13 +12,23 @@ class SearchBar extends Component {
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label">
           Inclui o texto:
-        <input 
-        type="text"
-        name="searchText"
-        value={ searchText } 
-        onChange={ onSearchTextChange } 
-        data-testid="text-input"/>
+          <input 
+            type="text"
+            name="searchText"
+            value={ searchText } 
+            onChange={ onSearchTextChange } 
+            data-testid="text-input"/>
         </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input 
+            type="checkbox" 
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        
       </form>
     )
   }
