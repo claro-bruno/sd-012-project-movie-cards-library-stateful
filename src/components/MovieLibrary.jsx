40eb9a30e3a3dd7e3ba094248aff967aec5e78bb
lucasnahas <lucasahas@gmail.com>
2021-06-28 16:41:27 -0300

    inicia a logica de movie library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 
 // import AddMovie from './AddMovie';
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import movies from '../data';
+
+
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.handleInput = this.handleInput.bind(this);
+    this.state = {};
+  }
+
+  handleInput(e) {
+    this.setState = {
+      inputValue: e.target.value,
+    };
   }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        {/* <MovieList movies={this.props.movies} /> */}
+        <SearchBar onChange={this.handleInput}/>
+        <MovieList movies={ movies } />
         {/* <AddMovie /> */}
       </div>
     );
