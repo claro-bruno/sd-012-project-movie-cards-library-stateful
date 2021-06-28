b6d8d0db8c2e78560425dbce26088e4e39bb7905
Eder Santos <3derms@gmail.com>
2021-06-28 20:15:43 -0300

    Resolve os Requisitos 6 e 7 - Cria um componente chamado <AddMovie /> e renderiza um formulário dentro

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
-// implement AddMovie component here
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Consultei o repositório do Wendell Costa para resolver essa parte.
+ * Link: https://github.com/tryber/sd-011-project-movie-cards-library-stateful/pull/174/commits/44b731c7cb8c4d936475d54de1125e003e202a80
+ */
+
+class AddMovie extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    };
+    const { onClick } = this.props
+  }
+  render() {
+    const {
+        subtitle,
+        title,
+        imagePath,
+        storyline,
+        rating,
+        genre
+    } = this.state
+    return <form data-testid="add-movie-form"></form>
+  }
+}
+
+export default AddMovie;
