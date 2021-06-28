d38262e6dad8b40abe66507175911e0eb9e91f47
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-28 14:50:43 -0300

    Requisito 6 - Criado o componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,17 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  render() {
+    const { onClick } = this.props;
+    return (
+      <h1>AddMovie</h1>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
