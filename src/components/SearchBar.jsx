5f7384d442e8b9c5481f3d2b49b90b07372be61c
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-06-28 14:56:25 -0300

    add select by genre in SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -30,6 +30,18 @@ class SearchBar extends React.Component {
               data-testid="checkbox-input"
             />
           </label>
+          <label data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input">
+                <option value="" data-testid="select-option" >Todos</option>
+                <option value="action" data-testid="select-option" >Ação</option>
+                <option value="comedy" data-testid="select-option" >Comédia</option>
+                <option value="thriller" data-testid="select-option" >Suspense</option>
+            </select>
+          </label>
         </form>
     );
   }
