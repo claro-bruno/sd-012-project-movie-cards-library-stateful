ca30915185da5dbb52fbed1e2034812f000d82ea
David Azevedo <zevid.dev@outlook.com>
2021-06-28 16:33:14 -0300

    add isRequired in SearchBar.PropTypes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -43,12 +43,12 @@ class SearchBar extends React.Component {
   }
 }
 SearchBar.propTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
-  bookmarkedOnly: PropTypes.bool,
-  onBookmarkedChange: PropTypes.func,
-  selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.func,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
