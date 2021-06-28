ece18b4f904c19d63b563fad48a52f4eff0f926a
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-06-28 17:57:48 -0300

    Requisito 16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
 export default class MovieLibrary extends Component {
   constructor(props) {
     super(props);
@@ -8,18 +11,29 @@ export default class MovieLibrary extends Component {
     const { movies } = this.props;
 
     this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
       movies,
     };
   }
 
   render() {
+    const { searchText, movies } = this.props;
+
     return (
-      <div />
+      <div>
+        <SearchBar searchText={ searchText } />
+        <MovieList movies={ movies } />
+      </div>
     );
   }
 }
 
 MovieLibrary.propTypes = {
-  movies: PropTypes
-    .arrayOf(PropTypes.oneOfType([PropTypes.string])).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string])),
+};
+
+MovieLibrary.defaultProps = {
+  movies: '',
 };
