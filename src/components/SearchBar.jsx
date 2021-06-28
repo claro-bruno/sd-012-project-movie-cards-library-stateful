d82a8a9e95dfe14d08ad320296b170785a1c0a1e
David Azevedo <zevid.dev@outlook.com>
2021-06-28 15:45:42 -0300

    Create/Render label, input type checkbox in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,13 +7,24 @@ class SearchBar extends React.Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto</label>
-        <input
-          data-testid="text-input"
-          type="text"
-          value={ searchText }
-          onChange={ onSearchTextChange }
-        />
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
       </form>
     );
   }
