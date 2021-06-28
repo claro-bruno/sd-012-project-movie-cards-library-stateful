687dcab814bd632e72ed9eda32f9fb69bd769ea6
Roberval Filho <rslfilho@gmail.com>
2021-06-28 16:29:13 -0300

    Criando component AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <div>
+        AddMovie
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
