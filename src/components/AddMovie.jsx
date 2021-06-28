3fd468af35e2ba6e76ec54a8975e3c61db73bcf5
Genivaldo <cgenivaldo46@gmail.com>
2021-06-28 17:06:21 -0300

    Requisito 06

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,21 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     subtitle: '',
+  //     tittle: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: 'action',
+  //   };
+  // }
+
+  render() {
+    // const { subtitle, title, imagePath, storyline, rating, genre } = this.state
+    return (<div />);
+  }
+}
