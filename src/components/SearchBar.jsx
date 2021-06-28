2d313c800ab67905e1fd5dea4ca90ddb83175162
Euller Braz <euller.braz99@gmail.com>
2021-06-28 15:19:56 -0300

    Requisito 3: Adicionado Input no SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form data-testid="search-bar-form">Vazio</form>
+      <form data-testid="search-bar-form">
+        <label
+          htmlFor="textInclude"
+          data-testid="text-input-label"
+        >
+          Inclui o texto
+          <input
+            type="text"
+            id="textInclude"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
