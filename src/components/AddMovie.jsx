46bb366c10619d9188accf917f06e7cfb1b03b30
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-06-28 15:43:28 -0300

    Requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: "action",
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form
+        data-testid="add-movie-form"
+      />
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}.isRequired;
+
+export default AddMovie;
