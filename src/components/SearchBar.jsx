54bb1a8efa4cabee11ff7855a6a002acbf177d09
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-06-28 15:08:04 -0300

    Cria componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,27 @@
-// implement SearchBar component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    // const { filter } = this.props;
+    // const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = filter;
+    return 'xablau';
+  }
+}
+
+// SearchBar.propTypes = {
+//   filter: PropTypes.shape({
+//     searchText: PropTypes.string.isRequired,
+//     onSearchTextChange: PropTypes.callback.isRequired,
+//     bookmarkedOnly: PropTypes.boolean.isRequired,
+//     onBookmarkedChange: PropTypes.callback.isRequired,
+//     selectedGenre: PropTypes.string.isRequired,
+//     onSelectedGenreChange: PropTypes.callback.isRequired,
+//   }),
+// };
+
+// SearchBar.defaultProps = {
+//   movie: { },
+// };
+
+export default SearchBar;
