22f93cfce0de9cd2f528e87fcd464b0ecfb12c4b
Daniel <danielbped@gmail.com>
2021-06-28 15:28:13 -0300

    Resolve requisito 11, semi-resolve requisitos 8, 9, 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,8 +17,46 @@ class AddMovie extends Component {
 
   render() {
     const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline } = this.state;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="movie-title-input">
+          Título
+          <input
+            data-testid="title-input"
+            id="movie-title-input"
+            type="text"
+            value={ title }
+          />
+        </label>
+        <label htmlFor="movie-subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            data-testid="subtitle-input"
+            id="movie-subtitle-input"
+            value={ subtitle }
+          />
+        </label>
+        <label htmlFor="movie-image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            id="movie-image-input"
+            value={ imagePath }
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor="movie-storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            id="movie-storyline-input"
+            data-testid="storyline-input"
+            cols="30"
+            rows="10"
+          />
+        </label>
+      </form>
     );
   }
 }
