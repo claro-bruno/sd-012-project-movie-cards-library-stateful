4d0a96cedb47f7dd7e9f57225ed28e21c6048a7d
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-28 15:04:20 -0300

    Requisito 13 - Adicionado um select de genre com suas devidas opcoes e atributos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -78,6 +78,19 @@ class AddMovie extends React.Component {
             onChange={ this.handleChange }
           />
         </label>
+        <label htmlFor="genre-input-label" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="genre"
+            value={ genre }
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
