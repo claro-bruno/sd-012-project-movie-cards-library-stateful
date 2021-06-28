f414072dd86aae3bdad776ee8a78e7c2b7d2ec57
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-28 14:34:50 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  reder() {
+    return (
+      <div>
+        <span>First Commit</span>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
