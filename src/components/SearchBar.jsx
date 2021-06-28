a93c0746ed7cabe4f690fe2025956e8f42e901a5
gscuri <gscuri6@hotmail.com>
2021-06-28 18:17:40 -0300

    fazendo exercicio 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <div> </div>
+      <div><form data-testid="search-bar-form" /></div>
     );
   }
 }
