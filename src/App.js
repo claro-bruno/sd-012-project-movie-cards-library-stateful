b5ae6039f1e6e4141c9f0693a6a308ed92ece800
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-06-28 20:56:12 -0300

    create component AddMovie and MovieLibrary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +11,8 @@ class App extends Component {
       <div className="App">
         <Header />
         <SearchBar />
+        <AddMovie />
+        <MovieLibrary />
       </div>
     );
   }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <p>AddMovie</p>
+    );
+  }
+}
+
+export default AddMovie;
