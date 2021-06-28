3a4f1e6d22fb41d373e8fabdff7b5156d191ac5f
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-06-28 15:30:01 -0300

    Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -37,6 +37,31 @@ class SearchBar extends Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label
+          htmlFor="checkbox-input-label"
+          data-testid="select-input-label"
+        >
+          Filtrar por gênero
+          <select
+            type="checkbox"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">
+              Todos
+            </option>
+            <option value="action" data-testid="select-option">
+              Ação
+            </option>
+            <option value="comedy" data-testid="select-option">
+              Comédia
+            </option>
+            <option value="thriller" data-testid="select-option">
+              Suspense
+            </option>
+          </select>
+        </label>
       </form>
     );
   }
