395e6da5f58e8d7c4b65e1b1113e9ef58d5a631e
CamilaDamasio <camiladamasioef@hotmail.com>
2021-06-28 14:41:35 -0300

    Corrige erro no lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        {/* <MovieList movies={ this.props.movies } /> */}
         <AddMovie />
       </div>
     );
