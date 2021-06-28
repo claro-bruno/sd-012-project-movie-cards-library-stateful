5466978ce7da587644a290ec54a6ba76965f49cc
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-06-28 18:56:56 -0300

    Resolve requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,37 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+/* Requirito 06 */
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.stateSet = this.stateSet.bind(this);
+  }
+
+  stateSet() {
+    const { onClick } = this.props;
+    onClick();
+  }
+
+  render() {
+    /* Requisito 7 */
+    return (
+      <div>
+        <form data-testid="add-movie-form" />
+      </div>
+    );
+  }
+}
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
