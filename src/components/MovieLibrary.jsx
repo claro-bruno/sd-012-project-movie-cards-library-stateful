900f0f78665d4cc2ba562b9fe11d497c13ba8d17
David Azevedo <zevid.dev@outlook.com>
2021-06-28 18:05:51 -0300

    Render SearchBar in MovieList

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: '',
+    }
+  }
+  render() {
+    return(
+      <form>
+        <SearchBar />
+      </form>
+    )
+  }
+}
+
+export default MovieLibrary;
