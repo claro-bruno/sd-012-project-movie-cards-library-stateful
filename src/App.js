8498d7dff77220abcf7987437fa0a01ad57cd9b0
pedro21medeiros <pedro21medeiros@gmail.com>
2021-06-28 17:08:13 -0300

    adiciona AppMovie no App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddMovie />
     </div>
   );
 }
