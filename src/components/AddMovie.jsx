56fd552d2f3e66b53f23bb7c3f89862921cb372f
Adriana Biberg <adribiberg@hotmail.com>
2021-06-28 18:36:24 -0300

    Cumpre requisito 6 - Cria um componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,36 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleClick() {
+    console.log(this.state);
+  }
+
+  render() {
+    const { onclick } = this.props;
+    return (
+      <form>
+        <li>{ onclick }</li>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
