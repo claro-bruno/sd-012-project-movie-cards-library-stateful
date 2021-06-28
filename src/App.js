64599bb39ac783ef07fa12efcf814f504edb6462
Milton Castro <castro.milton07@gmail.com>
2021-06-28 12:41:27 -0300

    00 - Inicializando o projeto Movie Cards Library VQV :rocket:

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
