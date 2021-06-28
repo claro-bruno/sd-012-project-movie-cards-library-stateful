42c014b2b12edb38f78590fc1291ec40d257fbf9
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-06-28 16:02:37 -0300

    envia requisito 6 testado e aprovado local

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default AddMovie;
