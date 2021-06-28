0cc2b57ca6a283ba5607ab73c58117302373355b
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-28 15:57:20 -0300

    renderiza select em searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,9 +31,19 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
-        Bora passar no lint com:
-        {selectedGenre}
-        {onSelectedGenreChange}
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
