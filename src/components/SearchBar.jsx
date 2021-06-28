1022f9c423e2fb2d0af34cf32be3e275baefb946
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-06-28 15:20:11 -0300

    Adiciona requsito 2 - Renderiza um forma dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,16 +4,30 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
     return (
-      <>
-      </>
+      <form data-testid="search-bar-form" action="">
+        <input />
+      </form>
     );
   }
 }
 
 SearchBar.propTypes = {
-  searchText: PropTypes.string,
-
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.string.isRequired,
+  onBookmarkedChange: PropTypes.string.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.string.isRequired,
 };
 
 export default SearchBar;
