2a3a84a1df4842e889b0d736c0aa2fcb78bd44fa
Jossany Moura <mjossany@gmail.com>
2021-06-28 16:36:06 -0300

    Resolve requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 class App extends React.Component {
@@ -7,6 +8,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        <SearchBar
+          searchText=""
+          onSearchTextChange={ () => {} }
+          bookmarkedOnly=""
+          onBookmarkedChange={ () => {} }
+          selectedGenre=""
+          onSelectedGenreChange={ () => {} }
+        />
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      'Eu sou uma search bar'
+    );
+  }
+}
+
+export default SearchBar;
