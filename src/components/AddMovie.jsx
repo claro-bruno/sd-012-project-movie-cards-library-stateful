7c1d59fbaeccc9cd84dcc77c2f5f2bdc4dfcb06f
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-06-28 16:40:46 -0300

    Resolvendo requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,14 +13,33 @@ class AddMovie extends React.Component {
       genre: 'action',
     }
 
-    this.handle
+    this.handleTitleChange = this.handleTitleChange.bind(this);
   }
-    
+
+  handleTitleChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
   render() {
+
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
-[]
-    );
+      <form data-testid="add-movie-form">
+        <label
+          htmlFor="label-title"
+          data-testid="title-input-label"
+        >
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleTitleChange }
+          />
+        </label>
+      </form>
+    )
   }
 }
 
