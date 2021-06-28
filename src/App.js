fcd8a96e8a8b1c736168c8b452a5e79518fc5f2b
Sass <dudu_sass12@hotmail.com>
2021-06-28 16:16:00 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import movies from './data';
 
 function App() {
   return (
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  render() {
-    const { movies } = this.props;
+  constructor() {
+    super()
 
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange} = this.props
     return (
-      <span>oi</span>
+      <form data-testid="search-bar-form"> 
+      <label>
+        
+      </label>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string, 
+ }
 
 export default SearchBar;
 // implement SearchBar component here
