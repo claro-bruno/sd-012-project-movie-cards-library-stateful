46f8ad6eb98be22c3b7574dc58c02f7d5608bcc2
Merlone <alestori7@gmail.com>
2021-06-28 14:38:53 -0300

    requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+const initialState = {
+  title: '',
+  subtitle: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form" />
+    );
+  }
+}
