1cef0441b61c646124efd753531173622f68fd73
RyanLaiber <ryanlaiber@gmail.com>
2021-06-28 16:03:25 -0300

    adiciona requisito 4 e 5, corrige lint e finaliza requisitos de construçao do componente SearchBar, so faltando a prop validation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,16 +7,47 @@ class SearchBar extends React.Component {
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onselectedGenreChange,
+      onSelectedGenreChange,
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label">
+        <label htmlFor="search-text" data-testid="text-input-label">
           Inclui o texto:
-          <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input"/>
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            id="search-text"
+          />
+        </label>
+        <label htmlFor="marked-checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            id="marked-checkbox"
+          />
+        </label>
+        <label htmlFor="select-genre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            type="select"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+            id="select-genre"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
-    )
+    );
   }
 }
 
