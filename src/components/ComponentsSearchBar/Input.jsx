b1e9f95309b18a1c658fe00373c4bdef57461f51
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-28 20:57:56 -0300

    adequa requisito_03 com o component Input

--- src/components/ComponentsSearchBar/Input.jsx
+++ src/components/ComponentsSearchBar/Input.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Input extends React.Component {
   render() {
-    const { type, datatestid, value, onChange, labelText } = this.props;
+    const { type, datatestid, value, onChange, labelText, datatestidLabel } = this.props;
     return (
       <section>
-        <label htmlFor={ datatestid } data-testid="text-input-label">
+        <label htmlFor={ datatestid } data-testid={ datatestidLabel }>
           { labelText }
           <input
             type={ type }
             id={ datatestid }
             data-testid={ datatestid }
             value={ value }
+            checked={ value }
             onChange={ onChange }
           />
         </label>
@@ -19,5 +21,14 @@ class Input extends React.Component {
     );
   }
 }
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  datatestid: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.string.isRequired,
+  labelText: PropTypes.string.isRequired,
+  datatestidLabel: PropTypes.string.isRequired,
+}.isRequired;
 
 export default Input;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,31 +4,17 @@ import Input from './ComponentsSearchBar/Input';
 
 class SearchBar extends React.Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-    } = this.props;
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <Input
           type="text"
-          datatestidInput="text-input"
+          datatestid="text-input"
           value={ searchText }
           onChange={ onSearchTextChange }
-          labelText="Inclui o texto:"
-          datatestLabel="text-input-label"
+          labelText="Inclui o texto"
+          datatestidLabel="text-input-label"
         />
-        <label htmlFor="inputCheckboxTag" data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input
-            type="checkbox"
-            id="inputTextTag"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-          />
-        </label>
       </form>
     );
   }
@@ -37,14 +23,10 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.string.isRequired,
-  bookmarkedOnly: PropTypes.string.isRequired,
-  onBookmarkedChange: PropTypes.string.isRequired,
 }.isRequired;
 
 SearchBar.defaultProps = {
   searchText: undefined,
   onSearchTextChange: undefined,
-  bookmarkedOnly: undefined,
-  onBookmarkedChange: undefined,
 };
 export default SearchBar;
