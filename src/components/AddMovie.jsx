a1d82c5fac0e927b17e6453de30976fd0dcf4194
Renata Nunes <renatanunes231@yahoo.com.br>
2021-06-28 17:01:46 -0300

    Criando componente AddMovie, seus estados e prop

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form"></form>
+    );
+  }
+}
+
+export default AddMovie;
