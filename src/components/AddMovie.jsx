c0e7f74b99add4d8f3b3dfef00bf1de36b7549b2
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-06-28 15:03:59 -0300

    render AddMovie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePatch: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+  render() {
+    const { onClick } = this.props;
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default AddMovie;
