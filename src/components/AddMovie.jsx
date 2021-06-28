1fb1d388ba4503ee5375fad0d7bf4d883e42ae80
Nuwanda <morato_dev@protonmail.com>
2021-06-28 17:26:08 -0300

    state do input de sinopse, caminho da imagem e subtitulo importados feitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,17 +15,47 @@ class AddMovie extends React.Component {
   }
 
   render() {
-    const { importMovieTitle } = this.state;
+    const { importMovieTitle, importMovieSubtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="importMovieTitle" data-testid="title-input-label">
           Título
           <input
             type="text"
+            data-testid="title-input"
             name="importMovieTitle"
             value={ importMovieTitle }
             id="importMovieTitle"
-            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="importMovieSubtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            data-testid="subtitle-input"
+            name="importMovieSubtitle"
+            value={ importMovieSubtitle }
+            id="importMovieSubtitle"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="imagePath" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+            name="imagePath"
+            value={ imagePath }
+            id="imagePath"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="importTextarea" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            id="importTextarea"
+            data-testid="storyline-input"
             onChange={ this.handleChange }
           />
         </label>
