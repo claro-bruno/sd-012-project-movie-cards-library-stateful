bfd69fbd6ab1648a133984c51eda52dcdcfcc7e3
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-28 18:10:02 -0300

    Cria um componente chamado <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,29 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.exact({
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      imagePath: PropTypes.string.isRequired,
+      bookmarked: PropTypes.bool.isRequired,
+      genre: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
+
+export default MovieLibrary;
