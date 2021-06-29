7357b985b64b600b0a8adff98b64dd2abaa5eb49
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-29 19:25:01 -0300

    Requisito 15 - Cria MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 // implement MovieLibrary component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -7,15 +8,22 @@ import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
