eecdbae0314fb46b604fb5077418c86bd7a77977
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-28 22:39:38 -0300

    requisito 6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
       </div>
     );
   }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,16 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form> </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func,
+}.isRequired;
+
+export default AddMovie;
