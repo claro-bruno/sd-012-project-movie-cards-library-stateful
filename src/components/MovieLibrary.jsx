7cc91a34a7eda5c25e1e35ec9be1741d8513f926
ladcs <hpladcs@gmail.com>
2021-06-29 12:09:47 -0300

    R15 and R16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,31 @@
 // implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <MovieList movies={ movies } />
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieLibrary;
