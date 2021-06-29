b43b35c8dc8c29b1fd6bb6c7e51de9f3137ab1d3
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-29 16:51:35 -0300

    adiciona estilizaçao ao componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+import '../css/movieCard.css';
 
 class MovieCard extends React.Component {
   render() {
@@ -8,8 +9,8 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
         <div className="movie-card-body">
+          <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
