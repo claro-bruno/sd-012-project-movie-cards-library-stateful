30dfb0e2f9bd51c7f33ad3a42603c1c38dcef6fa
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-28 21:17:18 -0300

    Promove resolucao para lint

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// prettier-ignore
 class AddButton extends React.Component {
   render() {
     const { onClick } = this.props;
     return (
-      <button type="submit" data-testid="send-button" onClick={onClick}>
+      <button type="submit" data-testid="send-button" onClick={ onClick }>
         Adicionar filme
       </button>
     );
