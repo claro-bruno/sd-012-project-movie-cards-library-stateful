ee14aae87f00b76de000f9f2082d6d95e677cc7b
Ed Santos <edsantos2822@gmail.com>
2021-06-29 16:17:39 -0300

    Finalizando - Requisito 2 - Adicionando um form ao componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <span>só nos tests</span>
+      <form data-testid="search-bar-form" />
     );
   }
 }
