eb59f55d46a999b85741face2296016bbf60bea5
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-06-29 17:46:31 -0300

    requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -20,20 +20,30 @@ class AddMovie extends React.Component {
   handleClick() {
     this.setState((estadoAnterior) => ({
       title: estadoAnterior.title,
+      subtitle: estadoAnterior.subtitle,
     }));
   }
 
   render() {
     const { onclick } = this.props;
     const { title } = this.state;
+    const { subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="input-titulo" data-testid="title-input-label">
-          Título:
+          Título
           <input
             placeholder={ title }
             onChange={ this.handleClick }
             data-testid="title-input"
+          />
+        </label>
+        <label htmlFor="input-Subtitulo" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            placeholder={ subtitle }
+            onChange={ this.handleClick }
+            data-testid="subtitle-input"
           />
         </label>
         <ul>
