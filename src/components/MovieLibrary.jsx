3992580b303c21370ac80406520d07623014869a
Pedro Gonçalves <peg.gep27@gmail.com>
2021-06-28 21:56:36 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,12 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    // const { movies } = this.props;
+    return (
+      <div />
+    );
+  }
+}
+
+export default MovieLibrary;
