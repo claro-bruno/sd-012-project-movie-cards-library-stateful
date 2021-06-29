1d5472c98ef5d40e86d17149b741384e4f5a2c3a
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-06-29 20:13:35 -0300

    render a form in <AddMovie>, render input type text to get title, subtitle, img, storyline and render input type number, but lack one requisition

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,45 @@ import React, { Component } from 'react';
 class AddMovie extends Component {
   render() {
     return (
-      <p>AddMovie</p>
+      <form data-testid="add-movie-form">
+        <label htmlFor=" " data-testid="title-input-label">
+          <p>Título</p>
+          <input
+            type="text"
+            data-testid="title-input"
+          />
+        </label>
+        <label htmlFor=" " data-testid="subtitle-input-label">
+          <p>Subtítulo</p>
+          <input
+            type="text"
+            data-testid="subtitle-input"
+          />
+        </label>
+        <label htmlFor=" " data-testid="image-input-label">
+          <p>Imagem</p>
+          <input
+            type="text"
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor=" " data-testid="storyline-input-label">
+          <p>Sinopse</p>
+          <input
+            type="textarea"
+            data-testid="storyline-input"
+          />
+        </label>
+        <label htmlFor=" " data-testid="rating-input-label">
+          <p>Avaliação</p>
+          <input
+            type="number"
+            data-testid="rating-input"
+            name="rating"
+            value={ 0 }
+          />
+        </label>
+      </form>
     );
   }
 }
