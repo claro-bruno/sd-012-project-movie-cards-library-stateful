6e054a93d6e6485419d6f3a1663d1f3ddf877c5d
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-06-29 20:18:42 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,12 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        ola
+      </div>
+    );
+  }
+}
