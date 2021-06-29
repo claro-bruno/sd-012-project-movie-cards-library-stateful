77dfb25174b7ec12ac59038e72b1f018ffcece07
martinsph <ph.mmartins@gmail.com>
2021-06-29 14:40:40 -0300

    teste1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -9,16 +9,16 @@
 //     super(props);
 //   }
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
 
 export default MovieLibrary;
