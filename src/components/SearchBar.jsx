dcd8841365aa0b69d833b8b8e7ff64c58563686c
Willian Prado <willian_prado@hotmail.com>
2021-06-29 13:24:30 -0300

    Req 4 - Renderiza um input checkbox dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,12 +13,23 @@ class SearchBar extends React.Component {
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="search" data-testid="text-input-label">
+          Inclui o texto:
           <input
             id="seach"
             type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
+          />
+        </label>
+
+        <label htmlFor="searchBar-checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            id="searchBar-checkbox"
+            type="checkbox"
+            name=""
+            onChange={ onBookmarkedChange }
           />
         </label>
       </form>
