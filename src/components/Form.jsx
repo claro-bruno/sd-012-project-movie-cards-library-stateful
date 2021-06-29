079b1cd4dd92298a7245ed4632f9c3b39157aa3b
Álvaro Palma <palma.alvaro@outlook.com>
2021-06-28 22:14:10 -0300

    Adiciona input de texto ligado ao estado do componente form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Para pesquisar clique em OK');
+    event.preventDefault();
+  }
+
   render() {
-    <div>
-      <label data-testid="text-input-label">Inclui o texto:</label>
-      <input type="text"></input>
-    </div>
+    <form onSubmit={this.handleSubmit}>
+      <label data-testid="text-input-label">Inclui o texto:
+        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+      </label>
+    </form>
   }
 }
 
