0dd5b0e8b970f7bbb98e8a261f6457604b148e11
matheus oliveira de proenca <mproenca.marketing@gmail.com>
2021-06-29 15:43:14 -0300

    adiciona requsito 8 e 9 subtitulo e titulo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
 
-        this.state = {
-            subtitle: '',
-            title: '',
-            imagePath: '',
-            storyline: '',
-            rating: 0,
-            genre: 'action',
-        }
-    }
-    render() {
-       return()
-    }
-} 
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { title, subtitle, handleChange } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            data-testid="title-input"
+            id="title-input"
+            type="text"
+            name="title"
+            value={ title }
+            onChange={ handleChange }
+          />
+        </label>
+
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+          Subtítulo
+          <input
+            id="subtitle-input"
+            data-testid="subtitle-input"
+            type="text"
+            name="subtitle"
+            value={ subtitle }
+            onChange={ handleChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
 
 export default AddMovie;
