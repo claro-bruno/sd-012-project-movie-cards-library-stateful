6bd19308bf8146aa42b2b9a0a0a4dbf07f03afd8
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-06-29 15:43:39 -0300

    Requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import MovieTitle from './MovieCards/MovieTitle';
+import MovieSubtitle from './MovieCards/MovieSubtitle';
+import MovieImage from './MovieCards/MovieImage';
+import MovieStory from './MovieCards/MovieStory';
+import MovieRating from './MovieCards/MovieRating';
+import MovieGenre from './MovieCards/MovieGenre';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <MovieTitle />
+        <MovieSubtitle />
+        <MovieImage />
+        <MovieStory />
+        <MovieRating />
+        <MovieGenre />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
