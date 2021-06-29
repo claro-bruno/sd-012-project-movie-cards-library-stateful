a69fd3823ed2ea31a27f7ffbd18bb6cf5f66baeb
igor <igorsife80@gmail.com>
2021-06-29 12:21:39 -0300

    cumpre os requisitos de 6 a 11 com base no teste AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,15 +14,16 @@ class AddMovie extends React.Component {
     }
   }
 
+
   render() {
     return (
       <form data-testid="add-movie-form" >
-        <label data-testid="subtitle-input-label">subtítulo<input data-testid="subtitle-input" type="text"/></label>
         <label data-testid="title-input-label"
-        >Titulo<input data-testid="title-input" type="text"/></label>
-        <label data-testid="image-input-label" >caminho da imagem<input data-testid="image-input" type="text"/></label>
-        <label data-testid="storyline-input-label" >sinopse <textarea data-testid="storyline-input" name="" id="" cols="30" rows="10"></textarea></label>
-        <label data-testid="rating-input-label" >avaliação<input data-testid="rating-input" type="text"/></label>
+        >Título<input data-testid="title-input" type="text"/></label>
+        <label data-testid="subtitle-input-label">Subtítulo<input data-testid="subtitle-input" type="text"/></label>
+        <label data-testid="image-input-label" >Imagem<input data-testid="image-input" type="text"/></label>
+        <label data-testid="storyline-input-label" >Sinopse <textarea data-testid="storyline-input" name="" id="" cols="30" rows="10"></textarea></label>
+        <label data-testid="rating-input-label" >Avaliação<input data-testid="rating-input" type="text"/></label>
         <label data-testid="genre-input-label" >gênero <select data-testid="genre-input" name="" id=""></select></label>
       </form>
     )
