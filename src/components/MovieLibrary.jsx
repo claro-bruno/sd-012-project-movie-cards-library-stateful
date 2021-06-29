55032c7af5d7a5320918e09af592ed0db84036c8
Djaniza Vasques <djanizav@gmail.com>
2021-06-29 16:52:47 -0300

    Adiciona MovieLibrary e MovieLibrary state

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,28 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import movies from '../data';
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: { movies }
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+      </div>
+    );
+  }
+
+}
+
+export default MovieLibrary;
