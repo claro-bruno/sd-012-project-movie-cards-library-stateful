59d1b26fe50a0ae69037a99a188640727b58a059
David Azevedo <zevid.dev@outlook.com>
2021-06-29 19:07:55 -0300

    Create functions onChange in MovieLibrary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieLibrary from './components/MovieLibrary';
-import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Header />
       <MovieLibrary />
-      <AddMovie />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 import movies from '../data';
 
 class MovieLibrary extends React.Component {
@@ -12,13 +13,44 @@ class MovieLibrary extends React.Component {
       selectedGenre: '',
       movies: '',
     };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange(e) {
+    this.setState({
+      searchText: e.target.value,
+    });
+  }
+
+  onBookmarkedChange(e) {
+    this.setState({
+      bookmarkedOnly: e.target.checked,
+    });
+  }
+
+  onSelectedGenreChange(e) {
+    this.setState({
+      selectedGenre: e.target.value,
+    });
   }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <form>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
         <MovieList movies={ movies } />
+        <AddMovie />
       </form>
     );
   }
