1b61ccb18e8eafb19abc22c11c69becb2b5f827b
Rodrigo <rodrigo.facury14@gmail.com>
2021-06-29 16:52:04 -0300

    refatora AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,49 +15,115 @@ class AddMovie extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange() {
-    console.log(this.state);
+  handleChange({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+  }
+
+  // Dividi em funções devido à dica da colega Adriana Biberg.
+
+  createTitleInput() {
+    return (
+      <label htmlFor="add-title" data-testid="title-input-label">
+        Título
+        <input
+          name="title"
+          type="text"
+          id="add-title"
+          data-testid="title-input"
+          onChange={ this.handleChange }
+        />
+      </label>);
+  }
+
+  createSubtitleInput() {
+    return (
+      <label htmlFor="add-subtitle" data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          name="subtitle"
+          type="text"
+          id="add-subtitle"
+          data-testid="subtitle-input"
+          onChange={ this.handleChange }
+        />
+      </label>);
+  }
+
+  createImageInput() {
+    return (
+      <label htmlFor="add-image" data-testid="image-input-label">
+        Imagem
+        <input
+          type="text"
+          id="add-image"
+          data-testid="image-input"
+          name="imagePath"
+          onChange={ this.handleChange }
+        />
+      </label>);
+  }
+
+  createStorylineInput() {
+    return (
+      <label htmlFor="add-storyline" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          id="add-storyline"
+          data-testid="storyline-input"
+          name="storyline"
+          onChange={ this.handleChange }
+        />
+      </label>);
+  }
+
+  createRatingInput() {
+    return (
+      <label htmlFor="add-rating" data-testid="rating-input-label">
+        Avaliação
+        <input
+          type="number"
+          id="add-rating"
+          data-testid="rating-input"
+          defaultValue={ 0 }
+          name="rating"
+          onChange={ this.handleChange }
+        />
+      </label>);
+  }
+
+  createGenreInput() {
+    return (
+      <label htmlFor="add-genre" data-testid="genre-input-label">
+        Gênero
+        <select
+          id="add-genre"
+          defaultValue="Ação"
+          data-testid="genre-input"
+          name="genre"
+          onChange={ this.handleChange }
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+      </label>);
   }
 
   render() {
     const { onClick } = this.props;
-    const { rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         { onClick() }
-        <label htmlFor="add-title" data-testid="title-input-label">
-          Título
-          <input type="text" id="add-title" data-testid="title-input" />
-        </label>
-        <label htmlFor="add-subtitle" data-testid="subtitle-input-label">
-          Subtítulo
-          <input type="text" id="add-subtitle" data-testid="subtitle-input" />
-        </label>
-        <label htmlFor="add-image" data-testid="image-input-label">
-          Imagem
-          <input type="text" id="add-image" data-testid="image-input" />
-        </label>
-        <label htmlFor="add-storyline" data-testid="storyline-input-label">
-          Sinopse
-          <textarea id="add-storyline" data-testid="storyline-input" />
-        </label>
-        <label htmlFor="add-rating" data-testid="rating-input-label">
-          Avaliação
-          <input
-            type="number"
-            id="add-rating"
-            data-testid="rating-input"
-            defaultValue={ rating }
-          />
-        </label>
-        <label htmlFor="find-genre" data-testid="genre-input-label">
-          Gênero
-          <select id="find-genre" defaultValue={ genre } data-testid="genre-input">
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
-          </select>
-        </label>
+        { this.createTitleInput() }
+        { this.createSubtitleInput() }
+        { this.createImageInput() }
+        { this.createStorylineInput() }
+        { this.createRatingInput() }
+        { this.createGenreInput() }
+        {console.log(this.state)}
       </form>
     );
   }
