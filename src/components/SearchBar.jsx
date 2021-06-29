937e86a7b637062519f74caf5dcf0a04b86007cc
André Dantas <aluizcfd@gmail.com>
2021-06-29 01:35:04 -0300

    Correção ESLint, declaração das props em component SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ export class SearchBar extends Component {
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange
+      onSelectedGenreChange,
     } = this.props;
 
     return (
