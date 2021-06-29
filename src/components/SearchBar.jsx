3bd1391a7dcecaef8062b3822f361fe368db9396
André Dantas <aluizcfd@gmail.com>
2021-06-29 01:09:08 -0300

    Implementados os dois primeiros inputs no forms componentizado em SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,41 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+export class SearchBar extends Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <label htmlFor="text-input" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              data-testid="text-input"
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+          <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+            Inclui o texto:
+            <input
+              data-testid="checkbox-input"
+              type="text"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
+        </form>
+
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
