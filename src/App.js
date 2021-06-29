0266e8eda4c7b606ba37a7aeec08856bb49c8c4e
Roberval Filho <rslfilho@gmail.com>
2021-06-29 15:33:34 -0300

    Adicionando footer que tinha criado já no projeto anterior

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './components/Header';
 import './App.css';
 import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MovieLibrary movies={ movies } />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../style/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>
+          <span>Desenvolvido por </span>
+          <a href="https://www.rslfilho.com.br/" target="_blank" rel="noreferrer">Roberval Filho</a>
+          <span> usando React - Bloco 12 - </span>
+          <a href="http://www.betrybe.com.br/" target="_blank" rel="noreferrer">Trybe</a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
