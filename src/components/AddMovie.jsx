b4a7dd95bc1973e0acd5b8ce9be38430a9c7c8f3
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-28 23:05:58 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
   render() {
+    const { title } = this.state;
     return (
-      <form data-testid="add-movie-form"> </form>
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            name="title"
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
