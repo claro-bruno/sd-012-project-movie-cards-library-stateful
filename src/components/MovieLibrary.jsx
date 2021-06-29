c33b91684f21b269054e92d045c317222423f378
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-29 16:29:50 -0300

    conclui requisito_15 - Crie um componente chamado <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,9 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return <h1>teste</h1>;
+  }
+}
+
+export default MovieLibrary;
