a51a2f46123b68e1f9525062b69816b2d1844391
thales <thalesdaiel2013@gmail.com>
2021-06-29 00:34:19 -0300

    feito etapa 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+  }
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <input type="text" />
+      </form>
+    );
+  }
+}
+export default AddMovie;
