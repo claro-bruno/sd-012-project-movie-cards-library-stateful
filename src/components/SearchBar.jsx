4142860b72d5d112a2b1c30e240a5c4c7bd415d2
Victor <juanvictor70@gmail.com>
2021-06-29 15:46:51 -0400

    renderizando form dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,30 @@
 // implement SearchBar component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+    } = this.props;
+
     return (
-      <input type="text" />
+      <div>
+        <form data-testid="search-bar-form">
+          <input
+            type="text"
+            value={ searchText }
+            handleChange={ onSearchTextChange }
+          />
+        </form>
+      </div>
     );
   }
 }
 
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 export default SearchBar;
