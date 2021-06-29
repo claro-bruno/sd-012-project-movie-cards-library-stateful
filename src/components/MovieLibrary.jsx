9531e2b20ff61c1ddc6baa44bebd1f9d586062cc
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-28 21:44:16 -0300

    Pretende resolver novos requisitos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
 // implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <section>
+        <SearchBar />
+        <AddMovie />
+      </section>
+    );
+  }
+}
+
+export default MovieLibrary;
