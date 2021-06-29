72b861d7531e1a2f1600f41e2e410ed4ee6bdf85
ladcs <hpladcs@gmail.com>
2021-06-29 14:36:06 -0300

    R18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,6 +14,7 @@ class MovieLibrary extends React.Component {
       selectedGenre: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.filteOfMovies = this.filteOfMovies.bind(this);
   }
 
   handleChange({ target }) {
@@ -24,9 +25,25 @@ class MovieLibrary extends React.Component {
     });
   }
 
+  filteOfMovies() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    let movieList = movies;
+    if (searchText !== '') {
+      movieList = movieList.filter((findMovie) => findMovie.title.includes(searchText)
+      || findMovie.subtitle.includes(searchText)
+      || findMovie.storyline.includes(searchText));
+    }
+    movieList = bookmarkedOnly ? movieList
+      .filter((movieFind) => movieFind.bookmarked) : movieList;
+    movieList = selectedGenre === '' ? movieList : movieList
+      .filter((movieFind) => movieFind.genre === selectedGenre);
+    return movieList;
+  }
+
   render() {
-    const { handleChange, props, state } = this;
-    const { movies } = props;
+    const { handleChange, state, filteOfMovies } = this;
+    const movies = filteOfMovies();
     const { searchText, bookmarkedOnly, selectedGenre } = state;
     return (
       <div>
