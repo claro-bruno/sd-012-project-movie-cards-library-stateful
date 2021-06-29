16f361863ec746acc07771a6ccd9163b0e926776
Euller Braz <euller.braz99@gmail.com>
2021-06-29 16:28:21 -0300

    Refatorado AddMovie adicionando MovieInfo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import Input from './Input';
-import Textarea from './Textarea';
+import MovieInfo from './MovieInfo';
 
 class AddMovie extends React.Component {
   constructor() {
     super();
 
-    this.handleChange = this.handleChange.bind(this);
-
     this.state = {
       title: '',
       subtitle: '',
       imagePath: '',
-    /*       storyline: '',
-      rating: 0,
+      storyline: '',
+    /*       rating: 0,
       genre: 'action', */
     };
   }
 
-  handleChange({ target }) {
+  handleChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
@@ -31,45 +28,14 @@ class AddMovie extends React.Component {
     const { title, subtitle, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <Input
-          inputId="title"
-          inputName="title"
-          testIdInput="title-input"
-          testIdLabel="title-input-label"
-          typeInput="text"
-          value={ title }
+        <MovieInfo
+          title={ title }
+          subtitle={ subtitle }
+          imagePath={ imagePath }
+          storyline={ storyline }
           onChange={ this.handleChange }
-          labelText="Título"
         />
-        <Input
-          inputId="subtitle"
-          inputName="subtitle"
-          testIdInput="subtitle-input"
-          testIdLabel="subtitle-input-label"
-          typeInput="text"
-          value={ subtitle }
-          onChange={ this.handleChange }
-          labelText="Subtítulo"
-        />
-        <Input
-          inputId="imagePath"
-          inputName="imagePath"
-          testIdInput="image-input"
-          testIdLabel="image-input-label"
-          typeInput="text"
-          value={ imagePath }
-          onChange={ this.handleChange }
-          labelText="Imagem"
-        />
-        <Textarea
-          textareaId="storyline"
-          textareaName="storyline"
-          testIdTextarea="storyline-input"
-          testIdLabel="storyline-input-label"
-          value={ storyline }
-          onChange={ this.handleChange }
-          labelText="Sinopse"
-        />
+
       </form>
     );
   }
---
+++ src/components/MovieInfo.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Input from './Input';
+import Textarea from './Textarea';
+
+class MovieInfo extends React.Component {
+  render() {
+    const { title, subtitle, imagePath, storyline, onChange } = this.props;
+    return (
+      <div>
+        <Input
+          inputId="title"
+          inputName="title"
+          testIdInput="title-input"
+          testIdLabel="title-input-label"
+          typeInput="text"
+          value={ title }
+          onChange={ onChange }
+          labelText="Título"
+        />
+        <Input
+          inputId="subtitle"
+          inputName="subtitle"
+          testIdInput="subtitle-input"
+          testIdLabel="subtitle-input-label"
+          typeInput="text"
+          value={ subtitle }
+          onChange={ onChange }
+          labelText="Subtítulo"
+        />
+        <Input
+          inputId="imagePath"
+          inputName="imagePath"
+          testIdInput="image-input"
+          testIdLabel="image-input-label"
+          typeInput="text"
+          value={ imagePath }
+          onChange={ onChange }
+          labelText="Imagem"
+        />
+        <Textarea
+          textareaId="storyline"
+          textareaName="storyline"
+          testIdTextarea="storyline-input"
+          testIdLabel="storyline-input-label"
+          value={ storyline }
+          onChange={ onChange }
+          labelText="Sinopse"
+        />
+      </div>
+    );
+  }
+}
+
+MovieInfo.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  imagePath: PropTypes.string,
+  storyline: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+MovieInfo.defaultProps = {
+  title: '',
+  subtitle: '',
+  imagePath: '',
+  storyline: '',
+  onChange: console.log,
+};
+
+export default MovieInfo;
