51a9a5f35d8a2044d645a9b2a4eb39d83358b600
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-06-29 16:37:31 -0300

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,41 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const initialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  render() {
+    const { onclick } = this.props;
+    return (
+      <form>
+        <ul>
+          { onclick }
+        </ul>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func.isRequired,
+  initialState: PropTypes.shape({
+    subtitle: PropTypes.string,
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    genre: PropTypes.string,
+  }).isRequired,
+};
+export default AddMovie;
