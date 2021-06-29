937f1b8a3a6c484b6fbf4fa012bb466c016f4b26
Roberval Filho <rslfilho@gmail.com>
2021-06-29 15:37:10 -0300

    Moficiando atributo rel de links conforme warning do React

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ class Footer extends Component {
       <div className="footer">
         <p>
           <span>Desenvolvido por </span>
-          <a href="https://www.rslfilho.com.br/" target="_blank" rel="noreferrer">Roberval Filho</a>
+          <a href="https://www.rslfilho.com.br/" target="_blank" rel="noopener noreferrer">Roberval Filho</a>
           <span> usando React - Bloco 12 - </span>
-          <a href="http://www.betrybe.com.br/" target="_blank" rel="noreferrer">Trybe</a>
+          <a href="http://www.betrybe.com.br/" target="_blank" rel="noopener noreferrer">Trybe</a>
         </p>
       </div>
     );
