11cc5c61ad02d168735eb046be638cd0f415e287
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-28 22:25:00 -0300

    Pretende resolver mais um requisito para alcancar 80% (3)

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,39 @@
 // implement MovieLibrary component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import movieList from '../data';
 
+const defaultML = {
+  searchText: '',
+  bookmarkedOnly: false,
+  selectedGenre: '',
+  movies: movieList,
+};
+
 // prettier-ignore
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    this.state = defaultML;
+  }
+
   render() {
+    const { movies } = this.props;
     return (
       <section>
         <SearchBar />
-        <MovieList movies={ movieList } />
+        <MovieList movies={ movies } />
         <AddMovie />
       </section>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.element.isRequired,
+};
 
 export default MovieLibrary;
