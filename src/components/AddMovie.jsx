2ec0ee250dad3b5956277ed06ff6bda5cee17069
Rodrigo <rodrigo.facury14@gmail.com>
2021-06-29 12:35:17 -0300

    requisito 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,6 +12,7 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange() {
@@ -20,27 +21,35 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick } = this.props;
-    // const { title } = this.state;
+    const { rating } = this.state;
     return (
       <form data-testid="add-movie-form">
         { onClick() }
-        <label htmlFor="find-title" data-testid="title-input-label">
+        <label htmlFor="add-title" data-testid="title-input-label">
           Título
-          <input type="text" id="find-title" data-testid="title-input" />
+          <input type="text" id="add-title" data-testid="title-input" />
         </label>
-        <label htmlFor="find-subtitle" data-testid="subtitle-input-label">
+        <label htmlFor="add-subtitle" data-testid="subtitle-input-label">
           Subtítulo
-          <input type="text" id="find-subtitle" data-testid="subtitle-input" />
+          <input type="text" id="add-subtitle" data-testid="subtitle-input" />
         </label>
-        <label htmlFor="find-image" data-testid="image-input-label">
+        <label htmlFor="add-image" data-testid="image-input-label">
           Imagem
-          <input type="text" id="find-image" data-testid="image-input" />
+          <input type="text" id="add-image" data-testid="image-input" />
         </label>
-        <label htmlFor="find-storyline" data-testid="storyline-input-label">
+        <label htmlFor="add-storyline" data-testid="storyline-input-label">
           Sinopse
-          <textarea id="find-storyline" data-testid="storyline-input" />
+          <textarea id="add-storyline" data-testid="storyline-input" />
         </label>
-        <input type="text" id="find-rating" />
+        <label htmlFor="add-rating" data-testid="rating-input-label">
+          Avaliação
+          <input
+            type="number"
+            id="add-rating"
+            data-testid="rating-input"
+            defaultValue={ rating }
+          />
+        </label>
         <input type="text" id="find-genre" />
       </form>
     );
