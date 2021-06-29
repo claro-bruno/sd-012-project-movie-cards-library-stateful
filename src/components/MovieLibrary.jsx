01c6e3a61f7c5f60bd292f215678cb283f31f2c8
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-29 10:33:05 -0300

    Requirement 16-> Set initial state to `MovieLibrary`

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class MovieLibrary extends Component {
+  constructor() {
+    super(props);
+    this.state = {
+      searchText: "",
+      bookmarkedOnly: false,
+      selectedGenre: "",
+      movies: this.props.movies,
+    }
+  }
   render() {
     return ();
   }
