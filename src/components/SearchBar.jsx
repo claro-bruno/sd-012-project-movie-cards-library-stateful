cf48eccf7d1d7e1964eb53ecaa323f637d931f73
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-06-29 14:58:25 -0300

    Requisitos 4 e 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,14 +6,40 @@ class SearchBar extends Component {
   render() {
     const {
       searchText,
-      onSearchTextChange } = this.props;
-      // bookmarkedOnly,
-      // onBookmarkedChange,
-      // selectedGenre,
-      // onSelectedGenreChange
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <SearchText searchText={ searchText } onSearchTextChange={ onSearchTextChange } />
+        <label htmlFor="bookmarkedOnly" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            name="bookmarkedOnly"
+          />
+        </label>
+        <label htmlFor="genre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name="selectedGenre"
+            id="selectedGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+            className="input"
+          >
+            <option data-testid="select-input" value="">Todos</option>
+            <option data-testid="select-input" value="action">Ação</option>
+            <option data-testid="select-input" value="comedy">Comédia</option>
+            <option data-testid="select-input" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
@@ -22,10 +48,10 @@ class SearchBar extends Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
-  // bookmarkedOnly: PropTypes.bool.isRequired,
-  // onBookmarkedChange: PropTypes.func.isRequired,
-  // selectedGenre: PropTypes.string.isRequired,
-  // onSelectedGenreChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
