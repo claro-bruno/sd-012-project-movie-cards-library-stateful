b5705d2a1747263c3e0e5a6ca3b0d14396842eac
Josimar Souza <contact.josimarsouza@gmail.com>
2021-06-29 15:45:06 -0300

    Adiciona input no form no componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,16 +3,26 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    /* const {
+    const {
       searchText,
       onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props; */
+      // bookmarkedOnly,
+      // onBookmarkedChange,
+      // selectedGenre,
+      // onSelectedGenreChange,
+    } = this.props;
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
     );
   }
 }
