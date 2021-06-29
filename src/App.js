d3dc3f81b5e1736dd744a63992ca620452ab33e5
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-28 22:02:54 -0300

    Pretende resolver mais um requisito para alcancar 80% (2)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
-import './App.css';
 import MovieLibrary from './components/MovieLibrary';
+import './App.css';
 
+// prettier-ignore
 function App() {
   return (
     <div className="App">
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import movieList from '../data';
 
+// prettier-ignore
 class MovieLibrary extends React.Component {
   render() {
     return (
       <section>
         <SearchBar />
-        <MovieList />
+        <MovieList movies={ movieList } />
         <AddMovie />
       </section>
     );
