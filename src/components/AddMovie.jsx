c312ee300f28346a7a85ddf3ea861a02b95d3a82
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-29 16:43:10 -0300

    Requisito 6 - Criei componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
 // implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <p>oi</p>
+    );
+  }
+}
+
+export default AddMovie;
