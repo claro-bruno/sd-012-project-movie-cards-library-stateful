67ecaec031b2222e65232344ab2885f1a30232c5
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-28 21:48:52 -0300

    Pretende resolver mais um requisito para alcancar 80%

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,7 @@
 // implement MovieLibrary component here
 import React from 'react';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
@@ -8,6 +9,7 @@ class MovieLibrary extends React.Component {
     return (
       <section>
         <SearchBar />
+        <MovieList />
         <AddMovie />
       </section>
     );
