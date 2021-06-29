b988545470c4d84b77df3b49f4ba1446eb9a23a8
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-06-29 15:08:49 -0300

    adiciona componente para o botao adicionar filme

---
+++ src/components/AddMovieComponents/AddMovieButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovieButton extends React.Component {
+  render() {
+    const { funcOnClick } = this.props;
+    return (
+      <button
+        data-testid="send-button"
+        type="button"
+        onClick={ funcOnClick }
+      >
+        Acicionar filme
+      </button>
+    );
+  }
+}
+
+export default AddMovieButton;
+
+AddMovieButton.propTypes = {
+  funcOnClick: PropTypes.func.isRequired,
+};
