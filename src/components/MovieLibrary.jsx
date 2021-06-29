28257c6e9fe4a7ab9ea8a2dd835fc7c8fee66035
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-28 21:21:29 -0300

    renderiza searchbar dentro de MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
+
+    const { movies } = this.props;
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
@@ -11,12 +19,31 @@ class MovieLibrary extends React.Component {
       movies: movies,
     };
   }
+
+  onSearchTextChange({target}) {
+    this.setState({ searchText: target.value });
+  }
+
+  onBookmarkedChange() {
+    this.setState((anterior, _prop) => ({ bookmarkedOnly: !anterior.bookmarkedOnly }));
+  }
+
+  onSelectedGenreChange({target}) {
+    this.setState({ selectedGenre: target.value });
+  }
+  
   render() {
-    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
-        Bora passar no lint com:
-        { movies }
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
       </div>
     );
   }
