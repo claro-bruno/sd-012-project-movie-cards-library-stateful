1b21f3cc7ba525abaa1e568f60506891844e1401
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-29 18:20:25 -0300

    requisito 1 feito

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <input />
+    );
+  }
+}
+
+export default SearchBar;
