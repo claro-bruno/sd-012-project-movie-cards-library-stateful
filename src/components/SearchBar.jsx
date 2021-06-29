a9b1204496bef7977ec1c47b55d93cb10d99ae1f
Josimar Souza <contact.josimarsouza@gmail.com>
2021-06-29 15:27:50 -0300

    Cria componente SearchBar e retorna um form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    /* const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props; */
+    return (
+      <form data-testid="search-bar-form" />
+    );
+  }
+}
+
+export default SearchBar;
