57d8174f8100843c300aa7a7f897f7584793a9d9
gscuri <gscuri6@hotmail.com>
2021-06-30 00:47:52 -0300

    exercicio 8 comentando onClick, import propType

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,11 +3,37 @@ import React from 'react';
 // import PropType from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+  }
+
+  handleTitleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
   render() {
     // const { onClick } = this.props;
+    const { title } = this.state;
     return (
       <div>
         <form data-testid="add-movie-form" />
+        <label
+          htmlFor="tituloID"
+          data-testid="title-input-label"
+        >
+          Título
+          <input
+            id="tituloID"
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleTitleChange }
+          />
+        </label>
       </div>
     );
   }
