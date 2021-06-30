7b69d480e5307fe920b2d5eda7bee6186522eba0
Marcela Silva <marcela.ops@hotmail.br>
2021-06-29 21:33:58 -0300

    tentantiva requisito 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -20,23 +20,54 @@ export default class MovieLibrary extends Component {
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.filtered = this.filtered.bind(this);
   }
 
+  // handleChange({ target }) {
+  //   this.setState({
+  //     [target.name]: target.type === 'checkbox' ? target.checked : target.value,
+  //   });
+  // }
   onSearchTextChange({ target }) {
-    const { value } = target;
-    this.setState(() => ({ searchText: value }));
+    this.setState(() => ({ searchText: target.value }));
   }
 
   onBookmarkedChange({ target }) {
-    const { value } = target;
-    this.setState(() => ({ bookmarkedOnly: value }));
+    this.setState(() => ({ bookmarkedOnly: target.checked }));
   }
 
   onSelectedGenreChange({ target }) {
-    const { value } = target;
-    this.setState(() => ({ selectedGenre: value }));
+    this.setState(() => ({ selectedGenre: target.value }));
   }
 
+  // Requisito 18 - Ajuda monitor Isaac
+  // filtered(movies) {
+  //   const { bookmarkedOnly, selectedGenre, searchText } = this.state;
+  //   return movies
+  //     .filter((movie) => this.filtereBookmarked(movie, bookmarkedOnly))
+  //     .filter(() => this.filteredGenre(movie, this.filteredGenre(movie, selectedGenre))
+  //     .filter(() => ));
+  // }
+
+  filtereBookmarked(movie, bookmarkedOnly) {
+    if (bookmarkedOnly && movie.bookmarked) return true;
+    if (bookmarkedOnly) return false;
+    return true;
+  }
+  // tem q trabalhar a lógica dessas duas proximas funcoes, por hora eu só copiei e colei da func ai de cima
+  // filteredGenre(movie, selectedGenre) {
+  //   if (selectedGenre && movie.genre) return true;
+  //   if (selectedGenre) return false;
+  //   return true;
+  // }
+
+  // filteredText(movie, searchText) {
+  //   if (searchText && movie.Text) return true;
+  //   if (searchText) return false;
+  //   return true;
+  // }
+
+  // Requisito 18 - Ajuda Monitora Lêticia com as funções callbacks.
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
 
@@ -51,7 +82,7 @@ export default class MovieLibrary extends Component {
           onSelectedGenreChange={ this.onSelectedGenreChange }
         />
 
-        <MovieList movies={ movies } />
+        <MovieList movies={ this.filtered(movies) } />
         <AddMovie onClick={ movies } />
       </div>
     );
