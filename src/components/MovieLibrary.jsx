e0f8220520e200a4803aff6b1877a8e217451054
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-30 00:42:19 -0300

    conclui requisito_16 - Configure o estado inicial do componente MovieLibray

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { handleChange } = this;
     return (
-      <SearchBar />
+      <section>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ handleChange }
+          onBookmarkedChange={ handleChange }
+          onSelectedGenreChange={ handleChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </section>
+
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
