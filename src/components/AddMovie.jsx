8e4d381241276407dd7a39f3dc34c4f20d7207f2
André Dantas <aluizcfd@gmail.com>
2021-06-30 02:50:21 -0300

    Criação component genérico Input, renderização Inputs Título e Subtítulo em AddMovie, renderização de AddMovie em App.js

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,57 @@
-// implement AddMovie component here
+import React from 'react';
+import Input from './Input';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { title, subtitle } = this.state;
+    return (
+      <section>
+        <form data-testid="add-movie-form">
+          <Input
+            htmlFor="title-input"
+            dataTestIdLabel="title-input-label"
+            label="Título"
+            dataTestIdInput="title-input"
+            name="title"
+            type="text"
+            value={ title }
+            handleChange={ this.handleChange }
+          />
+
+          <Input
+            htmlFor="subtitle-input"
+            dataTestIdLabel="subtitle-input-label"
+            label="Subtítulo"
+            dataTestIdInput="subtitle-input"
+            name="subtitle"
+            type="text"
+            value={ subtitle }
+            handleChange={ this.handleChange }
+          />
+        </form>
+      </section>
+    );
+  }
+}
+
+export default AddMovie;
