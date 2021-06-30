05e8e0a888c9ec400bd1ba63844c9b87e143f5e4
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-06-30 10:43:12 -0300

    1-4 componentes searchBar,formulario,input e checkbox

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,59 +2,61 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Input extends React.Component {
-    render() {
-      const {
-        inputId,
-        inputTestId,
-        inputLabel,
-        inputLabelTestId,
-        inputType,
-        inputName,
-        inputValue,
-        onChangeInput,
-        inputIsRequired,
-        inputMaxlength,
-    } = this.props;    
-
+  render() {
+    const {
+      inputId,
+      inputTestId,
+      inputLabel,
+      inputLabelTestId,
+      inputType,
+      inputName,
+      inputChecked,
+      inputValue,
+      onChangeInput,
+      inputIsRequired,
+      inputMaxlength,
+    } = this.props;
 
     return (
-        <label htmlFor={ inputId } data-testid={ inputLabelTestId }>
-          { inputLabel}
-          <input
-             id={ inputId } 
-             data-testid={ inputTestId }
-             type={ inputType }
-             name={ inputName }
-             value={ inputValue }
-             onChange={ onChangeInput }
-             required={ inputIsRequired }
-             maxLength={ inputMaxlength }
-            />
-        </label>
+      <label htmlFor={ inputId } data-testid={ inputLabelTestId }>
+        { inputLabel}
+        <input
+          id={ inputId }
+          data-testid={ inputTestId }
+          type={ inputType }
+          name={ inputName }
+          checked={ inputChecked }
+          value={ inputValue }
+          onChange={ onChangeInput }
+          required={ inputIsRequired }
+          maxLength={ inputMaxlength }
+        />
+      </label>
     );
   }
-
 }
 
 Input.propTypes = {
-    inputId: PropTypes.string.isRequired,
-    inputTestId: PropTypes.string,
-    inputLabel: PropTypes.string.isRequired,
-    inputLabelTestId: PropTypes.string,
-    inputType: PropTypes.string,
-    inputName: PropTypes.string.isRequired,
-    inputValue: PropTypes.string.isRequired,
-    onChangeInput: PropTypes.func.isRequired,
-    inputIsRequired: PropTypes.bool,
-    inputMaxlength: PropTypes.number, 
-}
+  inputId: PropTypes.string.isRequired,
+  inputTestId: PropTypes.string,
+  inputLabel: PropTypes.string.isRequired,
+  inputLabelTestId: PropTypes.string,
+  inputType: PropTypes.string,
+  inputName: PropTypes.string.isRequired,
+  inputChecked: PropTypes.bool.isRequired,
+  inputValue: PropTypes.string.isRequired,
+  onChangeInput: PropTypes.func.isRequired,
+  inputIsRequired: PropTypes.bool,
+  inputMaxlength: PropTypes.number,
+};
 
 Input.defaultProps = {
-    inputType: 'text',
-    inputTestId: '',
-    inputLabelTestId: '',
-    inputIsRequired: true,
-    inputMaxlength: 1000,
-  };
+  inputType: 'text',
+  inputTestId: '',
+  inputChecked: false,
+  inputLabelTestId: '',
+  inputIsRequired: true,
+  inputMaxlength: 250,
+};
 
 export default Input;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,29 +3,38 @@ import Proptypes from 'prop-types';
 import Input from './Input';
 
 class SearchBar extends React.Component {
-    render() {
-     const {
-        searchText,
-        onSearchTextChange,
-        bookmarkedOnly,
-        onBookmarkedChange,
-        selectedGenre,
-        onSelectedGenreChange
-     } = this.props;
-     
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+    } = this.props;
+
     return (
       <section>
         <form data-testid="search-bar-form">
-         Searchbar form   
-        <Input
-         inputLabel="Inclui o texto:"
-         inputLabelTestId="text-input-label"
-         inputValue={ searchText }
-         onChangeInput={ onSearchTextChange }
-         inputTestId="text-input"
-         inputId="text-includes"
-         inputName="includes"
-        />
+          Searchbar form
+          <Input
+            inputLabel="Inclui o texto:"
+            inputLabelTestId="text-input-label"
+            inputValue={ searchText }
+            onChangeInput={ onSearchTextChange }
+            inputTestId="text-input"
+            inputId="text-includes"
+            inputName="includes"
+          />
+          <Input
+            inputType="checkbox"
+            inputLabel="Mostrar somente favoritos"
+            inputLabelTestId="checkbox-input-label"
+            inputValue="favorites"
+            inputChecked={ bookmarkedOnly }
+            onChangeInput={ onBookmarkedChange }
+            inputId="input-favorite"
+            inputTestId="checkbox-input"
+            inputName="favorites"
+          />
         </form>
       </section>
     );
@@ -33,12 +42,10 @@ class SearchBar extends React.Component {
 }
 
 SearchBar.propTypes = {
-    searchText: Proptypes.string.isRequired,
-    onSearchTexChange: Proptypes.func.isRequired,
-    bookmarkedOnly: Proptypes.bool.isRequired,
-    onBookmarkedChange: Proptypes.func.isRequired,
-    selectedGenre: Proptypes.string.isRequired,
-    onSelectedGenreChange: Proptypes.func.isRequired,
+  searchText: Proptypes.string.isRequired,
+  onSearchTextChange: Proptypes.func.isRequired,
+  bookmarkedOnly: Proptypes.bool.isRequired,
+  onBookmarkedChange: Proptypes.func.isRequired,
 };
 
 export default SearchBar;
