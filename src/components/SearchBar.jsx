855f016724123dcf6953a3a8b17840a8752d242c
jmlinger <jmlinger10@gmail.com>
2021-06-30 10:29:47 -0300

    Resolve requisito 2: renderizou-se um form em SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, 
+      selectedGenre, onSelectedGenreChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
