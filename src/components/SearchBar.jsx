b17491a8073b1623b849afed530d929bc3e8608b
Heder Waine <hederw@hotmail.com>
2021-06-30 18:09:49 -0300

    Requisitos 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,7 +13,21 @@ class SearchBar extends React.Component {
     return (
       <form data-testid="search-bar-form">
           <label data-testid="text-input-label">Inclui o texto:</label>
-          <input value={ searchText } onChange={ onSearchTextChange} data-testid="text-input"></input>
+          <input 
+          type="text"
+          value={ searchText } 
+          onChange={ onSearchTextChange} 
+          data-testid="text-input">
+
+          </input>
+
+          <label data-testid="checkbox-input-label">Mostrar somente favoritos
+          </label>
+          <input
+          type="checkbox"
+          checked={ bookmarkedOnly } 
+          onChange={ onBookmarkedChange } 
+          data-testid="checkbox-input"></input>
       </form>
     );
   }
