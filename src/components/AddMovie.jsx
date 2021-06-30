155cfc40778fcd989e1dd983148233107b553801
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-06-30 19:32:50 -0300

    req 8 - cria input text para add titulo + handleChange

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,6 +6,8 @@ class AddMovie extends Component {
   constructor() {
     super();
 
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
       subtitle: '',
       title: '',
@@ -16,13 +18,38 @@ class AddMovie extends Component {
     };
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  renderTitles() {
+    const { title } = this.state;
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Título
+        <input id="title-input" data-testid="title-input" />
+        <input
+          name="title"
+          type="text"
+          id="title-input"
+          data-testid="title-input"
+          value={ title }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
   render() {
     const { onClick } = this.props;
-
     return (
       <div>
+        <h2>Adiciona Filme</h2>
         <form data-testid="add-movie-form">
-          Adicionar Filme
+          { this.renderTitles() }
         </form>
       </div>
     );
@@ -30,7 +57,7 @@ class AddMovie extends Component {
 }
 
 AddMovie.propTypes = {
-  onClick: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
