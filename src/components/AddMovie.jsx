127ad5085283c13e4b0d1849ced6ad411055da9a
thales <thalesdaiel2013@gmail.com>
2021-06-30 02:24:47 -0300

    feito etapa 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovieTitle from './AddMovieTitle ';
 import AddMovieSubTitle from './AddMovieSubtitle';
 import AddMovieGenre from './AddMovieGenre';
@@ -18,11 +19,25 @@ class AddMovie extends React.Component {
       genre: 'action',
     };
     this.changeValue = this.changeValue.bind(this);
+    this.returnValue = this.returnValue.bind(this);
   }
 
   changeValue(event) {
     this.setState({
       [event.target.name]: event.target.value });
+  }
+
+  returnValue() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
   }
 
   render() {
@@ -35,8 +50,15 @@ class AddMovie extends React.Component {
         <AddMovieStoryline onChange={ this.changeValue } value={ storyline } />
         <AddMovieRating onChange={ this.changeValue } value={ rating } />
         <AddMovieGenre onChange={ this.changeValue } value={ genre } />
+        <button onClick={ this.returnValue } data-testid="send-button" type="button">
+          Adicionar filme
+        </button>
       </form>
     );
   }
 }
 export default AddMovie;
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
