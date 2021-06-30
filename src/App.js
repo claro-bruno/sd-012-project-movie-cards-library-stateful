b2a9bc08f404fbf80609df384d7755b96e6b175a
André Dantas <aluizcfd@gmail.com>
2021-06-30 04:36:52 -0300

    Renderização de MovieLibrary em App.js, início da criação do componente MovieLibrary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
+import moviesList from './data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <AddMovie />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesList,
+    };
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className="App">
+        <Header />
+        <MovieLibrary movies={ movies } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies:
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Placeholder</h1>
+      </div>
+    )
+  }
+}
+
+export default MovieLibrary;
