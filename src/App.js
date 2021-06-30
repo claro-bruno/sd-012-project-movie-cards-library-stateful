c9c3e03445b7ba2e35bb6a3c658964ca44452969
Heder Waine <hederw@hotmail.com>
2021-06-30 16:36:14 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import MovieLibrary from './components/MovieLibrary'
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
 function App() {
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import movies from '../data';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
-    render() {
-        return(
-            <div> ola</div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
 }
 
 export default MovieLibrary;
