9da962df7689016fecb582238222d3d4e23156bb
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-06-30 18:42:20 -0300

    req 6- cria componente add movie com estrutura basica

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  render () {
+    return (
+      <div>
+        Adiciona Filme
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
