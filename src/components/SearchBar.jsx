d96f5f8d1565ae90f4b7152b7de77e729a22e8ce
Stefano <stefanooliveira1@gmail.com>
2021-06-30 18:57:55 -0300

    Requirement 1 - SearchBar component created and propTypes set

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,24 @@
 // implement SearchBar component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class SearchBar extends React.Component {
+  render(){
+    return (
+
+    )
+  }
+}
+
+SearchBar.propTypes = {
+  SearchBar: PropType.shape({
+    searchText: PropType.string,
+    onSearchTextChange: PropType.func,
+    bookmarkedOnly: PropType.bool,
+    onBookmarkedChange: PropType.func,
+    selectedGenre: PropType.string,
+    onSelectedGenreChange: PropType.func,
+  })
+};
+
+export default SearchBar;
