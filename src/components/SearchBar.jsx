454560b822d45196dd95127956a0eeee23cd0a46
Stefano <stefanooliveira1@gmail.com>
2021-06-30 19:24:23 -0300

    Req 2 and 3 - Implements props, form and input boxes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,36 @@
 // implement SearchBar component here
-import React from 'react';
+import React, { Component } from 'react';
 import PropType from 'prop-types';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render(){
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
     return (
-
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+        Inclui o texto:
+        <input type="text" value={ this.searchText } onChange={ this.onSearchTextChange } />
+        </label>
+      </form>
     )
   }
 }
 
 SearchBar.propTypes = {
   SearchBar: PropType.shape({
-    searchText: PropType.string,
-    onSearchTextChange: PropType.func,
-    bookmarkedOnly: PropType.bool,
-    onBookmarkedChange: PropType.func,
-    selectedGenre: PropType.string,
-    onSelectedGenreChange: PropType.func,
+    searchText: PropType.string.isRequired,
+    onSearchTextChange: PropType.func.isRequired,
+    bookmarkedOnly: PropType.bool.isRequired,
+    onBookmarkedChange: PropType.func.isRequired,
+    selectedGenre: PropType.string.isRequired,
+    onSelectedGenreChange: PropType.func.isRequired,
   })
 };
 
