faaded45b6a38a77f8ce6be77a318f4b50c1aae3
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-06-30 14:51:59 -0300

    req 1 - cria componente SearchBar e atribui props e proptypes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,33 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render () {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <div>
+        <h2>Search Bar</h2>
+      </div>
+    );
+  }
+}
+
+SearchBar.PropTypes = {
+searchText: PropTypes.string.isRequired,
+onSearchTextChange: PropTypes.func.isRequired,
+bookmarkedOnly: PropTypes.bool.isRequired,
+onBookmarkedChange: PropTypes.func.isRequired,
+selectedGenre: PropTypes.string.isRequired,
+onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
