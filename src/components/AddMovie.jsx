0950d4beeabf3fa9fd680e4d801e2ef69425601d
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-30 14:12:07 -0300

    requisito 8 feito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,11 +2,39 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      title: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
   render() {
+    const { title } = this.state;
     return (
       <form
         data-testid="add-movie-form"
-      />
+      >
+        <label
+          htmlFor="formText"
+          data-testid="title-input-label"
+        >
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
