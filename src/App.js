1df7147b812030520ab797f5ab695c99d03ee5e5
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-30 13:47:47 -0300

    requisito 6 feito

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,12 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default AddMovie;
