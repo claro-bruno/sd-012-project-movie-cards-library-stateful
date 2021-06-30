04b4f2e4e833073a0e4edaa0595f5f1367e4609b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-30 00:17:46 -0300

    corrige component AddMovie e conclui requisito_14 - Renderize um botão do formulário em <AddMovie /> para fazer uso dos dados do novo filme, contidos no estado de <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import InputTitle from './ComponentsAddMovie/InputTitle';
 import InputSubtitle from './ComponentsAddMovie/InputSubtitle';
 import InputImage from './ComponentsAddMovie/InputImage';
-import InputStoryine from './ComponentsAddMovie/InputStoryline';
+import TextAreaStoryline from './ComponentsAddMovie/TextAreaStoryline';
 import InputRating from './ComponentsAddMovie/InputRating';
 import SelectGenre from './ComponentsAddMovie/SelectGenre';
 
@@ -20,7 +21,7 @@ class AddMovie extends React.Component {
     super();
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClear = this.handleClear.bind(this);
 
     this.state = initialState;
   }
@@ -34,26 +35,29 @@ class AddMovie extends React.Component {
     });
   }
 
-  handleSubmit(event) {
+  handleClear(event) {
     event.preventDefault();
+    const { onClick } = this.props;
+    onClick();
     this.setState(initialState);
   }
 
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
-    const { handleChange, handleSubmit } = this;
+    const { handleChange, handleClear } = this;
+
     return (
       <form data-testid="add-movie-form">
         <InputTitle title={ title } handleChange={ handleChange } />
         <InputSubtitle subtitle={ subtitle } handleChange={ handleChange } />
         <InputImage imagePath={ imagePath } handleChange={ handleChange } />
-        <InputStoryine storyline={ storyline } handleChange={ handleChange } />
+        <TextAreaStoryline storyline={ storyline } handleChange={ handleChange } />
         <InputRating rating={ rating } handleChange={ handleChange } />
         <SelectGenre genre={ genre } handleChange={ handleChange } />
         <button
           type="submit"
           data-testid="send-button"
-          onClick={ handleSubmit }
+          onClick={ handleClear }
         >
           Adicionar filme
         </button>
@@ -61,5 +65,9 @@ class AddMovie extends React.Component {
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func,
+}.isRequired;
 
 export default AddMovie;
