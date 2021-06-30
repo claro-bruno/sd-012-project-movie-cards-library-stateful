b086733b11b8237cca5fd31a06d831607f43b313
gscuri <gscuri6@hotmail.com>
2021-06-29 23:08:03 -0300

    exercicio 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         onBookmarkedChange={ onBookmarkedChange }
         selectedGenre={ selectedGenre }
         onSelectedGenreChange={ onSelectedGenreChange }
+      />
+      <AddMovie
+        onClick={ onClick }
       />
     </div>
   );
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,20 @@
 // implement AddMovie component here
 import React from 'react';
+import PropType from 'prop-types';
 
 class AddMovie extends React.Component {
   render() {
+    const { onClick } = this.props;
     return (
-      <section />
+      <div>
+        <form data-testid="add-movie-form" />
+      </div>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropType.func.isRequired,
+};
 
 export default AddMovie;
