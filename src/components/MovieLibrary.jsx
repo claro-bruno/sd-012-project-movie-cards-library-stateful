1c87595cad8f9ff0dfcde1fc9b093bf2b9d7238a
Euller Braz <euller.braz99@gmail.com>
2021-07-01 13:45:31 -0300

    Requisito 18: Adicionado Filtragem em MovieList

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -19,10 +19,38 @@ class MovieLibrary extends React.Component {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
-    this.setState({
+    this.setState(() => ({
       [name]: value,
-    });
+    }), this.filterMovies);
   }
+
+  filterGenre = (movies, selectedGenre) => {
+    const genreMovies = movies.filter((movie) => movie.genre === selectedGenre);
+    return genreMovies;
+  }
+
+  filterMarks = (movies, bookmarkedOnly) => {
+    const markedMovies = movies.filter((movie) => movie.bookmarked === bookmarkedOnly);
+    return markedMovies;
+  }
+
+  filterByText = (movies, searchText) => {
+    const filteredMovies = movies.filter((movie) => movie.title.includes(searchText)
+    || movie.subtitle.includes(searchText) || movie.storyline.includes(searchText));
+    return filteredMovies;
+  }
+
+  filterMovies = () => {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { movies } = this.props;
+    let filteredMovies = [...movies];
+
+    if (selectedGenre) filteredMovies = this.filterGenre(filteredMovies, selectedGenre);
+    if (bookmarkedOnly) filteredMovies = this.filterMarks(filteredMovies, bookmarkedOnly);
+    if (searchText) filteredMovies = this.filterByText(filteredMovies, searchText);
+
+    this.setState({ movies: filteredMovies });
+  };
 
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
