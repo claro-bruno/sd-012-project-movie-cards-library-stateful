148c53d94b48dce997956ea6fbf2abbc29e77e55
jmlinger <jmlinger10@gmail.com>
2021-06-30 22:22:47 -0300

    Resolve requisito 4 e 5: renderizou-se uma label com um input tipo checkbox associado e uma outra label com um select associado, assim como suas props e propTypes devidas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,13 +15,37 @@ class SearchBar extends Component {
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
-          <imput
+          <input
             name="searchText"
             type="text"
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
           />
+        </label>
+
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
@@ -30,12 +54,21 @@ class SearchBar extends Component {
 
 export default SearchBar;
 
+// Referência: Natalia Souza - Turma 11.
 SearchBar.propTypes = {
   searchText: PropTypes.string,
   onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
 };
 
 SearchBar.defaultProps = {
   searchText: '',
   onSearchTextChange: () => {},
+  bookmarkedOnly: false,
+  onBookmarkedChange: () => {},
+  selectedGenre: '',
+  onSelectedGenreChange: () => {},
 };
