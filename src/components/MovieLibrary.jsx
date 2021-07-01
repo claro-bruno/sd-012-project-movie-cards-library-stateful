a82f0fe84f4e8a12f63c351b67ff13b1b01e7821
CamilaDamasio <camiladamasioef@hotmail.com>
2021-07-01 20:27:00 -0300

    Verifica requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,32 +1,39 @@
-// import React from 'react';
+import React from 'react';
+// import PropTypes from 'prop-types';
 // import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends React.Component {
-//   // constructor() {
-//   //   super();
-//   //   this.state = {
-//   //     searchText: '',
-//   //     bookmarkedOnly: false,
-//   //     selectedGenre: '',
-//   //     movies: '',
-//   //     // - `movies`: a lista de filmes passadas pela props `movies`.
-//   //   };
-//   // }
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // searchText: '',
+      // bookmarkedOnly: false,
+      // selectedGenre: '',
+      // movies: props.movies,
+      // - `movies`: a lista de filmes passadas pela props `movies`.
+    };
+  }
 
-//   render() {
-//     const onClick = (objeto) => console.log(objeto);
-//     const movies = (objeto) => objeto;
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={ movies } />
-//         <AddMovie onClick={ onClick } />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    const onClick = (objeto) => console.log(objeto);
+    // const movies = (objeto) => objeto;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        {/* <MovieList movies={ movies } /> */}
+        <AddMovie onClick={ onClick } />
+      </div>
+    );
+  }
+}
 
-// export default MovieLibrary;
+// MovieLibrary.propTypes = {
+//   movies: PropTypes.arrayOf(
+//     PropTypes.object,
+//   ).isRequired,
+// };
+
+export default MovieLibrary;
