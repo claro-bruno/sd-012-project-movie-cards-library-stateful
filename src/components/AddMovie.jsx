104f3b59b0b2d6d18daa733317abc5374146d61a
Stefano <stefanooliveira1@gmail.com>
2021-06-30 23:57:49 -0300

    Req ...7 - Implements AddMovie components first steps, adds form and state

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,26 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <form action="" data-testid="add-movie-form">
+
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
