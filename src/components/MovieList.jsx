5a97c92e59de2292db2b01fb9474898561dd1156
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-30 22:56:47 -0300

    corrige proptype do component Movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -9,14 +9,16 @@ class MovieList extends React.Component {
 
     return (
       <div data-testid="movie-list" className="movie-list">
-        { movies().map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.func.isRequired,
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
 };
 
 export default MovieList;
