d26ff1f4ea9306ad4291af8bc9a4603c0570e16b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-30 22:24:14 -0300

    altera propType do component MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -9,16 +9,14 @@ class MovieList extends React.Component {
 
     return (
       <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies().map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.object,
-  ).isRequired,
+  movies: PropTypes.func.isRequired,
 };
 
 export default MovieList;
