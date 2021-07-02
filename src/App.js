61d15d9f584804ce63df68038234d8bb4a4b61df
tiagoornelas <advtiagoornelas@gmail.com>
2021-07-02 16:42:42 -0300

    Resolve problema de loading da pagina

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieLibrary from './components/MovieLibrary';
+import movieList from './data';
 import './App.css';
 
 // prettier-ignore
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieLibrary />
+      <MovieLibrary movies={ movieList }/>
     </div>
   );
 }
