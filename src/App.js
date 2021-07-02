b7a801713933609d34672f878b0c901dd432d6fd
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-07-02 19:02:36 -0300

    requisitos 6, 7 e 8

--- src/App.js
+++ src/App.js
@@ -2,30 +2,51 @@ import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
 import movies from './data';
 import './App.css';
 
-function App() {
-  const searchTextChange = () => {
-  };
-  const bookmarkedChange = () => {
-  };
-  const selectedGenreChange = () => {
-  };
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar
-        searchText=""
-        onSearchTextChange={ searchTextChange }
-        bookmarkedOnly
-        onBookmarkedChange={ bookmarkedChange }
-        selectedGenre=""
-        onSelectedGenreChange={ selectedGenreChange }
-      />
-      <MovieList movies={ movies } />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.searchTextChange = this.searchTextChange.bind(props);
+    this.bookmarkedChange = this.bookmarkedChange.bind(props);
+    this.selectedGenreChange = this.selectedGenreChange.bind(props);
+  }
+
+  render() {
+    const searchTextChange = () => {
+    };
+    const bookmarkedChange = () => {
+    };
+    const selectedGenreChange = () => {
+    };
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          searchText=""
+          onSearchTextChange={ searchTextChange }
+          bookmarkedOnly
+          onBookmarkedChange={ bookmarkedChange }
+          selectedGenre=""
+          onSelectedGenreChange={ selectedGenreChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie
+          onClick={ handleClick }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,26 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <form action="" data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.action,
+}.isRequired;
+
+export default AddMovie;
