8b8bcb36ed3ce8e3f381877b94072fab3d17c87a
MaxAlves0035 <maxalves2@hotmail.com>
2021-07-02 16:02:34 -0300

    requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,30 +12,42 @@ class SearchBar extends React.Component {
     } = this.props;
 
     return (
-      <div>
-        <form data-testid="search-bar-form">
-          <label htmlFor="text-input" data-testid="text-input-label">
-            Inclui o texto
-            <input
-              type="text"
-              data-testid="text-input"
-              name="search-bar"
-              value={ searchText }
-              onChange={ onSearchTextChange }
-            />
-          </label>
-          <label htmlFor="text-input" data-testid="checkbox-input-label">
-            Mostrar somente favoritos
-            <input
-              type="checkbox"
-              data-testid="checkbox-input"
-              name="favorite-only"
-              checked={ bookmarkedOnly }
-              onChange={ onBookmarkedChange }
-            />
-          </label>
-        </form>
-      </div>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            type="text"
+            data-testid="text-input"
+            name="search-bar"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="text-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            name="favorite-only"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label htmlFor="filter" data-testid="select-input-label">
+          Filtrar por gênero
+          <select 
+            data-testid="select-input"
+            name="filter"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
