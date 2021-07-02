30f049ff656e6c57d0ce9aae4eb230f07151f1b1
Janderson Soares <jandinsm@gmail.com>
2021-07-02 01:59:15 -0300

    Corrigi a ordem de props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import MovieList from './components/MovieList';
+
+import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
+      <MovieLibrary />
+      <SearchBar />
     </div>
   );
 }
