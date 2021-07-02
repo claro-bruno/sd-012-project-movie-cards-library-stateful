4cb3fe76596840917bd9f09709ff17da8db7bfc0
MaxAlves0035 <maxalves2@hotmail.com>
2021-07-02 16:06:45 -0300

    PropTypes SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -35,7 +36,7 @@ class SearchBar extends React.Component {
         </label>
         <label htmlFor="filter" data-testid="select-input-label">
           Filtrar por gênero
-          <select 
+          <select
             data-testid="select-input"
             name="filter"
             value={ selectedGenre }
@@ -51,5 +52,14 @@ class SearchBar extends React.Component {
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
