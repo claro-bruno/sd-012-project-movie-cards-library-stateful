4d1eb82c46d616b1f7f549fd8a8241308dc4b8d4
MGSiqueira <marcos.siqueira98@gmail.com>
2021-07-02 10:51:33 -0300

    Faltou uma lintada

--- src/components/AddMovieSelect.jsx
+++ src/components/AddMovieSelect.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class AddMovieSelect extends React.Component {
   render() {
@@ -20,3 +21,8 @@ export default class AddMovieSelect extends React.Component {
     );
   }
 }
+
+AddMovieSelect.propType = ({
+  genre: PropTypes.string,
+  handleChange: PropTypes.func,
+}).isRequired;
