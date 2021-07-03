b86bb50bdb281c969090c6bf738bf072111b035e
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-03 20:18:20 -0300

    Adiciona funcionalidades e prop-types ao componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,52 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkerdOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
+  handleClick({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkerdOnly, selectedGenre, movies } = this.state;
+    return (
+      <section>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleClick }
+          bookmarkedOnly={ bookmarkerdOnly }
+          onBookmarkedChange={ this.handleClick }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleClick }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </section>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+
+export default MovieLibrary;
