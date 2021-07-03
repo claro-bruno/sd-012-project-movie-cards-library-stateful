1d06257e60c710814b681a0567777239ed08ea15
Martin <escorpmartin97@gmail.com>
2021-07-03 20:26:57 -0300

    req 8, title do filme

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
-  render() {
-    const INITIAL_STATE = {
-      subtitle: '',
+  constructor() {
+    super();
+    this.state = {
       title: '',
-      imagemPath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
     };
 
-    const { onClick } = this.props;
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    // const { onClick } = this.props;
+    const { title } = this.state;
     return (
       <div>
         <form data-testid="add-movie-form">
-          <h1>meuForm</h1>
+          <section>
+            <label htmlFor="title" data-testid="title-input-label">
+              Título
+              <input
+                data-testid="title-input"
+                type="text"
+                name="title"
+                value={ title }
+                onChange={ this.changeHandler }
+              />
+            </label>
+          </section>
         </form>
       </div>
     );
   }
 }
 
-AddMovie.propTypes = {
-  onClick: PropTypes.func.isRequired,
-};
+// AddMovie.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+// };
 export default AddMovie;
