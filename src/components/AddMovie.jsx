bbccccd2b27b66cf899fa7f511fe2043629ad839
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-03 20:45:06 -0300

    resolve 9,10911 - renderiza inputs subtitulo,image e textarea

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,6 +12,26 @@ class AddMovie extends React.Component {
               type="text"
             />
           </label>
+          <label data-testid="subtitle-input-label" htmlFor="subtitle-input-label">
+          Subtítulo
+          <input
+            data-testid="subtitle-input"
+            type="text"
+          />
+          <label data-testid="image-input-label" htmlFor="image-input-label">
+          Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+          />
+          </label>
+          </label>
+          <label data-testid="storyline-input-label" htmlFor="storyline-input-label">
+            Sinopse
+            <textarea
+            data-testid="storyline-input"
+            />
+          </label>
         </form>
       </section>
     );
