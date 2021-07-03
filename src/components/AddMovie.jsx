fa6117dbc6c470890d18b7829073d0299ba54685
Martin <escorpmartin97@gmail.com>
2021-07-03 17:07:11 -0300

    req 6, criando componente AddMovie e dou valor inicial para eles

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
-// hello
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  render() {
+    const INITIAL_STATE = {
+      subtitle: '',
+      title: '',
+      imagemPath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    const { onClick } = this.props;
+    return (
+      <div>AddMovie</div>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+export default AddMovie;
