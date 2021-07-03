0161f2d9df04ec27f78c64565661560d7e520941
jmlinger <jmlinger10@gmail.com>
2021-07-02 22:35:38 -0300

    Resolve requisito 6: criou-se o componente AddMovie e adicionou-se a ele um estado inicial

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form />
+    );
+  }
+}
+
+export default AddMovie;
