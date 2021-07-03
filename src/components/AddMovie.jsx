1781cb653fadea9aabe3ffb205974b1b7a1f3393
jmlinger <jmlinger10@gmail.com>
2021-07-02 23:34:48 -0300

    Resolve requisito 9: renderizou-se uma label com um input que recebe o subtitulo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,9 +14,39 @@ class AddMovie extends Component {
     };
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          <input
+            name="title"
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          <input
+            name="subtitle"
+            type="text"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
