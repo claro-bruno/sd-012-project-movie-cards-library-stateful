3ca5aa1dcd50481a3b57784d38678d467b9e604d
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-03 20:12:02 -0300

    Refatora componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import InputImg from './InputImg';
+import InputRating from './InputRating';
+import InputSubtitle from './InputSubtitle';
+import InputTitle from './InputTitle';
+import SelectGenre from './SelectGenre';
+import TextArea from './TextArea';
 
 class AddMovie extends React.Component {
   constructor() {
     super();
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       subtitle: '',
       title: '',
@@ -13,74 +20,24 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleClick({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="input-title" data-testid="title-input-label">
-          Título
-          <input
-            type="text"
-            id="input-title"
-            data-testid="title-input"
-            value={}
-            onChange={}
-          />
-        </label>
-        <label htmlFor="input-subtitle" data-testid="subtitle-input-label">
-           Subtítulo
-           <input
-           type="text"
-           id="input-subtitle"
-           data-testid="subtitle-input"
-           value={}
-           onChange={}
-            />
-        </label>
-        <label htmlFor="input-img" data-testid="image-input-label" >
-            Imagem
-            <input
-            type="text"
-            id="input-img"
-            data-testid="image-input"
-            value={}
-            onChange={}
-             />
-        </label>
-        <label htmlFor="input-sinopse" data-testid="storyline-input-label" >
-             Sinopse 
-             <textarea
-             id="input-sinopse"
-             data-testid="storyline-input"
-             onChange={}
-             >{''}</textarea>
-        </label>
-        <label htmlFor="input-rating" data-testid="rating-input-label" >
-             Avaliação
-             <input
-             type="number"
-             id="input-rating"
-             data-testid="rating-input"
-             value={} 
-             onChange={}
-             />
-        </label>
-        <label htmlFor="select-genre" data-testid="genre-input-label" >
-             Gênero
-             <select 
-            id="select-genre" 
-            data-testid="genre-input"
-            value={}
-            onChange={}
-            >
-             <option data-testid="genre-option" value="action">Ação</option>
-             <option data-testid="genre-option" value="comedy">Comédia</option>
-             <option data-testid="genre-option" value="thriller">Suspense</option>
-            </select>
-        </label>
-        <button
-        data-testid="send-button"
-        onClick={}
-        >Adicionar filme</button>
+        <InputTitle value={ title } onChange={ this.handleClick } />
+        <InputSubtitle value={ subtitle } onChange={ this.handleClick } />
+        <InputImg value={ imagePath } onChange={ this.handleClick } />
+        <TextArea value={ storyline } onChange={ this.handleClick } />
+        <InputRating value={ rating } onChange={ this.handleClick } />
+        <SelectGenre value={ genre } onChange={ this.handleClick } />
       </form>
     );
   }
