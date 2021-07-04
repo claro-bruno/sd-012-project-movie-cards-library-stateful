010e200627d9925b0fc67710cd216c4d1093c138
Gustavo-Mourao <gcmbh1@gmail.com>
2021-07-04 15:12:14 -0300

    Q 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,11 @@
 // implement AddMovie component here
+import React from 'react';
+import Form from './Form';
+
+class AddMovie extends React.Component {
+  render() {
+    return (<Form />);
+  }
+}
+
+export default AddMovie;
---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form />);
+  }
+}
+
+export default Form;
