dc8332903dc0ecc3e02b48249a94bbcd12d695f1
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-04 09:26:35 -0300

    Requisito 1.Criando componente SearchBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+// import movies from './data';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+// import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
 // implement SearchBar component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange  } = this.props;
+    return (
+      <div>
+        ...
+      </div>
+    );
+  }
+}
+
+// Rating.propTypes = { rating: PropTypes.number };
+
+// Rating.defaultProps = {
+//   rating: 'undefined',
+// };
+
+export default SearchBar;
