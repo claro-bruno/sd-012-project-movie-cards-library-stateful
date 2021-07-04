8a6cdc18d6e1d8fa89f4536ac78710c60f1cfdc3
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-04 09:29:11 -0300

    Requisito 2.Renderizando um formulário dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,7 +7,9 @@ class SearchBar extends React.Component {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange  } = this.props;
     return (
       <div>
-        ...
+        <form data-testid="search-bar-form">
+          ...
+        </form>
       </div>
     );
   }
