3e3fa3e501a645d2ef0080626b5392a620fd8ed5
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-07-04 19:00:36 -0300

    validacao props movieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -79,4 +79,16 @@ class MovieLibrary extends React.Component {
   }
 }
 
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+};
+
 export default MovieLibrary;
