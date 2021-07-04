42042c7a7226e69dc110d707342e5904e3683440
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-04 11:28:39 -0300

    Adicionado input no componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,22 +3,43 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       // subtitle: '',
-      // title: '',
+      title: '',
       // imagePath: '',
       // storyline: '',
       // rating: 0,
       // genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      title: e.target.value,
+    });
   }
 
   render() {
     // const { onClick } = this.props;
+
+    const { title } = this.state;
+
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
