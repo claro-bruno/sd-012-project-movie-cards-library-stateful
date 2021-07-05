f5d8a11d07c1b9ec055dfeaf932b40a7c41b6123
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-05 10:50:52 -0300

    Inicio da implementaçao do componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,12 @@
 // implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <section />
+    );
+  }
+}
+
+export default MovieLibrary;
