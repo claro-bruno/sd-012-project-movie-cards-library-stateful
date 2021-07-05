e27e99efad217e8a94ccb6bf69f4c9e189f8f482
Victor Emmanuel <victorpassos2012@gmail.com>
2021-07-05 19:21:22 -0300

    Requisito 15 feito

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
 // vai usar state
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return 0;
+  }
+}
+
+export default MovieLibrary;
