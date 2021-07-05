7634a4f1a9958cff35a87783a270c33694ef74fd
Kevin Willian <kevinwillian084@gmail.com>
2021-07-05 14:34:15 -0300

    Adiciona requisitos 8 e 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Title from './Title';
+import Subtitle from './Subtitle';
 
 class AddMovie extends Component {
   constructor() {
@@ -12,15 +14,32 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { target } = e;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? 'checked' : target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
+        <Title title={ title } onChange={ this.handleChange } />
+        <Subtitle subtitle={ subtitle } onChange={ this.handleChange } />
       </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func,
+}.isRequired;
 
 export default AddMovie;
