fe7ea2bc8e84bef03e5c719e7bd8a06bc52c221d
Eurides <euridesalves@yahoo.com.br>
2021-07-05 14:45:15 -0300

    Adicionando requisitos 03, 04 e 05

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
     const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      <form data-testid="search-bar-form">
-        <p> </p>
+      <form action="" data-testid="search-bar-form">
+        <label htmlFor="text" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            id="text"
+          />
+        </label>
+        <label htmlFor="input1" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            id="input1"
+            checked={ bookmarkedOnly }
+          />
+        </label>
+        <label htmlFor="select" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name="select"
+            onChange={ onSelectedGenreChange }
+            id="select"
+            value={ selectedGenre }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
@@ -23,13 +57,3 @@ SearchBar.propTypes = ({
 }).isRequired;
 
 export default SearchBar;
-/* <label for="" data-testid="text-input-label">
-          Inclui o texto:
-          <input 
-            type="text" 
-            data-testid="text-input" 
-            name="" value={ searchText } 
-            onChange={ onSearchTextChange }>
-
-          </input>
-        </label> */
