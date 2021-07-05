61c678829a31ea7c66c0d150f7ef4a79711115e0
Eric Faria <ericdeofaria@gmail.com>
2021-07-05 20:06:07 -0300

    Requisitos 15 a 17

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+
+export default MovieLibrary;
