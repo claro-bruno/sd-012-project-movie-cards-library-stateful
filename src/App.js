7f75cd01e0294848116e129054fa5b459a8d71ee
Dayvid Ruan <ruan12bbg@gmail.com>
2021-07-05 18:44:44 -0300

    Requisito 15 e 17

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import movies from './data';
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
-        <AddMovie />
+        <MovieLibrary movies={ movies } />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
