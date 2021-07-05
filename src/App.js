584f5c94a5b78294526e4006de587e390d9c8731
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-05 00:05:59 -0300

    Renderiza MovieLibrary no componente App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
-import MovieList from './components/MovieList';
+import MovieLibrary from './components/MovieLibrary';
+import movies from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
