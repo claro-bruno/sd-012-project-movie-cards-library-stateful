131bbc227cf585c23912a022f8c188f09c6ab0c8
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-07-05 01:16:56 -0300

    add req 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,34 +3,55 @@ import React from 'react';
 // import Rating from './Rating';
 
 class AddMovie extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       subtitle: '',
-//       title: '',
-//       imagePath: '',
-//       storyline: '',
-//       rating: 0,
-//       genre: 'action',
-//     };
-//     this.addMovie = this.addMovie.bind(this);
-//   }
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+    //   imagePath: '',
+    //   storyline: '',
+    //   rating: 0,
+    //   genre: 'action',
+    };
+    this.handleAddMovie = this.handleAddMovie.bind(this);
+  }
 
-  //   addMovie = () => {
-  //     this.setState((subtitulo, titulo, imagem, sinopse, nota, genero) => ({
-  //       subtitle: subtitulo,
-  //       title: titulo,
-  //       imagePath: imagem,
-  //       storyline: sinopse,
-  //       rating: nota,
-  //       genre: genero,
-  //     }));
-  //   }
+  handleAddMovie(event) {
+    this.setState({
+      subtitle: event.target.subtitle,
+      title: event.target.title,
+    //   imagePath: event.target.imagePath,
+    //   storyline: event.target.storyline,
+    //   rating: event.target.rating,
+    //   genre: event.target.genre,
+    });
+  }
 
   render() {
     // const { onClick } = this.props;
+    const { title, subtitle } = this.state;
     return (
-      <form data-testid="add-movie-form"> </form>
+      <form data-testid="add-movie-form">
+        <label htmlFor="titulo" data-testid="title-input-label">
+          Título
+          <input
+            id="titulo"
+            type="text"
+            value={ title }
+            onChange={ this.handleAddMovie }
+            data-testid="title-input"
+          />
+        </label>
+        <label htmlFor="subtitulo" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            id="subtitulo"
+            value={ subtitle }
+            onChange={ this.handleAddMovie }
+            data-testid="subtitle-input"
+          />
+        </label>
+      </form>
     );
   }
 }
