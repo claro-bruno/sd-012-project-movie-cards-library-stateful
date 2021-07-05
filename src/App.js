e33b4fa1ccd1604b4dafaa55e06898b8a83e96c9
paulomichael <paulo.michael@gmail.com>
2021-07-04 22:50:25 -0300

    requisito 15 - cria um componente chamado <MovieLibrary />

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
-import AddMovie from './components/AddMovie';
 import './App.css';
-
-function handleClick(receivedState) {
-  console.log('handleClick was called', receivedState);
-}
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AddMovie onClick={ handleClick } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,2 +1,14 @@
-// implement MovieLibrary component here
-//
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <span>MovieLibrary Component</span>
+    );
+  }
+}
+
+// MovieLibrary.propTypes = {};
+
+export default MovieLibrary;
