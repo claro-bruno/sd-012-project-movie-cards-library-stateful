78de411c97ef5ec1ef8019b9b4fe0e995c2f1824
Eric Faria <ericdeofaria@gmail.com>
2021-07-05 20:14:11 -0300

    Adicionando importação do Proptypes no arquivo MoovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends Component {
   render() {
