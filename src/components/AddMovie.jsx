84bf434a080920af7ea89bf943b9ca6b85ef704e
Ed Santos <edsantos2822@gmail.com>
2021-07-05 08:14:14 -0300

    Finalizando - Requisito 6 - Renderizando componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <span>só de test</span>
+    );
+  }
+}
+
+AddMovie.propType = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
