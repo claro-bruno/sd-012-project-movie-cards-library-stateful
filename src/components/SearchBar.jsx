f4d86798c0324623643d2709bccb6c256f858542
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-05 10:18:15 -0300

    adiciona requisito 1 lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    render() {
-        return <SearchBar />
-    }
+  render() {
+    return <SearchBar />
+  };
 }
 
 export default SearchBar;
