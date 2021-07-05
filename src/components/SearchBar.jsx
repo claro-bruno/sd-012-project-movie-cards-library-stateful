74ab6b71958e08e078f1804e59a9f8a54e9e84e2
Kevin Willian <kevinwillian084@gmail.com>
2021-07-04 21:33:03 -0300

    Adiciona requisitos 3 e 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Input from './Input';
 
 class SearchBar extends Component {
   constructor() {
@@ -11,16 +12,36 @@ class SearchBar extends Component {
 
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
-    const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { onBookmarkedChange } = this.props;
     return (
       <form data-testid="search-bar-form">
+        <Input
+          labelTestId="text-input-label"
+          labelText="Inclui o texto"
+          name="text-input"
+          inputTestId="text-input"
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+        <Input
+          labelTestId="checkbox-input-label"
+          labelText="Mostrar somente favoritos"
+          name="checkbox-input"
+          inputTestId="checkbox-input"
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+        />
       </form>
     );
   }
 }
 
 SearchBar.propTypes = {
-
-};
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+}.isRequired;
 
 export default SearchBar;
