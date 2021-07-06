58735c913e511d4c25caf9836a612c45dbf34c84
Victor <juanvictor70@gmail.com>
2021-07-06 09:03:13 -0400

    renderizando o AddMovie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 class App extends React.Component {
@@ -29,6 +30,7 @@ class App extends React.Component {
           selectedGenre={ genreOption }
           onSelectedGenreChange={ this.handleChange }
         />
+        <AddMovie />
       </div>
     );
   }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie">
+        <label htmlFor="movie-title" data-testid="title-input-label">
+          Título
+          <input
+            id="movieTitle"
+            data-testid="title-input"
+            type="text"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
