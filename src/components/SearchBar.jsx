1c2c8c511635df98eb19211fde56a78b28797708
AndZoni <lorenzoniandre@outlook.com>
2021-07-06 14:22:28 +0100

    Add Requirement 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,6 +31,20 @@ class SearchBar extends React.Component {
             onChange={ onBookmarkedChange }
           />
         </label>
+        <label htmlFor="Gênero" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+            name="Gênero"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
