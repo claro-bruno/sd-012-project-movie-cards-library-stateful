d84d958aeb4f7e351c71afee3d7996ec62a398c2
Wadson <wadson.dev92@gmail.com>
2021-07-05 21:35:54 -0300

    Requisito 1 ao 3 com ajuda da Gabriela Guerra

---
+++ src/components/Forms/Forms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+  render() {
+    return (
+      <form>
+        <h1>Formulário</h1>
+      </form>
+    );
+  }
+}
+
+export default Forms;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+
+      <form data-testid="search-bar-form">
+        <label htmlFor="texto" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            id="texto"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
