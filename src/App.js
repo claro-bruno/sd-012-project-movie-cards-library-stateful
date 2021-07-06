ca261fab0ff29df8afbefab053c199893cb3dc51
Martin <escorpmartin97@gmail.com>
2021-07-05 23:14:34 -0300

    16 e 17

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,31 +1,31 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// import PropTypes from 'prop-types';
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends Component {
+class MovieLibrary extends Component {
 //   constructor(props) {
 //     super(props);
 //   }
 
-//   render() {
-//     const { movies } = this.props;
+  render() {
+    const { movies } = this.props;
 
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={ movies } />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
 
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-// };
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
-// export default MovieLibrary;
+export default MovieLibrary;
