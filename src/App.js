2339efbdce73ed32937a6656c3adfda83da46599
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-07-06 14:29:00 -0300

    Refazendo o projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieList from './components/MovieList';
 import movies from './data';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    console.log(React.Component);
     return (
       <div className="App">
         <Header />
