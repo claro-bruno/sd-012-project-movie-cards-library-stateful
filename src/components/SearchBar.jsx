1a5ab2fd8e9929630edcab5c3d8bc225cc9036c5
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-05 22:47:08 -0300

    adiciona requisito 1 ao 5 com lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const {  
+    const {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
@@ -13,22 +13,47 @@ class SearchBar extends React.Component {
     } = this.props;
 
     return (
-      
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" />Inclui o texto:
-        <input 
+        <label htmlFor="texto" data-testid="text-input-label">
+          Inclui o texto:
+          <input
             type="text"
             id="texto"
             value={ searchText }
             onChange={ onSearchTextChange }
-            data-testid="text-input"          
-        />
+            data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="check" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="check"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="selected" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            id="selected"
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
 }
 
-SearchBar.propTypes={
+SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
@@ -38,3 +63,4 @@ SearchBar.propTypes={
 };
 
 export default SearchBar;
+
