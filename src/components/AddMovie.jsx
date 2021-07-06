a1ccb727342aa31aae1eb3d0c132ef67b204580d
Victor <juanvictor70@gmail.com>
2021-07-06 09:45:19 -0400

    iniciando a componentizacao do formulario da AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,14 +14,46 @@ class AddMovie extends React.Component {
   }
 
   render() {
+    const { movieTitle, movieSubtitle, imagePath } = this.state;
     return (
-      <form data-testid="add-movie">
+      <form data-testid="add-movie-form">
         <label htmlFor="movie-title" data-testid="title-input-label">
           Título
           <input
             id="movieTitle"
             data-testid="title-input"
             type="text"
+            value={ movieTitle }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="movie-subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            id="movieSubtitle"
+            data-testid="subtitle-input"
+            type="text"
+            value={ movieSubtitle }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="image-path" data-testid="image-input-label">
+          Imagem
+          <input
+            id="imagePath"
+            data-testid="image-input"
+            type="text"
+            value={ imagePath }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="storyLine-movie" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            id="storyLine"
+            data-testid="storyline-input"
+            type="text"
+            onChange={ this.handleChange }
           />
         </label>
       </form>
