b445532ddfc0b69e7d993c8d3c6565154dece56a
AndZoni <lorenzoniandre@outlook.com>
2021-07-06 14:34:36 +0100

    Add Requirement 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { onClick } = this.props;
+    return (
+      <p>a</p>
+    );
+  }
+}
+
+/* AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}; */
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
