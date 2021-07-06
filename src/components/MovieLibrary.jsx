c224e8fd06ee1da894bb0329d7f67fcce64d49d3
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-07-06 10:14:34 -0300

    requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,25 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
-export default class MovieLibrary extends Component {
+class MovieLibrary extends Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
-        ola
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
       </div>
     );
   }
 }
+
+export default MovieLibrary;
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
