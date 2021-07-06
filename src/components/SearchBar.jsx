7d4e246314e6064d73b1b96019c52977408259cf
uriel <urielbochi@gmail.com>
2021-07-06 15:52:07 -0300

    Requisito 4 + algumas correções no esLint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    render() {
-        const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props
-       
-    return(
-    <div>
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+
+    return (
+      <div>
         <form className="form" data-testid="search-bar-form">
-            <label data-testid="text-input-label"> Inclui o texto:
-            <input data-testid="text-input" type="text" value= { searchText } onChange={ onSearchTextChange }></input>
+          <label data-testid="text-input-label">
+            {' '}
+            Inclui o texto:
+            <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
+            <label data-testid="checkbox-input-label">
+              {' '}
+              Mostrar somente favoritos
+              <input data-testid="checkbox-input" type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
             </label>
+          </label>
         </form>
-    </div>
-    )
-    }
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
