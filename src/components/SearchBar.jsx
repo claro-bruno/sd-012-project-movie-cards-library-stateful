24ed1bef95255b6b42a5a2b36ce0949055cab525
AndZoni <lorenzoniandre@outlook.com>
2021-07-06 14:16:56 +0100

    Add Requirement 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,17 @@ class SearchBar extends React.Component {
       onSelectedGenreChange,
     } = this.props;
     return (
-      <form data-testid="search-bar-form"> </form>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input-label" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
     );
   }
 }
