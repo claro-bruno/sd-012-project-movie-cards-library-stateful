c6bbb1841a769156f5100fbcc72e1a556de34806
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-07-06 12:09:10 -0300

    Renderiza input do tipo texto associado a label, Requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,9 +6,21 @@ class SearchBar extends React.Component {
   } */
 
   render() {
+    const { onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <p>oi</p>
+        <label
+          htmlFor="IncluiTexto"
+          data-testid="text-input-label">
+          Inclui o texto:
+        </label>
+        <input
+          type="text"
+          id="IncluiTexto"
+          value="searchText"
+          onChange={onSearchTextChange}
+          data-testid="text-input"
+        />
       </form>
     );
   }
