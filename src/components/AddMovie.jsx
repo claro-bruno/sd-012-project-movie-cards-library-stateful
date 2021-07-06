5cbc21b7922d25fdf09faa1e43a1efef1056f940
walttoncoelho <walltton@outlook.com>
2021-07-06 19:58:33 -0300

    trabalhando no estado de AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,9 +6,60 @@ import Title from './addMovie/Title';
 import Rating from './addMovie/Rating';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      subtitle: '',
+      storyline: '',
+      imagePath: '',
+      genre: 'action',
+      rating: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMovie = this.submitMovie.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
+  submitMovie() {
+    // const { onClick } = this.props;
+    // A função onClick está apresentando um erro que ainda não identifiquei
+    const { title, subtitle, storyline,
+      imagePath, genre, rating } = this.state;
+
+    const updateMovies = {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+    };
+
+    onClick(updateMovies);
+
+    this.setState({
+      title: '',
+      subtitle: '',
+      storyline: '',
+      imagePath: '',
+      genre: 'action',
+      rating: 0,
+    });
+  }
+
   render() {
+    const { title, subtitle } = this.state;
+    const { imagePath, rating } = this.state;
+    const { storyline, genre } = this.state;
     return (
-      <form>
+      <form data-testid="add-movie-form" onSubmit={ (e) => e.preventDefault() }>
         <Title
           title={ title }
           handleChange={ this.handleChange }
@@ -51,4 +102,5 @@ class AddMovie extends React.Component {
     );
   }
 }
+
 export default AddMovie;
