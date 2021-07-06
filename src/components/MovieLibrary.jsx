e60cc1babbb6038969cad780e1831fa817346fd5
AndZoni <lorenzoniandre@outlook.com>
2021-07-06 15:20:08 +0100

    Add Requirement 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+// import { objectOf, arrayOf } from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  // super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
@@ -20,5 +31,12 @@ class MovieLibrary extends Component {
     );
   }
 }
+
+/* MovieLibrary.propTypes = {
+  movies: arrayOf(objectOf),
+};
+MovieLibrary.defaultProps = {
+  movies: ['object'],
+}; */
 
 export default MovieLibrary;
