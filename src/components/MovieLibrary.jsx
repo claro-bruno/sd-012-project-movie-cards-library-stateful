482cf917093f2af74ff954549529ca9fec251499
AndZoni <lorenzoniandre@outlook.com>
2021-07-06 13:27:27 +0100

    corrigindo lint inicial

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,20 +5,20 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  // super(props);
+  // }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList />
         <AddMovie />
       </div>
     );
   }
 }
-
+// movies={ this.props.movies }
 export default MovieLibrary;
