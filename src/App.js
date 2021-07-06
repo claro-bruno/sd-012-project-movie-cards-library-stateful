9df3b258ddcdddde22cda18f9f5b48d958766469
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-07-05 21:49:13 -0300

    Fin Req 1.1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Header from './components/Header';
-import './App.css';
+import React, { Component } from 'react';
+import MovieList from './components/MovieList';
+import movies from './data';
+import SearchBar from './components/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello World! </h1> 
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MovieList movies={ movies } />
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <label htmlFor="busca">
+        <input type="search" name="busca" />
+      </label>
+    );
+  }
+}
+
+export default SearchBar;
