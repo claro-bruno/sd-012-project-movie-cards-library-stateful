20ecd6545fa20965d0e17a72b7ba65ed8bfae40e
Julio Pereira <jandrade.pereira1@gmail.com>
2021-07-06 01:25:21 -0300

    Requisitos 15 e 16 - Insere as respostas dos requisitos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AddMovie onClick="callback" />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,80 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [...props.movies],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.funcFilter = this.funcFilter.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    }, () => this.filterMovies());
+  }
+
+  // O trecho a seguir é retirado do projeto da colega Caroline Benichio
+  // Fonte: https://github.com/tryber/sd-012-project-movie-cards-library-stateful/tree/caroline-benichio-movie-cards-library-stateful
+
+  onClick(newMovie) { // feito em conjunto com Lucas, Gui e Gabriel
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, newMovie],
+    });
+  }
+
+  funcFilter() { // feito em conjunto com Lucas, Gui e Gabriel
+    const { bookmarkedOnly, searchText, selectedGenre, movies } = this.state;
+    return (movies.filter((movie) => (
+      (movie.title.toLowerCase().includes(searchText.toLowerCase())
+      || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+      || movie.storyline.toLowerCase().includes(searchText.toLowerCase()))
+      && (bookmarkedOnly ? movie.bookmarked : true)
+      && movie.genre.includes(selectedGenre)
+    )));
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList />
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.handleChange }
+          onBookmarkedChange={ this.handleChange }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ this.funcFilter() } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
