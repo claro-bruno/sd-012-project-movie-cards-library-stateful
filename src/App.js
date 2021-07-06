dd0f31bf660d4dee0577443b9c3b3b1fb2d1ac3e
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-06 15:25:42 -0300

    Implementa App.css e App.js do projeto anterior.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import movies from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }
