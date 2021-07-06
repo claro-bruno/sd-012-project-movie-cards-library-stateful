5327dcb6bdd67a585d2e381dad3ff59e013e0fcb
Álvaro Palma <palma.alvaro@outlook.com>
2021-07-06 09:45:34 -0300

    Cria subtitle, imagepath e storyline inputs em AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,10 +15,25 @@ class AddMovie extends React.Component {
     }
 
     this.addTitle = this.addTitle.bind(this);
+    this.addSubtitle = this.addSubtitle.bind(this);
+    this.addImage= this.addImage.bind(this);
+    this.addStoryline= this.addStoryline.bind(this);
   }
 
   addTitle(e) {
     this.setState({ title: e.target.value });
+  }
+
+  addSubtitle(e) {
+    this.setState({ subtitle: e.target.value });
+  }
+
+  addImage(e) {
+    this.setState({ imagePath: e.target.value });
+  }
+  
+  addStoryline(e) {
+    this.setState({ storyline: e.target.value });
   }
 
   render() {
@@ -33,12 +48,35 @@ class AddMovie extends React.Component {
 
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label">Título:
+        <label data-testid="title-input-label">Título
           <input
             data-testid="title-input"
             type="text"
             value={ title }
             onChange={ this.addTitle }
+            />
+        </label>
+        <label data-testid="subtitle-input-label">Subtítulo
+          <input
+            data-testid="subtitle-input"
+            type="text"
+            value={ subtitle }
+            onChange={ this.addSubtitle }
+            />
+        </label>
+        <label data-testid="image-input-label">Imagem
+          <input
+            data-testid="image-input"
+            type="text"
+            value={ imagePath }
+            onChange={ this.addImage }
+            />
+        </label>
+        <label data-testid="storyline-input-label">Sinopse
+          <textarea
+            data-testid="storyline-input"
+            value={ storyline }
+            onChange={ this.addStoryline }
             />
         </label>
       </form>
