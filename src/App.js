72c6601f9939bf334c3486d1f4342e5b4226b915
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-07-06 18:47:00 -0300

    Refatora import do App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import MovieList from './components/MovieList';
 import movies from './data';
-import Header from './components/Header';
 import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 
 class App extends React.Component {
   render() {
