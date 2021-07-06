e04624fa216f643608f39795e4288e40e97ccfd2
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-05 21:24:38 -0300

    adiciona requisito 1, 2 e 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import SearchBar from './SearchBar';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
 
     return (
-      <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      <div>
+ 
+        <div data-testid="movie-list" className="movie-list">
+          { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        </div>
+
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    return <SearchBar />;
+    const {  
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" />Inclui o texto:
+        <input 
+            type="text"
+            id="texto"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"          
+        />
+      </form>
+    );
   }
 }
+
+SearchBar.propTypes={
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
