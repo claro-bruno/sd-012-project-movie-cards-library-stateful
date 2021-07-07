98bd422cf11651bbe67ce5527f1b2c0013b3128f
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-07 02:53:17 -0300

    Componentiza todos as tags do formulario dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FilterForText from './FilterForText';
+import ShowFavs from './ShowFavs';
+import FilterForGenre from './FilterForGenre';
 
 class SearchBar extends React.Component {
   render() {
@@ -14,41 +17,19 @@ class SearchBar extends React.Component {
 
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="search-text" data-testid="text-input-label">
-          Inclui o texto:
-          <input
-            type="text"
-            id="search-text"
-            data-testid="text-input"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-          />
-        </label>
-        <label htmlFor="book-marked" data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input
-            type="checkbox"
-            id="book-marked"
-            data-testid="checkbox-input"
-            value={ searchText }
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-          />
-        </label>
-        <label htmlFor="select-genre" data-testid="select-input-label">
-          Filtrar por gênero
-          <select
-            id="select-genre"
-            data-testid="select-input"
-            value={ selectedGenre }
-            onChange={ onSelectedGenreChange }
-          >
-            <option data-testid="select-option" value="">Todos</option>
-            <option data-testid="select-option" value="action">Ação</option>
-            <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="thriller">Suspense</option>
-          </select>
-        </label>
+        <FilterForText
+          value={ searchText }
+          handleChange={ onSearchTextChange }
+        />
+        <ShowFavs
+          value={ searchText }
+          checked={ bookmarkedOnly }
+          handleChange={ onBookmarkedChange }
+        />
+        <FilterForGenre
+          value={ selectedGenre }
+          handleChange={ onSelectedGenreChange }
+        />
       </form>
     );
   }
