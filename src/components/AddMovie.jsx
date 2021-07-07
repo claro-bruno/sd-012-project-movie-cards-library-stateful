3db2e14f6c09584ec7c4fe1e3c2f85c27a44e08d
uriel <urielbochi@gmail.com>
2021-07-07 14:52:06 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default AddMovie;
