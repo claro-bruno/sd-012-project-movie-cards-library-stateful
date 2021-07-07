5a7a9d805b568cab1df544da2b7d7bcd93a30f64
Eicke Yumoto <eickeyumoto@gmail.com>
2021-07-07 11:13:25 -0300

    iniciando o projeto :rocket:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return escrevaOCodigoAqui;
+  }
+}
+
+export default SearchBar;
