3bd50d6fca90ba2d455fff80b88ded08f757e3ec
Janderson Soares <jandinsm@gmail.com>
2021-07-07 22:22:34 -0300

    72/27

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -22,33 +22,28 @@ class AddMovie extends Component {
     this.handleChange2 = this.handleChange2.bind(this);
   }
 
-  handleChange2({ e }) {
-    this.setState({ title: e.target.value,
-      subtitle: e.target.value,
-      imagePath: e.target.value,
-      storyline: e.target.value,
-      rating: e.target.value,
-      genre: e.target.value,
+  handleChange2(e) {
+    this.setState({ [e.target.name]: e.target.value,
     });
   }
 
   render() {
     const { title, subtitle,
-      imagePath, storyline, rating, genre, handleChange2 } = this.state;
+      imagePath, storyline, rating, genre } = this.state;
 
     return (
       <form data-testid="add-movie-form" action="">
-        <Title title={ title } handleChange={ handleChange2 } />
+        <Title title={ title } handleChange2={ this.handleChange2 } />
 
-        <Subtitle subtitle={ subtitle } handleChange={ handleChange2 } />
+        <Subtitle subtitle={ subtitle } handleChange2={ this.handleChange2 } />
 
-        <ImagePath imagePath={ imagePath } handleChange={ handleChange2 } />
+        <ImagePath imagePath={ imagePath } handleChange2={ this.handleChange2 } />
 
-        <Storyline storyline={ storyline } handleChange={ handleChange2 } />
+        <Storyline storyline={ storyline } handleChange2={ this.handleChange2 } />
 
-        <RatingAdd rating={ rating } handleChange={ handleChange2 } />
+        <RatingAdd rating={ rating } handleChange2={ this.handleChange2 } />
 
-        <SelectAdd genre={ genre } handleChange={ handleChange2 } />
+        <SelectAdd genre={ genre } handleChange2={ this.handleChange2 } />
 
         <ButtonAdd />
       </form>
