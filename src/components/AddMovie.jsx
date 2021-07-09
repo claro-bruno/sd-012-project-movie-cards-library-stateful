d7a768f9ec0adc06c1327be36598fe5acf4d837f
Ana Laura <analauraalves25@gmail.com>
2021-07-09 19:36:16 -0300

    renderiza AddMovie vazio

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = {
+      onClick: () => {},
+    };
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    return (
+      <form />
+    )
+  }
+}
+
+export default AddMovie;
