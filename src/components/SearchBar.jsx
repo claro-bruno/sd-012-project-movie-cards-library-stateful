b1c4f1bcbf6f4ae32a86068d24faf3a68843fb5c
Ana Laura <analauraalves25@gmail.com>
2021-07-09 19:27:52 -0300

    renderiza SearchBar com form - select e label

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,38 +14,41 @@ class SearchBar extends React.Component {
   }
 
   render() {
-    const { searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+
     return (
       <form data-testid="search-bar-form" action="">
-        <label
-          htmlFor="input-text"
-          data-testid="text-input-label"
-        >
+        <label data-testid="text-input-label" htmlFor>
           Inclui o texto:
           <input
             type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
-            name="input"
-            id="input-text"
           />
         </label>
-        <label
-          htmlFor="input-checkbox"
-          data-testid="checkbox-input-label"
-        >
+        <label data-testid="checkbox-input-label" htmlFor>
           Mostrar somente favoritos
           <input
             type="checkbox"
             onChange={ onBookmarkedChange }
             checked={ bookmarkedOnly }
             data-testid="checkbox-input"
-            id="input-checkbox"
           />
+        </label>
+        <label data-testid="select-input-label" htmlFor>
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
