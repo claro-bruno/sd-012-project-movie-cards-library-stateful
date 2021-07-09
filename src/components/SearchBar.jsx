7570eddb0edc256e922d0a94fc7103b7a58cc7f5
Ana Laura <analauraalves25@gmail.com>
2021-07-09 19:01:42 -0300

    renderiza SearchBar com form - input de texto e label

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,7 +14,25 @@ class SearchBar extends React.Component {
   }
 
   render() {
-    return <form data-testid="search-bar-form" action="" />;
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <form data-testid="search-bar-form" action="">
+        <label
+          htmlFor="input-text"
+          data-testid="text-input-label"
+        >
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            name="input"
+            id="input-text"
+          />
+        </label>
+      </form>
+    );
   }
 }
 
