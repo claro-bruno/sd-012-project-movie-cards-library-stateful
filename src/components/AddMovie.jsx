3e506d293776eead597aa886c116331664fe5bd0
Henrique Remedios <henriqueremedios@gmail.com>
2021-07-09 20:59:08 -0300

    Requisitos 6 e 7 finalizdos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title:'',
+      subtitle:'',
+      imagePath:'',
+      storyline:'',
+      rating: 0,
+      genre:'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form"></form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render() {
     const { searchText,
       bookmarkedOnly,
