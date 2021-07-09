d6f9e43703fe580f6e141914c52ae6b8874bc437
EmmanuelBMS <acquabriall2020@gmail.com>
2021-07-09 13:03:17 -0300

    resolve req 6, 7, 8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,18 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    const { subtitle, title, imagePath,
+       storyline, rating, genre} = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor>
+          Título
+          <input data-testid="title-input" value={ title } onChange={ title } />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
