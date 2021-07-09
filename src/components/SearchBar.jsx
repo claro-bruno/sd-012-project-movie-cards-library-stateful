e102d622415cdb7b0f6be99318654c8a19d194a6
Ana Laura <analauraalves25@gmail.com>
2021-07-09 18:22:39 -0300

    renderiza SearchBar vazio

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
-// implement SearchBar component here
+import React from 'react'
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = {
+      searchText: '',
+      onSearchTextChange: () => {},
+      bookmarkedOnly: true, 
+      onBookmarkedChange: () => {},
+      selectedGenre: '',
+      onSelectedGenreChange: () => {},
+    }
+  }
+
+  render() {
+    return <div />
+  }
+}
+
+export default SearchBar
