b43aa623fc69a2838084a6635c51286067dbd25b
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-10 11:09:44 -0300

    Implementa MovieLibrary, limpa erro de propTypes no Rating.jsx (requisitos 15 ao 20).

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import movies from './data';
-import MovieList from './components/MovieList';
-import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <MovieList movies={ movies } />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,121 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.searchTextUpdate = this.searchTextUpdate.bind(this);
+    this.bookmarkedUpdate = this.bookmarkedUpdate.bind(this);
+    this.selectedGenreUpdate = this.selectedGenreUpdate.bind(this);
+    this.addMovie = this.addMovie.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
 
-// class MovieLibrary extends Component {
-//   // constructor(props) {
-//   //   super(props);
-//   // }
+  searchTextUpdate(event) {
+    const { value } = event.target;
+    const { props } = this;
+    const { movies } = props;
+    const filter = movies
+      .filter((item) => item.title.includes(value)
+        || item.subtitle.includes(value)
+        || item.storyline.includes(value));
+    if (value === '') {
+      this.setState({
+        searchText: value,
+        movies,
+      });
+    } else {
+      this.setState({
+        searchText: value,
+        movies: filter,
+      });
+    }
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+  bookmarkedUpdate(event) {
+    const checkStatus = event.target.checked;
+    const { props } = this;
+    const { movies } = props;
+    const filter = movies.filter((item) => item.bookmarked);
+    if (checkStatus) {
+      this.setState({
+        bookmarkedOnly: checkStatus,
+        movies: filter,
+      });
+    } else {
+      this.setState({
+        bookmarkedOnly: checkStatus,
+        movies,
+      });
+    }
+  }
 
-// export default MovieLibrary;
+  selectedGenreUpdate(event) {
+    const { value } = event.target;
+    const { props } = this;
+    const { movies } = props;
+    const filter = movies.filter((item) => item.genre === value);
+    if (value === '') {
+      this.setState({
+        selectedGenre: value,
+        movies,
+      });
+    } else {
+      this.setState({
+        selectedGenre: value,
+        movies: filter,
+      });
+    }
+  }
+
+  addMovie(newMovie) {
+    this.setState((originalState) => ({
+      movies: [...originalState.movies, newMovie],
+    }));
+  }
+
+  // O projeto do Guilherme Hermenegildo me ajudou na ideia de como fazer os filtros: https://github.com/tryber/sd-012-project-movie-cards-library-stateful/pull/50/files
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.searchTextUpdate }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.bookmarkedUpdate }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.selectedGenreUpdate }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.addMovie } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      imagePath: PropTypes.string.isRequired,
+      bookmarked: PropTypes.bool.isRequired,
+      genre: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
+
+export default MovieLibrary;
