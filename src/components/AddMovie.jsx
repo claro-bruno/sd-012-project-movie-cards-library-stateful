ecbc502bde3ce7410df6f06698ce0b9b0c8c6455
Henrique Remedios <henriqueremedios@gmail.com>
2021-07-09 22:22:57 -0300

    Requisito 8 feito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+
 
 class AddMovie extends Component {
   constructor() {
     super();
     this.state = {
-      title:'',
-      subtitle:'',
-      imagePath:'',
-      storyline:'',
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
       rating: 0,
-      genre:'action',
+      genre: 'action',
     };
+
+    this.onChangeInputTitle = this.onChangeTitle.bind(this);
+    
   }
 
   render() {
     const { onClick } = this.props;
     return (
-      <form data-testid="add-movie-form"></form>
-    );
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="text-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            id="title-input"
+            type="text"
+            value={ this.state.title }
+            onChange={ this.onChangeTitle }
+          />
+        </label>
+      </form>
+    )
   }
 }
 
