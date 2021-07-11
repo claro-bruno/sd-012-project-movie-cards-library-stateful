537ad949ae4b11efd4c370c0cac6a142d0e2f228
mztbtg <mztbtg@mztbtg.mztbtg>
2021-07-11 15:13:27 -0300

    Willian Movie Cars Library Stateful

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
-// implement MovieLibrary component here
+import React, { component } from 'react';
+import MovieList from './MovieList';
+import SearchBar from  './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Minha incrível Movie Library</h2>
+                <SearchBar />
+                <MovieList movies={this.props.movies} />
+                <AddMovie />
+            </div>
+        );
+    }
+}
+
+export default MovieLibrary;
