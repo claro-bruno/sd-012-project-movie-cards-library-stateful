4ac99f51bf85a5fdc7f7b3cb224708790aa27ae8
MZTBTG <mztbtg@hotmail.com>
2021-07-11 20:38:56 -0300

    Requisito 5; Correção 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,14 +4,9 @@ import PropTypes from 'prop-types';
 class SearchBar extends Component {
   render() {
     const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
+      searchText,onSearchTextChange,bookmarkedOnly,
+      onBookmarkedChange,selectedGenre,onSelectedGenreChange,
     } = this.props;
-
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="textInput">
