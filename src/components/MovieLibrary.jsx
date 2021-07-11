8c889191235b92f929ad783b81200fd0e645fc9b
MZTBTG <mztbtg@hotmail.com>
2021-07-11 16:24:27 -0300

    Requisito 1, 2 e 3; Correção

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import MovieList from './MovieList';
-import SearchBar from  './SearchBar';
-import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <h2>Minha incrível Movie Library</h2>
-                <SearchBar />
-                <MovieList movies={this.props.movies} />
-                <AddMovie />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Minha incrível Movie Library</h2>
+        <SearchBar />
+        {/* <MovieList movies={ this.props.movies } /> */}
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
 }
 
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,32 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
-    render() {
-        const {
-            searchText,
-            onSearchTextChange,
-        } = this.props;
-        return(
-            <div>
-                <form data-testid="search-bar-form">
-                    <label data-testid="text-input-label">
-                    Inclui o texto  
-                        <input 
-                        type="text" 
-                        value={ searchText }
-                        onChange={ onSearchTextChange }
-                        data-testid="text-input"    
-                        />
-                    </label>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+    } = this.props;
+    return(
+      <div>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label">
+            Inclui o texto  
+            <input 
+              type="text" 
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"    
+            />
+          </label>
+        </form>
+      </div>
+    )
+  }
 }
 
 SearchBar.propTypes = {
-    seachText: PropTypes.string.isRequired,
-    onSearchTextChange: PropTypes.string.isRequired,
+  seachText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
 };
 
 export default SearchBar;
