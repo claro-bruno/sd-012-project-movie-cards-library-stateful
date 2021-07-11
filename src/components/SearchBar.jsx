cd2d102ab0c6eea2ee92759427a84b206c60d44e
MZTBTG <mztbtg@hotmail.com>
2021-07-11 20:47:35 -0300

    Requisito 5; Correção 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const {
-      searchText,onSearchTextChange,bookmarkedOnly,
-      onBookmarkedChange,selectedGenre,onSelectedGenreChange,
-    } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange, } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="textInput">
@@ -21,35 +19,34 @@ class SearchBar extends Component {
           <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
             Mostrar somente favoritos
             <input
-              id="checkbox-input"
               type="checkbox"
               checked={ bookmarkedOnly }
               onChange={ onBookmarkedChange }
               data-testid="checkbox-input"
             />
-            <label htmlFor="select-input" data-testid="select-input-label">
-              Filtrar por gênero
-              <select
-                name=""
-                id="select-input"
-                value={ selectedGenre }
-                onChange={ onSelectedGenreChange }
-                data-testid="select-input"
-              >
-                <option data-testid="select-option" value="">
-                    Todos
-                </option>
-                <option data-testid="select-option" value="action">
-                    Ação
-                </option>
-                <option data-testid="select-option" value="comedy">
-                    Comédia
-                </option>
-                <option data-testid="select-option" value="thriller">
-                    Terror
-                </option>
-              </select>
-            </label>
+          </label>
+          <label htmlFor="select-input" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              name=""
+              id="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
+              <option data-testid="select-option" value="">
+                  Todos
+              </option>
+              <option data-testid="select-option" value="action">
+                  Ação
+              </option>
+              <option data-testid="select-option" value="comedy">
+                  Comédia
+              </option>
+              <option data-testid="select-option" value="thriller">
+                  Terror
+              </option>
+            </select>
           </label>
         </label>
       </form>
