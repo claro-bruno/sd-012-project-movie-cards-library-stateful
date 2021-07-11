b91589864b3757e7f6ecdeba9d4056b68e3b1571
Ana Laura <analauraalves25@gmail.com>
2021-07-11 15:56:20 -0300

    seta estados da MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies, 
+    }
+  }
+  
   render() {
     const { movies } = this.props;
     return <div />;
