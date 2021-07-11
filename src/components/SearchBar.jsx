5b81f92a616ce30365cd1f5824b55ee1a7eaa48a
MZTBTG <mztbtg@hotmail.com>
2021-07-11 19:31:15 -0300

    Requisito 4; Correção 8

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,29 +11,28 @@ class SearchBar extends Component {
     } = this.props;
 
     return (
-      <div>
-        <form data-testid="search-bar-form">
-          <label data-testid="text-input-label" htmlFor="textInput">
-            Inclui o texto:
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="textInput">
+          Inclui o texto:
+          <input
+            id="textInput"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+          <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+            Mostrar somente favoritos
             <input
-              id="textInput"
-              type="text"
-              value={ searchText }
-              onChange={ onSearchTextChange }
-              data-testid="text-input"
+              id="checkbox-input"
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
             />
-            <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
-              Mostrar somente favoritos
-              <input
-                id="checkbox-input"
-                type="checkbox"
-                checked={ bookmarkedOnly }
-                onChange={ onBookmarkedChange }
-              />
-            </label>
           </label>
-        </form>
-      </div>
+        </label>
+      </form>
     );
   }
 }
