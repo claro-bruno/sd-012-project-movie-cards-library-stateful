6cf5a03846f000f0a70fc37141320743488df813
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-12 08:45:38 -0300

    implementa form em componnte searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,13 +3,51 @@ import React from 'react';
 import ProtoTypes from 'prop-types'
 
 class SearchBar extends React.Component{
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange, } = this.props;
+    return(
+      <form data-testid='search-bar-form' >
+        <label data-testid="text-input-label" >
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            value={searchText}
+            onChange={onBookmarkedChange}
+          />
+        </label>
 
+        <label data-testid="checkbox-input-label" >
+          Mostrar somente favoritos
+          <input 
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={bookmarkedOnly} />
+        </label>
+
+        <label data-testid="select-input-label" >
+          Filtrar por gênero
+         <select
+          data-testid="select-input" 
+          name="genre"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+         </select>
+        </label>
+      </form>
+    )
+  }
 }
 
 SearchBar.prototype = {
   searchText: ProtoTypes.string,
   onSearchTextChange: ProtoTypes.func,
   bookmarkedOnly: ProtoTypes.bool,
+  onBookmarkedChange: ProtoTypes.func,
   selectedGenre: ProtoTypes.string,
   onSelectedGenreChange: ProtoTypes.func
 }
