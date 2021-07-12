13070a15ba1f20f883f4fa3dcd08489938b6b4be
MZTBTG <mztbtg@hotmail.com>
2021-07-11 21:24:08 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <AddMovie />
     </div>
   );
 }
