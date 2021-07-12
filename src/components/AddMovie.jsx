912a3de5c579a1aea39fbda30ff1adc51803b18a
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-11 22:15:43 -0300

    adiciona requsiitos 6, 7 e 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,35 +1,51 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Input from './Input';
 
-// class AddMovie extends React.Component {
-//     constructor() {
-//       super();
-//       this.state = {
-//         subtitle: '',
-//         title: '',
-//         imagePath: '',
-//         storyline: '',
-//         rating: 0,
-//         genre: 'action',
-//       }
-//     }
+class AddMovie extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        // subtitle: '',
+        title: '',
+        // imagePath: '',
+        // storyline: '',
+        // rating: 0,
+        // genre: 'action',
+      };
 
-//   render() {
-//     const { onClick } = this.props;
-//     return (
-//       <form data-testid='add-movie-form'>
-//         <label htmlFor='titulo' data-testid='title-input-label'>
-//           Título
-//         <input id='titulo' type='text' onChange={ (e) => this.state.title } } />
+      this.handleChange = this.handleChange.bind(this);
+    }
 
-//         </label>
-//       </form>
-//     );
-//   }
-// }
+ //requisitos feito com ajuda do Wadson Almeida!
+    handleChange(event) {
+        this.setState = ({ 
+          subtitle: event.target.value, 
+          title: event.target.value, 
+          imagePath: event.target.value, 
+          storyline: event.target.value, 
+          rating: event.target.value, 
+          genre: event.target.value, 
+        });
+    }
 
-// AddMovie.proptypes = {
-//     onclick: PropTypes.func.isRequired,
-// };
+  render() {
+    const { title } = this.state;
+    return (
+      <form data-testid='add-movie-form'>
+        <Input
+          typeInput='text'
+          textLabel='Título'
+          valueInput={ title }
+          onChangeInput={ this.handleChange }
+        />
+      </form>
+    )
+  }
+}
 
-//   export default AddMovie;
+AddMovie.propTypes = PropTypes.exact({
+  onClick: PropTypes.func.isRequired,
+}).isRequired;
+
+export default AddMovie;
---
+++ src/components/Input.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends React.Component {
+    render() {
+      const { typeInput, textLabel, valueInput, onChangeInput } = this.props;
+      return (
+        <label htmlFor='titulo' data-testid='title-input-label'>
+          { textLabel }
+          <input type={ typeInput } value={ valueInput } onChange={ onChangeInput } data-testid='title-input' />
+        </label>
+
+        )
+    }
+}
+
+Input.propTypes = PropTypes.exact({
+    subtitle: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    genre: PropTypes.string.isRequired,
+}).isRequired;
+
+export default Input; 
