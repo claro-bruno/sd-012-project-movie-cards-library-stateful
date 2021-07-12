b66af9c6141d9eec27b09d9289f32350e20942f2
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-12 08:23:58 -0300

    implementa proptypes em componente searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
 // implement SearchBar component here
+import React from 'react';
+import ProtoTypes from 'prop-types'
+
+class SearchBar extends React.Component{
+
+}
+
+SearchBar.prototype = {
+  searchText: ProtoTypes.string,
+  onSearchTextChange: ProtoTypes.func,
+  bookmarkedOnly: ProtoTypes.bool,
+  selectedGenre: ProtoTypes.string,
+  onSelectedGenreChange: ProtoTypes.func
+}
+
+export default SearchBar;
