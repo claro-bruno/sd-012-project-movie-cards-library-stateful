2017bfe1916af05ea24ea9b406cfc77f1ac3490e
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-12 08:56:30 -0300

    implementa estados e proptypes em componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class AddMovie extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+}
+
+AddMovie.prototype = {
+  onClick: PropTypes.func
+}
+
+export default AddMovie;
