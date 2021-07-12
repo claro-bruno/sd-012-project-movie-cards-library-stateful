57e49335ac2c7072d756d743ea7f396cfa6b111e
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-07-11 22:09:46 -0300

    Adiciona requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,52 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label
+          htmlFor="text-input"
+          data-testid="text-input-label"
+        >
+          Inclui o texto
+          <input
+            type="search"
+            name="text-input"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <button type="submit">Search</button>
+        <label
+          htmlFor="checkbox-input"
+          data-testid="checkbox-input-label"
+        >
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name="checkbox-input"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
--- src/tests/AddMovie.test.js
+++ src/tests/AddMovie.test.js
@@ -54,7 +54,7 @@ beforeEach(() => {
 });
 
 describe('6 - Crie um componente chamado `<AddMovie />`', () => {
-  it.skip('Renderize o componente', () => {
+  it('Renderize o componente', () => {
     render(<AddMovie onClick={ () => jest.fn() } />);
   });
 });
