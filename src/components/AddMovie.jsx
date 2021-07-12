efb94bc8761dc8c0ac7afb3eb9e86432f6d9a797
Josimar Schneider <josimarschneider@gmail.com>
2021-07-11 23:57:15 -0400

    componente AddMovie de 6 a 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,55 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import TitleInput from './TitleInput';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleButtom = this.handleButtom.bind(this);
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleButtom() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  handleChange(event) {
+    const { name } = event.target;
+    this.setState({
+      [name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    const { handleChange } = this;
+    return (
+      <form data-testid="add-movie-form">
+        <TitleInput title={ title } handleChange={ handleChange } />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
