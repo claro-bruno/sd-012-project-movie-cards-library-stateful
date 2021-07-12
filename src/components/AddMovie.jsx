b8b999dd97911f9c293ae86857a373d6ce3abee3
MZTBTG <mztbtg@hotmail.com>
2021-07-11 21:47:46 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 class AddMovie extends Component {
   render() {
+    const {
+      title = "",
+    } = this.props;
+
     return (
       <form data-testid="add-movie-form">
-        
+        <label htmlFor="" data-testid="title-input-label">
+          <input
+          type="text"
+          value={ title }
+          data-testid="title-input"
+          onChange={ title = input.value }
+          />
+        </label>
       </form>
     );
   }
