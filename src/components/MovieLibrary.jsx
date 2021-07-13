9f4f59df9e67108efb39470d8af5cd54a466b73f
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-07-13 01:15:12 -0300

    Por algum motivo a passagem de props foi aceita

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,26 +1,30 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
-/* import PropTypes from 'prop-types'; */
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     this.state = {
       searchText: '',
-      /* bookmarkedOnly: false,
-      selectedGenre: '', */
-      /* movies: props.movies, */
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
     };
   }
 
   render() {
     const { searchText } = this.state;
+    const { movies } = this.props;
+
     return (
       <section>
         <SearchBar searchText={ searchText } />
         <AddMovie />
+        <MovieList movies={ movies } />
       </section>
     );
   }
@@ -28,8 +32,8 @@ class MovieLibrary extends Component {
 
 export default MovieLibrary;
 
-/* MovieLibrary.propTypes = {
+MovieLibrary.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
-}; */
+};
