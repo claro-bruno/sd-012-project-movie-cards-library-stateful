f13d46ab75c112d1cfd1dba476908a58fcb69b01
victordg00 <victord.goncalves@outlook.com>
2021-07-13 09:37:08 -0300

    req 8 e 9 finalizados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,11 +2,50 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+      subtitle: '',
+    };
+
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleSubtitleChange = this.handleSubtitleChange.bind(this);
+  }
+
+  handleTitleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  handleSubtitleChange(event) {
+    this.setState({ subtitle: event.target.value });
+  }
+
   render() {
+    const { title, subtitle } = this.state;
+
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="text">
-          <input type="text" />
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+          <input
+            data-testid="title-input"
+            name="title"
+            type="text"
+            value={ title }
+            onChange={ this.handleTitleChange }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">
+          Subtítulo
+          <input
+            data-testid="subtitle-input"
+            name="subtitle"
+            type="text"
+            value={ subtitle }
+            onChange={ this.handleSubtitleChange }
+          />
         </label>
       </form>
     );
