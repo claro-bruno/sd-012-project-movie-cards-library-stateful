fbdd44ec1f245d0ddf67c5210efacfea5144b54e
MZTBTG <mztbtg@hotmail.com>
2021-07-12 23:06:07 -0300

    Reduzindo números de linhas, desfazendo requisito 11 e corriindo 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -7,7 +7,6 @@ class AddMovie extends Component {
       title: '',
       subtitle: '',
       imagePath: '',
-      storyline: '',
     };
     this.textInputHandler = this.textInputHandler.bind(this);
   }
@@ -20,18 +19,12 @@ class AddMovie extends Component {
   }
 
   render() {
-    const {
-      title,
-      subtitle,
-      imagePath,
-      storyline,
-    } = this.state;
+    const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
+        <label data-testid="title-input-label">
           Título
           <input
-            id="title-input"
             type="text"
             value={ title }
             data-testid="title-input"
@@ -39,10 +32,9 @@ class AddMovie extends Component {
             name="title"
           />
         </label>
-        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        <label data-testid="subtitle-input-label">
           Subtítulo
           <input
-            id="subtitle-input"
             type="text"
             value={ subtitle }
             data-testid="subtitle-input"
@@ -50,26 +42,14 @@ class AddMovie extends Component {
             name="subtitle"
           />
         </label>
-        <label htmlFor="image-input" data-testid="image-input-label">
+        <label data-testid="image-input-label">
           Imagem
           <input
-            id="image-input"
             type="text"
             value={ imagePath }
             data-testid="image-input"
             onChange={ this.textInputHandler }
             name="image"
-          />
-        </label>
-        <label htmlFor="storyline-input" data-testid="storyline-input-label">
-          Imagem
-          <textarea
-            id="storyline-input"
-            type="text"
-            value={ storyline }
-            data-testid="storyline-input"
-            onChange={ this.textInputHandler }
-            name="storyline"
           />
         </label>
       </form>
