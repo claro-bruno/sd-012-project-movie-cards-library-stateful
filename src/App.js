62d9257bc941f7c485f49449cb666d2aee50c201
EmmanuelBMS <acquabriall2020@gmail.com>
2021-07-15 11:09:18 -0300

    resolve requisito 15, 16 com falta de passar movie como props, e 17, ainda sem lint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
-        <AddMovie />
+        <MovieLibrary />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,28 @@
-// implement MovieLibrary component here
+import React from 'react';
+import data from '../data';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [data],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
