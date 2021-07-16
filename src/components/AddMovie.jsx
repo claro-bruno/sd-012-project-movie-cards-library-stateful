b2db8a945d0d9b9018dde0b2660819388cb56b40
MZTBTG <mztbtg@hotmail.com>
2021-07-16 12:48:43 -0300

    AddMovie atualizado para evitar erros de tamanho e adição de idéia genial de um colega

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TitleForm from './TitleForm';
+import AutoForm from './AutoForm';
 
 class AddMovie extends Component {
   constructor() {
@@ -23,31 +23,33 @@ class AddMovie extends Component {
     const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <TitleForm
+
+        {/* Achei muito legal essa idéia do Leonardo Ferreira de fazer um componente altamente dinãmido e decidir fazer algo parecido! */}
+
+        <AutoForm 
           valueTitle={ title }
+          labelHtmlFor="title-input"
+          labelDataTestId="title-input-label"
+          labelTitle="Título"
+          onChange={ this.textInputHandler }
+          nameInput="title"
         />
-        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            id="subtitle-input"
-            type="text"
-            value={ subtitle }
-            data-testid="subtitle-input"
-            onChange={ this.textInputHandler }
-            name="subtitle"
-          />
-        </label>
-        <label htmlFor="image-input" data-testid="image-input-label">
-          Imagem
-          <input
-            id="image-input"
-            type="text"
-            value={ imagePath }
-            data-testid="image-input"
-            onChange={ this.textInputHandler }
-            name="imagePath"
-          />
-        </label>
+        <AutoForm 
+          valueTitle={ subtitle }
+          labelHtmlFor="subtitle-input"
+          labelDataTestId="subtitle-input-label"
+          labelTitle="Subtítulo"
+          onChange={ this.textInputHandler }
+          nameInput="subtitle"
+        />
+        <AutoForm 
+          valueTitle={ imagePath }
+          labelHtmlFor="image-input"
+          labelDataTestId="image-input-label"
+          labelTitle="Imagem"
+          onChange={ this.textInputHandler }
+          nameInput="imagePath"
+        />
       </form>
     );
   }
---
+++ src/components/AutoForm.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AutoForm extends Component {
+  render() {
+    const {
+      valueInput,
+      labelHtmlFor,
+      labelDataTestId,
+      labelTitle,
+      typeInput,
+      onChange,
+      nameInput,
+    } = this.props;
+    return (
+      <label htmlFor={ labelHtmlFor } data-testid={ labelDataTestId }>
+          {labelTitle}
+          <input
+            id={ labelHtmlFor }
+            type={ typeInput }
+            value={ valueInput }
+            data-testid={ labelDataTestId }
+            onChange={ onChange }
+            name={ nameInput }
+          />
+        </label>
+    );
+  }
+}
+
+AutoForm.defaultProps = {
+  typeInput: 'text',
+};
+
+AutoForm.propTypes = {
+  valueInput: PropTypes.string.isRequired,
+  labelHtmlFor: PropTypes.string.isRequired,
+  labelDataTestId: PropTypes.string.isRequired,
+  labelTitle: PropTypes.string.isRequired,
+  typeInput: PropTypes.string,
+  onChange: PropTypes.string.isRequired,
+  nameInput: PropTypes.string.isRequired,
+};
+
+export default AutoForm;
