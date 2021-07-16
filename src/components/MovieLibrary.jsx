a57e06b4a05b26a1b8f6e68dc4d9a0456a8e8faf
MZTBTG <mztbtg@hotmail.com>
2021-07-16 13:04:14 -0300

    AddMovie atualizado; Correção 4

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
 //   constructor(props) {
