28926eafe9cb015704687376a30011106d5c3fe3
Pedro Resck <pedroresck1@gmail.com>
2021-07-18 19:44:28 -0300

    Requisito 5 - renderize um select dentro do form de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -26,6 +26,19 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
