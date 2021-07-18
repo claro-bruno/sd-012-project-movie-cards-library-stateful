fce6c8f5b292ddf56112288ec857d1e00f563878
Pedro Resck <pedroresck1@gmail.com>
2021-07-18 19:10:08 -0300

    Requisito 2 - renderize um form dentro de SearchBar. Adicionei as props do SearchBar que eu havia esquecido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+
     return (
-      <>Search Bar</>
+      <form data-testid="search-bar-form">
+        Oi
+      </form>
     );
   }
 }
+
+SearchBar.protoTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
 export default SearchBar;
