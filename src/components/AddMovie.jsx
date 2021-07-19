0db897ddb67507e6f285a0b4ecbb4f27ec694821
Alice Gonçalves <galice9992@gmail.com>
2021-07-19 18:33:07 -0300

    resolve requisito 8, title input + label + state

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       subtitle: '',
-//       title: '',
-//       imagePath: '',
-//       storyline: '',
-//       rating: 0,
-//       genre: 'action',
-//     };
-//   }
+  constructor(props) {
+    super(props);
+    this.state = {
+    //   subtitle: '',
+      title: '',
+    //   imagePath: '',
+    //   storyline: '',
+    //   rating: 0,
+    //   genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ title: event.target.value });
+  }
 
   render() {
-    return (<form data-testid="add-movie-form" />);
+    const { title } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            id="title-input"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
   }
 }
 
