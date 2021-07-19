5b3bbbc923f0b3d5ace01dc98f1d111e2136e436
Djaniza Vasques <djanizav@gmail.com>
2021-07-19 09:55:45 -0300

    Adiciona requisitos 15, 16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
-import movies from '../data';
 
-class MovieLibrary extends Component {
+class MovieLibrary extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <SearchBar />
@@ -15,3 +16,7 @@ class MovieLibrary extends Component {
 }
 
 export default MovieLibrary;
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
