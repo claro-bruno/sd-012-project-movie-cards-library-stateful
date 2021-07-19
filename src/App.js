b3ab8c51aaa00269df0e15197d6f49d7ea68d9b7
Alice Gonçalves <galice9992@gmail.com>
2021-07-19 18:04:49 -0300

    resolve requisitos 6 e 7, criar e renderizar componente AddMovie e renderizar uma tag form dentro de AddMovie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         selectedGenre=""
         onSelectedGenreChange={ () => {} }
       />
+      <AddMovie onClick={ () => {} } />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,21 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       subtitle: '',
+//       title: '',
+//       imagePath: '',
+//       storyline: '',
+//       rating: 0,
+//       genre: 'action',
+//     };
+//   }
+
+  render() {
+    return (<form data-testid="add-movie-form" />);
+  }
+}
+
+export default AddMovie;
