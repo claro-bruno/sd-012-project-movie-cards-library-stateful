191c0750a37dd6da1220ead6a3cb8633ddaddb0b
MZTBTG <mztbtg@hotmail.com>
2021-07-19 12:46:31 -0300

    Retornando projeto ao ponto de maior avaliação

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import AutoForm from './AutoForm';
 
 class AddMovie extends Component {
   constructor() {
@@ -17,38 +16,47 @@ class AddMovie extends Component {
     this.setState({ [name]: value });
   }
 
-  // Achei muito legal essa idéia do Leonardo Ferreira de fazer
-  // um componente altamente dinãmido e decidir fazer algo parecido!
-
   render() {
-    const { title, subtitle, imagePath } = this.state;
+    const {
+      title,
+      subtitle,
+      imagePath,
+    } = this.state;
     return (
       <form data-testid="add-movie-form">
-
-        <AutoForm
-          valueTitle={ title }
-          labelHtmlFor="title-input"
-          labelDataTestId="title-input-label"
-          labelTitle="Título"
-          onChange={ this.textInputHandler }
-          nameInput="title"
-        />
-        <AutoForm
-          valueTitle={ subtitle }
-          labelHtmlFor="subtitle-input"
-          labelDataTestId="subtitle-input-label"
-          labelTitle="Subtítulo"
-          onChange={ this.textInputHandler }
-          nameInput="subtitle"
-        />
-        <AutoForm
-          valueTitle={ imagePath }
-          labelHtmlFor="image-input"
-          labelDataTestId="image-input-label"
-          labelTitle="Imagem"
-          onChange={ this.textInputHandler }
-          nameInput="imagePath"
-        />
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            id="title-input"
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.textInputHandler }
+            name="title"
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            id="subtitle-input"
+            type="text"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.textInputHandler }
+            name="subtitle"
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            id="image-input"
+            type="text"
+            value={ imagePath }
+            data-testid="image-input"
+            onChange={ this.textInputHandler }
+            name="imagePath"
+          />
+        </label>
       </form>
     );
   }
