71b5015804ccd6c6bec98ecdd296481e5fb7c4d1
Pedro Resck <pedroresck1@gmail.com>
2021-07-19 03:16:42 -0300

    Requisito 15 - renderize o componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement MovieLibrary component
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <>
+        Movie Library
+      </>
+    );
+  }
+}
+
+export default MovieLibrary;
