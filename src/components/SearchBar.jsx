5a01536539d832f3b222ba45f17d7defbce4833c
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-07-19 21:24:54 -0300

    Renderizando o componente SearchBar e passando as devidas props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>SearchBar</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
