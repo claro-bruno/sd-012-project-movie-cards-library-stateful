2671a15eb799900b938a9f37501e084fd28bb2bc
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-07-19 23:31:49 -0300

    Renderiza o select com as opções de gêneros de filmes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,17 +4,12 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
   render() {
     const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label
-          data-testid="text-input-label"
-          htmlFor="input-label"
-        >
+        <label data-testid="text-input-label" htmlFor="input-label">
           Inclui o texto:
           <input
             type="text"
@@ -36,6 +31,23 @@ class SearchBar extends React.Component {
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
+        </label>
+        <label
+          htmlFor="genre-options"
+          data-testid="select-input-label"
+        >
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            id="genre-options"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
