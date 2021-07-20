780021c056b15342066ad3a824fd9dcbc5e0fa4e
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-20 08:43:05 -0300

    adiciona requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default MovieLibrary;
