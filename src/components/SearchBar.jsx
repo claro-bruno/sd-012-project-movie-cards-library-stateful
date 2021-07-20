fcbcdc50f9f118c9695341cbbf3a902eb441323f
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-07-19 22:18:47 -0300

    Renderiza uma label e um input com o termo de pesquisa do filme

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form
-        data-testid="search-bar-form"
-      />
+      <form data-testid="search-bar-form">
+        <label
+          data-testid="text-input-label"
+          htmlFor="input-label"
+        >
+          Inclui o texto:
+          <input
+            type="text"
+            id="input-label"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
+
+SearchBar.defaultProps = {
+  searchText: '',
+};
 
 export default SearchBar;
