634f3b7d8c43248ccdeb6c136d140b777c64e380
Lucas Basques <lukasbasques@gmail.com>
2021-07-20 23:15:18 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+    };
+    this.onChangeHandle = this.onChangeHandle.bind(this);
+  }
+
+  onChangeHandle({ target }) {
+    const { value } = target;
+
+    this.setState({
+      title: value,
+    });
+  }
+
   render() {
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <p>placeholder</p>
+        <div htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            id="title-input"
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.onChangeHandle }
+          />
+        </div>
       </form>
     );
   }
