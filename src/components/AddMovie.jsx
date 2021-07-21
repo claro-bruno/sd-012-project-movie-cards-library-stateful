81c5fb0013402da883dffa0490a74a04d663d759
Caio-Veloso <caio.veloso@gmail.com>
2021-07-21 19:10:03 -0300

    Nunca te pedi nada Lint, quebra essa

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import TextArea from './TextArea';
 import NumberInput from './NumberInput';
 import Select from './Select';
