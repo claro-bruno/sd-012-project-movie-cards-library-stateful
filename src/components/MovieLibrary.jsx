c12ce368a3b0487316d2d5a7e0b8d1dec820cb62
Lucas Basques <lukasbasques@gmail.com>
2021-07-21 20:52:40 -0300

    Requisitos 15, 16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,53 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.onChangeHandle = this.onChangeHandle.bind(this);
+  }
+
+  onChangeHandle({ target }) {
+    const { name, type, checked } = target;
+    const value = type === 'checkbox' ? checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onChangeHandle }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onChangeHandle }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onChangeHandle }
+        />
+        <MovieList movies={ movies } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
