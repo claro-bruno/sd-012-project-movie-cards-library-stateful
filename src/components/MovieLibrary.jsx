a7238c62f770f6ebb73d4f20aed447c33d91601b
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-21 17:53:46 -0400

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+      </div>
+    );
+  }
+}
