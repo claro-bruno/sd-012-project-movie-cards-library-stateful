edc87bf477b59b9e555c00f9b4361ec4e82d3d8f
Aldria Melo <aldriamelo.academico@gmail.com>
2021-07-21 19:32:09 -0300

    adiciona requisitos 15 e 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropType from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibray extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    // this.listaFilmes = this.listaFilmes.bind(this);
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibray.propTypes = {
+  movies: PropType.arrayOf(PropType.object).isRequired,
+};
+
+export default MovieLibray;
