e02a4be0ef5695ab70f50edb59bc221fe19bb73a
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-21 19:41:49 -0400

    Requisito 3 - Rendezida input com as devidas props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class SearchBar extends Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      // bookmarkedOnly,
+      // onBookmarkedChange,
+      // selectedGenre,
+      // onSelectedGenreChange
+    } = this.props;
+
     return (
       <div>
-        <from data-testid="search-bar-form">
-          Dentro do form
-        </from>
+        <form data-testid="search-bar-form">
+
+          <label data-testid="text-input-label" htmlFor="Inclui o texto:">
+            Inclui o texto:
+            <input
+              data-testid="text-input"
+              name="imputTest"
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+
+        </form>
       </div>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  // bookmarkedOnly: PropTypes.bool.isRequired,
+  // onBookmarkedChange: PropTypes.func.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
+  // onSelectedGenreChange: PropTypes.func.isRequired,
+};
