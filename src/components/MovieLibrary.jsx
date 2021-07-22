dfbd3401e0fdd43c756eb2060778df8fadbac61b
Caio-Veloso <caio.veloso@gmail.com>
2021-07-21 23:00:49 -0300

    requisto 15 ao 18

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [...props.movies],
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    const { value } = event.target;
+    this.setState({ searchText: value });
+  }
+
+  onBookmarkedChange(event) {
+    const { checked } = event.target;
+    this.setState({ bookmarkedOnly: checked });
+  }
+
+  onSelectedGenreChange(event) {
+    const { value } = event.target;
+    this.setState({ selectedGenre: value });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    let { movies } = this.state;
+    if (bookmarkedOnly) movies = movies.filter((movie) => movie.bookmarked);
+
+    if (selectedGenre) movies = movies.filter((movie) => movie.genre === selectedGenre);
+
+    if (searchText) {
+      movies = movies
+        .filter((
+          { title, subtitle, storyline },
+        ) => title.includes(searchText)
+        || subtitle.includes(searchText) || storyline.includes(searchText));
+    }
+
+    return (
+      <section>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList movies={ movies } />
+      </section>
+    );
+  }
+}
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(objectOf).isRequired,
+};
+export default MovieLibrary;
