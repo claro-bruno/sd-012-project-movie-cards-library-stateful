a3870226f0efa431bdc85af4d777de94028eb98a
Alexander <alexanderteodorocastro95@hotmail.com>
2021-07-22 14:52:28 -0300

    test6

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -88,7 +88,15 @@ class MovieLibrary extends React.Component {
 }
 
 MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object),
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })),
 };
 
 MovieLibrary.defaultProps = {
