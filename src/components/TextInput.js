5278a4bb9b9a80a26cebe49b2d9deb35baa62f10
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-21 22:09:48 -0400

    Requisito 5 - Cria componente de fomularios e renderiza no SearchBar

---
+++ src/components/TextInput.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class TextInput extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+    } = this.props;
+
+    return (
+      <label data-testid="text-input-label" htmlFor="Inclui o texto:">
+        Inclui o texto
+        <input
+          data-testid="text-input"
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+      </label>
+    );
+  }
+}
+
+TextInput.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
