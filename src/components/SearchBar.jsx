ee1d723880b5883af28553d00eb977be4eea26bc
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-21 22:27:25 -0400

    Requisito 5 - Movendo componentes inputs para pasta input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import SelectInput from './SelectInput';
-import TextInput from './TextInput';
-import CheckboxInput from './CheckboxInput';
+import SelectInput from './inputs/SelectInput';
+import TextInput from './inputs/TextInput';
+import CheckboxInput from './inputs/CheckboxInput';
 
 export default class SearchBar extends Component {
   render() {
