8efbe73ca9fb688b9e0d7285d5fddf7028f585d6
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-22 04:39:21 -0400

    Requisito 16 - Configura o estado do componente MovieLibrary

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
 import './App.css';
-import AddMovie from './components/AddMovie';
-import MovieList from './components/MovieList';
 import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
 
 export default class App extends Component {
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(param) {
-    console.log(param);
-  }
+  // }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
-        <MovieList movies={ movies } />
-        <AddMovie onClick={ this.handleClick } />
-        <MovieLibrary />
+        <MovieLibrary movies={ movies } />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.states = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+
+    console.log(props);
+  }
+
+  handleClick(param) {
+    console.log(param);
+  }
+
   render() {
-    // const { movies } = this.props;
+    const { movies } = this.props;
 
     return (
       <div>
-        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.handleClick } />
       </div>
     );
   }
 }
 
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-// };
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
