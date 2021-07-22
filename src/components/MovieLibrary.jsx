ae3c8fc5e57dcf66034fd2e47f1143c8d558402f
MZTBTG <mztbtg@hotmail.com>
2021-07-22 00:13:52 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-// import MovieList from './MovieList';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2>Minha incrível Movie Library</h2>
         <SearchBar />
-        {/* <MovieList movies={ this.props.movies } /> */}
-        {/* <AddMovie /> */}
+        <MovieList movies={ movies } />
+        <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
